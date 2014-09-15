597f6f22cc55b7e6586010b81b6792667ef5d3cc
GreenRiverRUS <vadim-mazaev@mail.ru>
2014-09-15 23:25:16 +0400

    Fix style bugs using checkstyle.

--- src/ru/fizteh/fivt/students/vadim_mazaev/shell/Shell.java
+++ src/ru/fizteh/fivt/students/vadim_mazaev/shell/Shell.java
@@ -1,9 +1,9 @@
 package ru.fizteh.fivt.students.vadim_mazaev.shell;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Shell {
-	public static void main(String[] args) {
+public abstract class Shell {
+	public static void main(final String[] args) {
 		if (args.length > 0) {
 			//package
 			StringBuilder buff = new StringBuilder();
@@ -14,32 +14,31 @@ public class Shell {
 			String[] cmds = buff.toString().split(";");
 			for (int j = 0; j < cmds.length; j++) {
 				System.out.println("$ " + cmds[j].trim());
-				if (!ShellParser.parse(cmds[j]))
+				if (!ShellParser.parse(cmds[j])) {
 					System.exit(1);
+				}
 			}
 			System.exit(0);
-		}
-		else {
+		} else {
 			//interactive
 			try {
 				System.setProperty("user.dir", System.getProperty("user.home"));
-			}
-			catch(SecurityException e) {
+			} catch (SecurityException e) {
 				System.out.println("cannot open home directory: access denied");
 				System.exit(1);
 			}
-			
 			Scanner in = new Scanner(System.in);
 			try {
 				while (true) {
 					System.out.print("$ ");
 					String[] cmds = in.nextLine().split(";");
-					for (int j = 0; j < cmds.length; j++)
-						if (!ShellParser.parse(cmds[j]))
+					for (int j = 0; j < cmds.length; j++) {
+						if (!ShellParser.parse(cmds[j])) {
 							break;
+						}
+					}
 				}
-			}
-			finally {
+			} finally {
 				in.close();
 			}
 		}
--- src/ru/fizteh/fivt/students/vadim_mazaev/shell/ShellParser.java
+++ src/ru/fizteh/fivt/students/vadim_mazaev/shell/ShellParser.java
@@ -1,17 +1,24 @@
 package ru.fizteh.fivt.students.vadim_mazaev.shell;
 
-import java.io.*;
-import java.nio.file.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
 
 public abstract class ShellParser {
-	public static boolean parse(String cmd) {
-		cmd = cmd.trim();
+	public static boolean parse(final String cmd) {
 		String cmdArgs = "";
-		String cmdWithoutArgs = cmd;
-		int cmdWithoutArgsEnds = cmd.indexOf(" ");
+		String cmdWithoutArgs = cmd.trim();
+		int cmdWithoutArgsEnds = cmd.trim().indexOf(" ");
 		if (cmdWithoutArgsEnds != -1) {
-			cmdArgs = cmd.substring(cmdWithoutArgsEnds + 1);
-			cmdWithoutArgs = cmd.substring(0, cmdWithoutArgsEnds);
+			cmdArgs = cmd.trim().substring(cmdWithoutArgsEnds + 1);
+			cmdWithoutArgs = cmd.trim().substring(0, cmdWithoutArgsEnds);
 		}
 		
 		switch (cmdWithoutArgs) {
@@ -37,108 +44,122 @@ public abstract class ShellParser {
 		try {
 			System.out.println(System.getProperty("user.dir"));
 			return true;
-		}
-		catch (SecurityException e) {
-			System.out.println("pwd: cannot read current working directory: access denied");
+		} catch (SecurityException e) {
+			System.out.println(
+			"pwd: cannot read current working directory: access denied");
 		}
 		return false;
 	}
 	
 	private static boolean ls() {
 		try {
-			String[] fileNamesList = new File(System.getProperty("user.dir")).list();
-			for (int i = 0; i < fileNamesList.length; i++)
+			String[] fileNamesList =
+					new File(System.getProperty("user.dir")).list();
+			for (int i = 0; i < fileNamesList.length; i++) {
 				System.out.println(fileNamesList[i]);
+			}
 			return true;
-		}
-		catch (SecurityException e) {
-			System.out.println("ls: cannot get the list of files: access denied");
+		} catch (SecurityException e) {
+			System.out.println(
+					"ls: cannot get the list of files: access denied");
 		}
 		return false;
 	}
 	
-	private static boolean cd(String cmdArgs) {
+	private static boolean cd(final String cmdArgs) {
 		if (cmdArgs.isEmpty()) {
 			System.out.println("cd: missing operand");
 			return false;
 		}
 		try {
 			Path path = FileSystems.getDefault().getPath(cmdArgs);
-			if (!path.isAbsolute())
-				path = FileSystems.getDefault().getPath(System.getProperty("user.dir"), cmdArgs);
+			if (!path.isAbsolute()) {
+				path = FileSystems.getDefault()
+						.getPath(System.getProperty("user.dir"), cmdArgs);
+			}
 			File newWorkingDir = path.normalize().toFile();
 			if (newWorkingDir.exists()) {
 				System.setProperty("user.dir", newWorkingDir.getPath());
 				return true;
+			} else {
+				System.out.println("cd: '"
+					+ newWorkingDir.getPath() + "': No such file or directory");
 			}
-			else
-				System.out.println("cd: '" + newWorkingDir.getPath() + "': No such file or directory");
-		}
-		catch (InvalidPathException e) {
-			System.out.println("cd: cannot change directory: illegal character in path");
-		}
-		catch (SecurityException e) {
+		} catch (InvalidPathException e) {
+			System.out.println(
+					"cd: cannot change directory: illegal character in path");
+		} catch (SecurityException e) {
 			System.out.println("cd: cannot change directory: access denied");
 		}
 		return false;
 	}
 	
-	private static boolean mkdir(String cmdArgs) {
+	private static boolean mkdir(final String cmdArgs) {
 		if (cmdArgs.isEmpty()) {
 			System.out.println("mkdir: missing operand");
 			return false;
 		}
 		try {	
-			File makedDir = FileSystems.getDefault().getPath(System.getProperty("user.dir"), cmdArgs).toFile();
+			File makedDir = FileSystems.getDefault()
+					.getPath(System.getProperty("user.dir"), cmdArgs).toFile();
 			if (!makedDir.mkdir()) {
-				System.out.println("mkdir: cannot create directory '" + cmdArgs + "': File exists");
+				System.out.println(
+						"mkdir: cannot create directory '"
+								+ cmdArgs + "': File exists");
 				return false;
 			}
 			return true;
-		}
-		catch (InvalidPathException e) {
-			System.out.println("mkdir: cannot create directory '" + cmdArgs + "': illegal character in name");
-		}
-		catch (SecurityException e) {
-			System.out.println("mkdir: cannot create directory '" + cmdArgs + "': access denied");
+		} catch (InvalidPathException e) {
+			System.out.println(
+					"mkdir: cannot create directory '"
+						+ cmdArgs + "': illegal character in name");
+		} catch (SecurityException e) {
+			System.out.println(
+					"mkdir: cannot create directory '"
+						+ cmdArgs + "': access denied");
 		}
 		return false;
 	}
 	
-	private static boolean cat(String cmdArgs) {
+	private static boolean cat(final String cmdArgs) {
 		if (cmdArgs.isEmpty()) {
 			System.out.println("cat: missing operand");
 			return false;
 		}
 		try {
-			File cattedFile = FileSystems.getDefault().getPath(System.getProperty("user.dir"), cmdArgs).toFile();
+			File cattedFile = FileSystems.getDefault()
+					.getPath(System.getProperty("user.dir"), cmdArgs).toFile();
 			if (cattedFile.exists()) {
 				if (cattedFile.isFile()) {
 					try (BufferedReader reader = new BufferedReader(
-			                new InputStreamReader(new FileInputStream(cattedFile)));) {
+					new InputStreamReader(new FileInputStream(cattedFile)));) {
 						String line;
-						while((line = reader.readLine()) != null) {
+						while ((line = reader.readLine()) != null) {
 							System.out.println(line);
 						}
-						System.out.println();
+						//System.out.println();
 					}
 					catch (IOException e) {
-						System.out.println("cat " + cmdArgs + ": cannot read file");
+						System.out.println(
+								"cat " + cmdArgs + ": cannot read file");
 						return false;
 					}
 					return true;
+				} else {
+					System.out.println(
+						"cat: " + cmdArgs + ": This is a directory");
 				}
-				else
-					System.out.println("cat: " + cmdArgs + ": This is a directory");
+			} else {
+				System.out.println(
+					"cat: " + cmdArgs + ": No such file or directory");
 			}
-			else
-				System.out.println("cat: " + cmdArgs + ": No such file or directory");
-		}
-		catch(SecurityException e) {
-			System.out.println("cat: cannot open file '" + cmdArgs + "': access denied");
-		}
-		catch (InvalidPathException e) {
-			System.out.println("cat: cannot open file '" + cmdArgs + "': illegal character in name");
+		} catch (SecurityException e) {
+			System.out.println(
+				"cat: cannot open file '" + cmdArgs + "': access denied");
+		} catch (InvalidPathException e) {
+			System.out.println(
+				"cat: cannot open file '"
+					+ cmdArgs + "': illegal character in name");
 		}
 		return false;
 	}
