59d6d895d009500fb9ebbf6622c02ac060a70a9b
Alexey <aa_Zhur@mail.com>
2014-09-16 19:05:55 +0400

    Test commit

--- src/ru/fizteh/fivt/students/Zhuravlyov/shell/ShellMain.java
+++ src/ru/fizteh/fivt/students/Zhuravlyov/shell/ShellMain.java
@@ -1,12 +1,13 @@
-package ru.fizteh.fivt.students.test.shell;
+package ru.fizteh.fivt.students.Zhuravlyov.shell;
 
 /**
- * @author test
+ * @author Zhuravlyov
  */
 public class ShellMain {
 
     public static void main(String[] args) {
         System.out.println("not working");
+        System.out.println("but will be workings soon");
         System.exit(1);
     }
 }
