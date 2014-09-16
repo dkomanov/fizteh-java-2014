99c7d594bf0875e63c4f209c25a2d1b22ed51cfa
MrShoco <karenmanuk@yandex.ru>
2014-09-16 19:50:37 +0400

    Hello world

---
+++ src/ru/fizteh/fivt/students/mrshoco/shell/ShellMain.java
@@ -0,0 +1,12 @@
+package ru.fizteh.fivt.students.test.shell;
+
+/**
+ * @author test
+ */
+public class ShellMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.exit(1);
+    }
+}
