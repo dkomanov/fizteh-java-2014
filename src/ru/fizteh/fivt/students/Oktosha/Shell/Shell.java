a20e495d056de1c1c9df27f5f7a6c920cbb02427
Daria Kolodzey <kolodaria@google.com>
2014-09-17 21:38:27 +0400

    added simple javadoc comment

--- src/ru/fizteh/fivt/students/Oktosha/Shell/Shell.java
+++ src/ru/fizteh/fivt/students/Oktosha/Shell/Shell.java
@@ -1,5 +1,9 @@
 package ru.fizteh.fivt.students.Oktosha.Shell;
 
+/**
+ * The Shell class implements an application which partially emulates UNIX command line
+ */
+
 public class Shell
 {
     public static void main(String args[]) {
