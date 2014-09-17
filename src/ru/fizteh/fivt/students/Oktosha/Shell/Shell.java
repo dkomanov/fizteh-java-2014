dfeb9c14336b239d345969fde4148b467d1fb4a2
Daria Kolodzey <kolodaria@google.com>
2014-09-17 22:32:28 +0400

    Managed to write a source that checkstyle accepts.

--- src/ru/fizteh/fivt/students/Oktosha/Shell/Shell.java
+++ src/ru/fizteh/fivt/students/Oktosha/Shell/Shell.java
@@ -1,12 +1,24 @@
 package ru.fizteh.fivt.students.Oktosha.Shell;
 
 /**
- * The Shell class implements an application which partially emulates UNIX command line
+ * The Shell class implements an application,
+ * which partially emulates UNIX command line.
  */
 
-public class Shell
-{
-    public static void main(String args[]) {
+public final class Shell {
+
+    /**
+     * This is a private constructor, which is never called.
+     */
+    private Shell() { }
+
+    /**
+     *
+     * @param args are treated as shell commands
+     *             if no arguments are given
+     *             Shell runs in interactive mode
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello, world!");
     }
 }
