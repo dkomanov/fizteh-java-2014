c34876984c7d21ceda923f6331db5e93eefb2922
ZatsepinMikhail <salamence1996@gmail.com>
2014-09-18 19:44:11 +0400

    Second attempt of deleting

---
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/shell/ShellMain.java
@@ -0,0 +1,12 @@
+package ru.fizteh.fivt.students.ZatsepinMikhail.shell;
+
+/**
+ * @author test
+ */
+public class ShellMain {
+
+    public static void main(String[] args) {
+        System.out.println("not working");
+        System.exit(1);
+    }
+}
