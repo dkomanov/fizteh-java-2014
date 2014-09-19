1b21e5920d178d59fd54b6e0d64579faff70481c
shakarim <shakarim@shakarim-1005P>
2014-09-20 03:54:20 +0400

    version 5

--- src/ru/fizteh/fivt/students/kalandarovshakarim/shell/Shell.java
+++ src/ru/fizteh/fivt/students/kalandarovshakarim/shell/Shell.java
@@ -3,35 +3,38 @@
  * and open the template in the editor.
  */
 package ru.fizteh.fivt.students.kalandarovshakarim.shell;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 /**
  *
  * @author shakarim
  */
 public class Shell {
-    
+
     private static String curPath() {
         return System.getProperty("user.dir");
     }
-    
+
     public static void pwd() {
         System.out.println(curPath());
     }
-    
+
     public static void ls() {
         File curDir = new File(curPath());
         String[] list = curDir.list();
-        
+
         for (String name : list) {
-            if (name.charAt(0) != '.')
+            if (name.charAt(0) != '.') {
                 System.out.println(name);
+            }
         }
     }
-    
+
     public static void mkdir(File dir) {
         boolean isCreated = false;
         try {
@@ -40,10 +43,10 @@ public class Shell {
         }
         if (isCreated == false) {
             System.out.println("mkdir: Cannot create directory '"
-                    + dir.getName() +"'");
+                    + dir.getName() + "'");
         }
     }
-    
+
     public static void cd(File dirToGo) {
 
         if (dirToGo.isDirectory() == true) {
@@ -54,11 +57,11 @@ public class Shell {
             } catch (Exception e) {
             }
         } else {
-            System.out.println("cd: '" 
+            System.out.println("cd: '"
                     + dirToGo.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static void readWrite(InputStream in, OutputStream out) {
         int lengthRead;
         byte[] buffer = new byte[4096];
@@ -69,52 +72,52 @@ public class Shell {
         } catch (Exception e) {
         }
     }
-    
+
     public static void cat(File file) {
         if (file.exists() == true) {
             InputStream in;
             try {
                 in = new FileInputStream(file);
                 readWrite(in, System.out);
-            } catch(Exception e) {
+            } catch (Exception e) {
             }
         } else {
-            System.out.println("cat: '" 
+            System.out.println("cat: '"
                     + file.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static void copyFile(File source, File target) {
         if (source.exists() == true) {
             try {
                 InputStream in = new FileInputStream(source);
                 OutputStream out = new FileOutputStream(target);
                 readWrite(in, out);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("cp: Cannot create File '"
                         + target.getName() + "'");
             }
         } else {
-            System.out.println("cp: '" 
+            System.out.println("cp: '"
                     + source.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static void copyDirectory(File source, File target) {
         if (target.mkdir() == true) {
             String[] files = source.list();
-        
+
             for (String file : files) {
                 cp(true, new File(source, file), new File(target, file));
             }
-        } else  if (target.exists() == true) {
+        } else if (target.exists() == true) {
             copyDirectory(source, new File(target, source.getName()));
         } else {
             System.out.println("cp: Cannot create Directory '"
                     + target.getName() + "' No such File or Directory");
         }
     }
-    
+
     public static void cp(boolean rec, File source, File target) {
         if (source.isDirectory() == true) {
             if (target.isFile() == true) {
@@ -124,7 +127,7 @@ public class Shell {
             } else if (rec == true) {
                 copyDirectory(source, target);
             } else {
-                System.out.println("cp: '" 
+                System.out.println("cp: '"
                         + source.getName() + "' is a Directory");
             }
         } else if (source.isFile() == true && target.isDirectory() == true) {
@@ -132,11 +135,11 @@ public class Shell {
         } else if (source.exists() == true) {
             copyFile(source, target);
         } else {
-            System.out.println("cp: '" 
-                        + source.getName() + "' No such File or Directory");
+            System.out.println("cp: '"
+                    + source.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static void rm(boolean rec, File file) {
         if (file.isFile() == true) {
             file.delete();
@@ -147,42 +150,42 @@ public class Shell {
             }
             file.delete();
         } else if (rec == false && file.isDirectory() == true) {
-            System.out.println("rm: '" 
+            System.out.println("rm: '"
                     + file.getName() + "' is Directory");
         } else {
-            System.out.println("rm: '" 
+            System.out.println("rm: '"
                     + file.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static void mv(File source, File target) {
         if (source.exists() == true) {
             cp(true, source, target);
             rm(true, source);
         } else {
-            System.out.println("mv: '" 
+            System.out.println("mv: '"
                     + source.getName() + "' No such File or Directory");
         }
     }
-    
+
     private static String readStr(InputStream in) {
         byte[] buff = new byte[4096];
         int length = 0;
-        
+
         try {
             length = System.in.read(buff);
         } catch (Exception e) {
         }
-        
+
         String retVal = new String(buff, 0, length - 1);
 
         return retVal;
     }
-    
-    private static String[] parseCmd(String command){
+
+    private static String[] parseCmd(String command) {
         String[] retVal = new String[4];
         int j = 0;
-        
+
         for (int i = 0; i < 4; ++i) {
             StringBuilder cmd = new StringBuilder();
             while (j < command.length() && command.charAt(j) != ' ') {
@@ -190,17 +193,17 @@ public class Shell {
                 ++j;
             }
             while (j < command.length() && command.charAt(j) == ' ') {
-               ++j;
+                ++j;
             }
             retVal[i] = cmd.toString();
         }
-        
+
         return retVal;
     }
-    
+
     private static boolean caseCmd(String[] command) {
         File[] files = new File[3];
-        
+
         for (int i = 0; i < 3; ++i) {
             if (command[i + 1].length() > 0) {
                 if (command[i + 1].charAt(0) == '/') {
@@ -210,7 +213,7 @@ public class Shell {
                 }
             }
         }
-        
+
         if (command[0].equals("exit") == true) {
             return false;
         } else if (command[0].equals("ls") == true) {
@@ -226,14 +229,13 @@ public class Shell {
         } else if (command[0].equals("rm") == true) {
             if (command[1].equals("-r") == true) {
                 rm(true, files[1]);
-            }
-            else { 
+            } else {
                 rm(false, files[0]);
             }
         } else if (command[0].equals("cp") == true) {
             if (command[1].equals("-r") == true) {
                 cp(true, files[1], files[2]);
-            } else { 
+            } else {
                 cp(false, files[0], files[1]);
             }
         } else if (command[0].equals("mv") == true) {
@@ -241,10 +243,10 @@ public class Shell {
         } else if (command[0].length() != 0) {
             System.out.println("Shell: '" + command[0] + "' Unknown command");
         }
-        
+
         return true;
     }
-    
+
     public static void procShell() {
         String[] parsed;
         String command;
@@ -254,7 +256,7 @@ public class Shell {
             parsed = parseCmd(command);
         } while (caseCmd(parsed) == true);
     }
-    
+
     public static void procArgs(String[] args) {
         StringBuilder cmd = new StringBuilder();
         String[] parsed;
