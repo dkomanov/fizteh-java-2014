7aee20ce197009a88715f5610bc992d4744e8e38
AndreosGhost <andreos-ghost@ya.ru>
2014-09-20 17:28:03 +0400

    Shell v3

--- src/ru/fizteh/fivt/students/AndrewFedorov/shell/Commands.java
+++ src/ru/fizteh/fivt/students/AndrewFedorov/shell/Commands.java
@@ -2,15 +2,11 @@ package ru.fizteh.fivt.students.AndrewFedorov.shell;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.DirectoryStream;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Iterator;
 
 /**
@@ -26,56 +22,22 @@ import java.util.Iterator;
  * 
  */
 public class Commands {
-    /**
-     * Handles an occured exception.
-     * 
-     * @param exc
-     *            occured exception. If null, an {@link Exception} is
-     *            constructed via {@link Exception#Exception(String)}.
-     * @param message
-     *            message that can be reported to user and is written to log.
-     * @param reportToUser
-     *            if true, message is printed to {@link System#err}.
-     */
-    private static void handleError(Throwable exc, String message,
-	    boolean reportToUser) {
-	if (reportToUser) {
-	    System.err.println(message);
-	}
-	Log.log(Commands.class, exc, message);
-	if (exc == null) {
-	    exc = new Exception(message);
-	}
-	throw new HandledException(exc);
-    }
-
-    /**
-     * Handles an occured exception.<br/>
-     * Equivalent to handleError(null, message, true);
-     * 
-     * @param message
-     *            message that can be reported to user and is written to log.
-     */
-    private static void handleError(String message) {
-	handleError(null, message, true);
-    }
-
     @ShellCommand(description = "\tmkdir <dir>\tcreates a new directory")
     public static void mkdir(Shell shell, String[] args) {
 	if (args.length != 2) {
-	    handleError("method 'mkdir' requires only one parameter: directory");
+	    Utility.handleError("method 'mkdir' requires only one parameter: directory");
 	}
 
 	Path path = shell.getWorkingDirectory().resolve(args[1]).normalize();
 
 	if (Files.exists(path)) {
-	    handleError(String.format(
+	    Utility.handleError(String.format(
 		    "mkdir: %s: file or directory already exists", args[1]));
 	} else {
 	    try {
 		Files.createDirectory(path);
 	    } catch (Exception exc) {
-		handleError(exc, String.format(
+		Utility.handleError(exc, String.format(
 			"mkdir: directory '%s' not created", path), false);
 	    }
 	}
@@ -111,13 +73,13 @@ public class Commands {
 		} else if (dstName == null) {
 		    dstName = Paths.get(args[i]);
 		} else {
-		    handleError("Extra arguments given to cp; Invocation: cp [-r] <src> <dst>");
+		    Utility.handleError("Extra arguments given to cp; Invocation: cp [-r] <src> <dst>");
 		}
 	    }
 	}
 
 	if (srcName == null || dstName == null) {
-	    handleError((srcName == null ? "Source" : "Destination")
+	    Utility.handleError((srcName == null ? "Source" : "Destination")
 		    + " path not given");
 	}
 
@@ -127,13 +89,13 @@ public class Commands {
 		.normalize();
 
 	if (!Files.exists(sourcePath)) {
-	    handleError(String.format("%s: %s: No such file or directory",
+	    Utility.handleError(String.format("%s: %s: No such file or directory",
 		    args[0], srcName));
 	}
 
 	if (sourcePath.startsWith(targetPath)
 		|| targetPath.startsWith(sourcePath)) {
-	    handleError(String
+	    Utility.handleError(String
 		    .format("%s: Cannot %s '%s' to '%s', because one is prefix of the other",
 			    args[0], "cp".equals(args[0]) ? "copy" : "move",
 			    srcName, dstName));
@@ -142,66 +104,11 @@ public class Commands {
 	try {
 	    if (Files.isDirectory(sourcePath)) {
 		if (recursive) {
-		    Files.walkFileTree(sourcePath, new FileVisitor<Path>() {
-			@Override
-			public FileVisitResult postVisitDirectory(Path path,
-				IOException exc) throws IOException {
-			    if (exc == null) {
-				return FileVisitResult.CONTINUE;
-			    } else {
-				return visitFileFailed(path, exc);
-			    }
-			}
-
-			@Override
-			public FileVisitResult preVisitDirectory(
-				Path visitPath, BasicFileAttributes attrs)
-				throws IOException {
-			    try {
-				Path relativePath = sourcePath
-					.relativize(visitPath);
-				Path destDir = targetPath.resolve(relativePath)
-					.normalize();
-				Log.log(Commands.class, String.format(
-					"\tCopy dir '%s' -> '%s'", visitPath,
-					destDir));
-				if (!Files.exists(destDir)) {
-				    Files.createDirectory(destDir);
-				}
-			    } catch (IOException exc) {
-				visitFileFailed(visitPath, exc);
-			    }
-			    return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult visitFile(Path visitPath,
-				BasicFileAttributes attrs) throws IOException {
-			    try {
-				Path rel = sourcePath.relativize(visitPath);
-				Path destPath = targetPath.resolve(rel)
-					.normalize();
-				Log.log(Commands.class, String.format(
-					"\tCopy file '%s' -> '%s'", visitPath,
-					destPath));
-				Files.copy(visitPath, destPath,
-					StandardCopyOption.REPLACE_EXISTING);
-
-				return FileVisitResult.CONTINUE;
-			    } catch (IOException exc) {
-				return visitFileFailed(visitPath, exc);
-			    }
-			}
-
-			@Override
-			public FileVisitResult visitFileFailed(Path visitPath,
-				IOException exc) throws IOException {
-			    throw exc;
-			}
-		    });
+		    Files.walkFileTree(sourcePath, new Utility.FileTreeCopier(
+			    sourcePath, targetPath));
 		} else {
 		    // cannot occur when moving
-		    handleError(String.format(
+		    Utility.handleError(String.format(
 			    "cp: %s is a directory (not copied).", srcName));
 		}
 	    } else {
@@ -211,7 +118,7 @@ public class Commands {
 	} catch (HandledException exc) {
 	    throw exc;
 	} catch (Exception exc) {
-	    handleError(exc, String.format("%s: cannot %s '%s' to '%s'",
+	    Utility.handleError(exc, String.format("%s: cannot %s '%s' to '%s'",
 		    args[0], "cp".equals(args[0]) ? "copy" : "move", srcName,
 		    dstName), true);
 	}
@@ -220,7 +127,7 @@ public class Commands {
     @ShellCommand(description = "\tls\tprints current directory contents")
     public static void ls(Shell shell, String[] args) {
 	if (args.length > 1) {
-	    handleError("method 'ls' requires no arguments");
+	    Utility.handleError("method 'ls' requires no arguments");
 	}
 
 	Path home = shell.getWorkingDirectory();
@@ -232,7 +139,7 @@ public class Commands {
 		System.out.println(home.relativize(path));
 	    }
 	} catch (Exception exc) {
-	    handleError(exc, "Could not list directory " + home, false);
+	    Utility.handleError(exc, "Could not list directory " + home, false);
 	}
     }
 
@@ -246,12 +153,12 @@ public class Commands {
 	    } else if (dst == null) {
 		dst = args[i];
 	    } else {
-		handleError("mv: too many parameters given");
+		Utility.handleError("mv: too many parameters given");
 	    }
 	}
 
 	cp(shell, new String[] { "mv", "-r", src, dst });
-	rm(shell, new String[] { "rm", "-r", src });
+	rm(shell, new String[] { "mv", "-r", src });
 
     }
 
@@ -266,12 +173,12 @@ public class Commands {
 	    } else if (removeName == null) {
 		removeName = Paths.get(args[i]);
 	    } else {
-		handleError("Extra arguments given to rm; Invocation: rm [-r] <path>");
+		Utility.handleError("Extra arguments given to rm; Invocation: rm [-r] <path>");
 	    }
 	}
 
 	if (removeName == null) {
-	    handleError("rm: path not given");
+	    Utility.handleError("rm: path not given");
 	}
 
 	final Path removePath = shell.getWorkingDirectory().resolve(removeName)
@@ -280,51 +187,9 @@ public class Commands {
 	try {
 	    if (Files.isDirectory(removePath)) {
 		if (recursive) {
-		    Files.walkFileTree(removePath, new FileVisitor<Path>() {
-			@Override
-			public FileVisitResult postVisitDirectory(Path path,
-				IOException exc) throws IOException {
-			    if (exc != null) {
-				return visitFileFailed(path, exc);
-			    }
-			    try {
-				Files.delete(path);
-			    } catch (IOException ex) {
-				return visitFileFailed(path, ex);
-			    }
-			    return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult preVisitDirectory(
-				Path visitPath, BasicFileAttributes arg1)
-				throws IOException {
-			    return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult visitFile(Path visitPath,
-				BasicFileAttributes arg1) throws IOException {
-			    try {
-				Files.delete(visitPath);
-				return FileVisitResult.CONTINUE;
-			    } catch (IOException exc) {
-				return visitFileFailed(visitPath, exc);
-			    }
-			}
-
-			@Override
-			public FileVisitResult visitFileFailed(Path visitPath,
-				IOException exc) throws IOException {
-			    if (exc == null) {
-				return FileVisitResult.CONTINUE;
-			    } else {
-				throw exc;
-			    }
-			}
-		    });
+		    Files.walkFileTree(removePath, new Utility.FileTreeRemover());
 		} else {
-		    handleError(String.format("rm: %s: is a directory",
+		    Utility.handleError(String.format("rm: %s: is a directory",
 			    removeName));
 		}
 	    } else {
@@ -333,23 +198,23 @@ public class Commands {
 	} catch (HandledException exc) {
 	    throw exc;
 	} catch (Exception exc) {
-	    handleError(exc, String.format(
-		    "rm: cannot remove '%s': No such file or directory",
-		    removeName), true);
+	    Utility.handleError(exc, String.format(
+		    "%s: cannot remove '%s': No such file or directory",
+		    args[0], removeName), true);
 	}
     }
 
     @ShellCommand(description = "\tcat\toutputs file contents")
     public static void cat(Shell shell, String[] args) {
 	if (args.length != 2) {
-	    handleError("Invalid arguments count; Invocation: cat <path>");
+	    Utility.handleError("Invalid arguments count; Invocation: cat <path>");
 	}
 
 	Path catName = Paths.get(args[1]);
 	Path catPath = shell.getWorkingDirectory().resolve(catName).normalize();
 
 	if (Files.isDirectory(catPath)) {
-	    handleError(String.format("cat: %s: Is a directory", catName));
+	    Utility.handleError(String.format("cat: %s: Is a directory", catName));
 	} else {
 	    try (BufferedReader reader = new BufferedReader(new FileReader(
 		    catPath.toString()), shell.getReadBufferSize())) {
@@ -358,7 +223,7 @@ public class Commands {
 		    System.out.println(s);
 		}
 	    } catch (Exception exc) {
-		handleError(String.format("cat: %s: No such file or directory",
+		Utility.handleError(String.format("cat: %s: No such file or directory",
 			catName));
 	    }
 	}
@@ -367,7 +232,7 @@ public class Commands {
     @ShellCommand(description = "\texit\tcloses this shell")
     public static void exit(Shell shell, String[] args) {
 	if (args.length != 1) {
-	    handleError("Invalid arguments count; Invocation: exit");
+	    Utility.handleError("Invalid arguments count; Invocation: exit");
 	}
 
 	Log.log("Shell exitting");
@@ -378,7 +243,7 @@ public class Commands {
     @ShellCommand(description = "\tpwd\tprints working directory")
     public static void pwd(Shell shell, String[] args) {
 	if (args.length != 1) {
-	    handleError("Invalid arguments count; Invocation: pwd");
+	    Utility.handleError("Invalid arguments count; Invocation: pwd");
 	}
 	System.out.println(shell.getWorkingDirectory());
     }
@@ -386,7 +251,7 @@ public class Commands {
     @ShellCommand(description = "\tcd <dir>\tchange working directory")
     public static void cd(Shell shell, String[] args) {
 	if (args.length != 2) {
-	    handleError("Invalid arguments count; Invocation: cd <dir>");
+	    Utility.handleError("Invalid arguments count; Invocation: cd <dir>");
 	}
 	Path dir = shell.getWorkingDirectory();
 	Path newdir = dir.resolve(args[1]).normalize();
@@ -395,9 +260,9 @@ public class Commands {
 	    shell.setWorkingDirectory(newdir);
 	    Log.log(Commands.class, "Set working dir: " + newdir);
 	} else if (Files.exists(newdir)) {
-	    handleError(String.format("cd: %s: Is not a directory", newdir));
+	    Utility.handleError(String.format("cd: %s: Is not a directory", newdir));
 	} else {
-	    handleError(String.format("cd: %s: No such file or directory",
+	    Utility.handleError(String.format("cd: %s: No such file or directory",
 		    newdir));
 	}
     }
--- src/ru/fizteh/fivt/students/AndrewFedorov/shell/Shell.java
+++ src/ru/fizteh/fivt/students/AndrewFedorov/shell/Shell.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -141,6 +140,12 @@ public class Shell {
 			getWorkingDirectory()));
 
 		String str = reader.readLine();
+
+		// end of stream
+		if (str == null) {
+		    break;
+		}
+
 		String[] commands = str.split(";");
 		for (int i = 0, len = commands.length; i < len; i++) {
 		    boolean correct = execute(commands[i]);
