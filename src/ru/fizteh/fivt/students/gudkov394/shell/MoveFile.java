959f26e8aa0cca2c910bebf6cb424fd327cdd514
kagudkov <kagudkov@gmail.com>
2014-09-21 19:55:48 +0400

    Bugs fixed

--- src/ru/fizteh/fivt/students/gudkov394/shell/MoveFile.java
+++ src/ru/fizteh/fivt/students/gudkov394/shell/MoveFile.java
@@ -1,18 +1,42 @@
 package ru.fizteh.fivt.students.gudkov394.shell;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+/*работает*/
+
 public class MoveFile
 {
     public MoveFile(String[] current_args, CurrentDirectory cd)
     {
-        if(current_args.length > 2)
+        if(current_args.length > 3)
         {
             System.err.println("extra arguments for mv");
             System.exit(1);
         }
-        Copy copy = new Copy(current_args, cd);
-        String[] args_for_rm = new String[2];
-        args_for_rm[0] = "rm";
-        args_for_rm[1] = current_args[1];
-        Remove_derictory rm = new Remove_derictory(args_for_rm, cd);
+        CopyOption[] options = new CopyOption[]{ StandardCopyOption.REPLACE_EXISTING};
+        File from = new File(current_args[1]);
+        File to = new File(current_args[2]);
+        if(!from.isAbsolute())
+        {
+            from = new File(cd.get_Current_directory(), current_args[1]);
+        }
+        if(!to.isAbsolute())
+        {
+            to = new File(cd.get_Current_directory(), current_args[2]);
+        }
+        to = new File(to.getAbsolutePath(), from.getName());
+        try
+        {
+            Files.move(from.toPath(), to.toPath(), options);
+        }
+        catch (IOException e)
+        {
+            System.err.println("problem with move");
+            System.exit(4);
+        }
     }
 }
