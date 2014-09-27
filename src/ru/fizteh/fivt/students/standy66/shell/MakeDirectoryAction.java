4496b09de839e135688b5f0227343889ed491ca9
Andrew Stepanov <karn9050@gmail.com>
2014-09-27 22:40:23 +0400

    Revert to initial and added Shell

---
+++ src/ru/fizteh/fivt/students/standy66/shell/MakeDirectoryAction.java
@@ -0,0 +1,24 @@
+package ru.fizteh.fivt.students.standy66.shell;
+
+import java.io.File;
+
+/**
+ * Created by astepanov on 20.09.14.
+ */
+public class MakeDirectoryAction extends Action {
+
+	public MakeDirectoryAction(String[] args) {
+		super(args);
+	}
+
+	@Override
+	public boolean run() {
+		String name = arguments[1];
+		File f = FileUtils.fromPath(name);
+		if (!f.mkdirs()) {
+			System.err.printf("mkdir: '%s': failed to create directory\n", name);
+			return false;
+		}
+		return true;
+	}
+}
