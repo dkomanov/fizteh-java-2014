19e26caca9bba9a1b9f68c81394e597ad1b842ff
Norrius <norrius@gmail.com>
2014-09-29 01:47:46 +0400

    Renamed package for the new task.

--- src/ru/fizteh/fivt/students/andreyzakharov/multifilehashmap/Command.java
+++ src/ru/fizteh/fivt/students/andreyzakharov/multifilehashmap/Command.java
@@ -1,4 +1,4 @@
-package ru.fizteh.fivt.students.andreyzakharov.filemap;
+package ru.fizteh.fivt.students.andreyzakharov.multifilehashmap;
 
 public interface Command {
     String execute(DbConnector connector, String... args) throws CommandInterruptException;
--- src/ru/fizteh/fivt/students/andreyzakharov/multifilehashmap/FileMap.java
+++ src/ru/fizteh/fivt/students/andreyzakharov/multifilehashmap/FileMap.java
@@ -1,4 +1,4 @@
-package ru.fizteh.fivt.students.andreyzakharov.filemap;
+package ru.fizteh.fivt.students.andreyzakharov.multifilehashmap;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
