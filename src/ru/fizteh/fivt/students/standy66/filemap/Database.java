89e6cdedc72ef8d36d9fe25f9181f1724456527e
Andrew Stepanov <karn9050@gmail.com>
2014-09-28 15:27:46 +0400

    Added FileMap

---
+++ src/ru/fizteh/fivt/students/standy66/filemap/Database.java
@@ -0,0 +1,96 @@
+package ru.fizteh.fivt.students.standy66.filemap;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/** Simple database implementation. <br>
+ * Working mechanism is specified at {@link ru.fizteh.fivt.students.standy66.filemap} <br>
+ * Created by astepanov on 26.09.14.
+ */
+public class Database {
+
+	private String dbFilePath;
+	private Map<String, String> cache;
+
+	public Database(@NotNull final String databaseFilePath) {
+		dbFilePath = databaseFilePath;
+		cache = new HashMap<>();
+		reload();
+	}
+
+	private void reload() {
+		FileChannel channel = null;
+		try {
+			channel = new FileInputStream(dbFilePath).getChannel();
+			ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+			try {
+				while (buffer.remaining() > 0) {
+					int keySize = buffer.getInt();
+					byte[] key = new byte[keySize];
+					buffer.get(key);
+					int valueSize = buffer.getInt();
+					byte[] value = new byte[valueSize];
+					buffer.get(value);
+					cache.put(new String(key, "UTF-8"), new String(value, "UTF-8"));
+				}
+			} catch (BufferUnderflowException e) {
+				System.err.println("Corrupted file");
+				System.exit(1);
+			}
+			channel.close();
+		} catch (IOException e) {
+			System.err.println("Error reading file");
+			System.exit(2);
+		}
+	}
+
+	public Set<String> list() {
+		return cache.keySet();
+	}
+
+	public String get(@NotNull final String key) {
+		return cache.get(key);
+	}
+
+	public void put(@NotNull final String key, @NotNull final String value) {
+		cache.put(key, value);
+	}
+
+	public boolean remove(@NotNull final String key) {
+		if (cache.containsKey(key)) {
+			cache.remove(key);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void flush() {
+		try {
+			FileOutputStream fos = new FileOutputStream(dbFilePath, false);
+			ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+			for (String key : cache.keySet()) {
+				String value = cache.get(key);
+				byte[] keyEncoded = key.getBytes("UTF-8");
+				fos.write(lengthBuffer.putInt(0, keyEncoded.length).array());
+				fos.write(keyEncoded);
+				byte[] valueEncoded = value.getBytes("UTF-8");
+				fos.write(lengthBuffer.putInt(0, valueEncoded.length).array());
+				fos.write(valueEncoded);
+			}
+			fos.close();
+		} catch (IOException e) {
+			System.err.println("Error writing file");
+			System.exit(4);
+		}
+	}
+}
---
+++ src/ru/fizteh/fivt/students/standy66/filemap/FileMapShell.java
@@ -0,0 +1,119 @@
+package ru.fizteh.fivt.students.standy66.filemap;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/** Shell designed to interpret user commands. <br>
+ * Created by astepanov on 26.09.14.
+ */
+public class FileMapShell {
+
+	private Scanner scanner;
+	private boolean interactive;
+	private Database db;
+
+	public FileMapShell(@NotNull final InputStream inputStream, final boolean isInteractive) {
+		scanner = new Scanner(inputStream);
+		interactive = isInteractive;
+		String dbFile = System.getProperty("db.file");
+		if (dbFile == null) {
+			System.err.println("No file specified");
+			System.exit(5);
+		}
+		db = new Database(dbFile);
+	}
+
+	public void run() {
+		while (true) {
+			if (interactive) {
+				System.out.print("$ ");
+				System.out.flush();
+			}
+			String line;
+			try {
+				line = scanner.nextLine();
+			} catch (NoSuchElementException e) {
+				db.flush();
+				return;
+			}
+			String[] actions = line.split(";");
+
+			for (int i = 0; i < actions.length; i++) {
+				String[] args =
+						Stream.of(actions[i].split(" "))
+								.filter((s) -> (s.length() > 0))
+								.toArray((size) -> (new String[size]));
+				if (args.length == 0) {
+					continue;
+				}
+				switch (args[0]) {
+					case "put":
+						if (args.length != 3) {
+							System.err.println("Invalid number of arguments");
+							continue;
+						}
+						if (db.get(args[1]) == null) {
+							System.out.println("new");
+						} else {
+							System.out.println("overwrite");
+							System.out.println(db.get(args[1]));
+						}
+						db.put(args[1], args[2]);
+						break;
+					case "get":
+						if (args.length != 2) {
+							System.err.println("Invalid number of arguments");
+							continue;
+						}
+						if (db.get(args[1]) == null) {
+							System.out.println("not found");
+						} else {
+							System.out.println("found");
+							System.out.println(db.get(args[1]));
+						}
+						break;
+
+					case "remove":
+						if (args.length != 2) {
+							System.err.println("Invalid number of arguments");
+							continue;
+						}
+						if (db.get(args[1]) == null) {
+							System.out.println("not found");
+						} else {
+							System.out.println("removed");
+							db.remove(args[1]);
+						}
+						break;
+
+					case "list":
+						if (args.length != 1) {
+							System.err.println("Invalid number of arguments");
+							continue;
+						}
+						System.out.println(
+								Stream.of(db.list().toArray(new String[0]))
+										.collect(Collectors.joining(", ")));
+						break;
+					case "exit":
+						if (args.length != 1) {
+							System.err.println("Invalid number of arguments");
+							continue;
+						}
+						db.flush();
+						System.exit(0);
+						break;
+
+					default:
+						System.err.println("Unknown command");
+				}
+			}
+		}
+
+	}
+}
