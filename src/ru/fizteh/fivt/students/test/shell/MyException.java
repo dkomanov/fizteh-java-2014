b97afba08d520d397c55094ce76f5e5b67261229
Serg Popov <sergpopov95@gmail.com>
2014-09-29 18:22:30 +0400

    Commit #1

---
+++ src/ru/fizteh/fivt/students/test/shell/MyException.java
@@ -0,0 +1,12 @@
+package com.Shell.commads;
+
+/**
+ * Created by deserg on 22.09.14.
+ */
+public class MyException extends RuntimeException{
+
+    public MyException(String msg) {
+        super(msg);
+    }
+
+}
