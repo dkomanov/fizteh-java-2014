8a5da3c5f49c2ee9e4f290b6c4eb8f4bf67c5a22
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-09-30 04:47:34 +0400

    Enhanced command execution exception messages

--- src/ru/fizteh/fivt/students/moskupols/cliutils/CommandExecutionException.java
+++ src/ru/fizteh/fivt/students/moskupols/cliutils/CommandExecutionException.java
@@ -4,7 +4,7 @@ package ru.fizteh.fivt.students.moskupols.cliutils;
  * Created by moskupols on 28.09.14.
  */
 public class CommandExecutionException extends Exception {
-    public CommandExecutionException(String s) {
-        super(s);
+    public CommandExecutionException(Command cmd, String s) {
+        super(cmd.name() + ": " + s);
     }
 }
