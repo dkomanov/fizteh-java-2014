5e236fda3a5a0d5548c5ad0541920bd217bac9e0
rikachka <rika.95@mail.ru>
2014-10-01 04:19:42 +0400

    Create RmCommand.java

---
+++ src/ru/fizteh/fivt/students/irina_karatsapova/shell/commands/RmCommand.java
@@ -0,0 +1,41 @@
+package ru.fizteh.fivt.students.irina_karatsapova.shell.commands;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RmCommand implements Command {
+	public void execute(String[] args) throws IOException, Exception {
+		if (!args[1].equals("-r")) {
+			File removed = Utils.makePathAbsolute(args[1]).toFile();
+			Utils.checkExistance(removed);
+			if ((removed.isFile()) || ((removed.isDirectory()) && (removed.list().length == 0))) {
+				Utils.delete(removed);
+			}
+		} else {
+			File removed = Utils.makePathAbsolute(args[2]).toFile();
+			Utils.checkExistance(removed);
+			recursiveRemove(removed);
+		}
+	}
+	
+	public void recursiveRemove(File removed) throws Exception {
+		if (removed.isDirectory()) {
+			for (File object: removed.listFiles()) {
+				recursiveRemove(object);
+			}
+		}
+		Utils.delete(removed);
+	}
+	
+	public String name() {
+		return "rm";
+	}
+	
+	public int minArgs() {
+		return 2;
+	}
+	
+	public int maxArgs() {
+		return 3;
+	}
+}
