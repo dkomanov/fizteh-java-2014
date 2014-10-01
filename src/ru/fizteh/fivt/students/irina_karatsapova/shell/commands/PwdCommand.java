c1e787773b29ffdd0114c7021e337eca02f15b80
rikachka <rika.95@mail.ru>
2014-10-01 04:19:21 +0400

    Create PwdCommand.java

---
+++ src/ru/fizteh/fivt/students/irina_karatsapova/shell/commands/PwdCommand.java
@@ -0,0 +1,19 @@
+package ru.fizteh.fivt.students.irina_karatsapova.shell.commands;
+
+public class PwdCommand implements Command {
+	public void execute(String[] args) {
+		System.out.println(Utils.currentPath());
+	}
+	
+	public String name() {
+		return "pwd";
+	}
+	
+	public int minArgs() {
+		return 1;
+	}
+	
+	public int maxArgs() {
+		return 1;
+	}
+}
