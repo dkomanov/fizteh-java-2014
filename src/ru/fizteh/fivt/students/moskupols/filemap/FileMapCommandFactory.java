436bcea74f2038c39f45214aec3aaf7b0d977fa9
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-10-02 10:49:38 +0400

    Replaced many ifs with one switch

--- src/ru/fizteh/fivt/students/moskupols/filemap/FileMapCommandFactory.java
+++ src/ru/fizteh/fivt/students/moskupols/filemap/FileMapCommandFactory.java
@@ -16,100 +16,98 @@ public class FileMapCommandFactory implements CommandFactory {
     public Command fromString(String s) throws UnknownCommandException {
         final String[] argv = s.trim().split("\\s+");
 
-        if ("put".equals(argv[0])) {
-            return new Command() {
-                @Override
-                public void execute() throws CommandExecutionException, StopProcessingException {
-                    if (argv.length != 3) {
-                        throw new CommandExecutionException(this, "Unexpected number of arguments");
+        switch (argv[0]) {
+            case "put":
+                return new Command() {
+                    @Override
+                    public void execute() throws CommandExecutionException, StopProcessingException {
+                        if (argv.length != 3) {
+                            throw new CommandExecutionException(this, "Unexpected number of arguments");
+                        }
+                        String old = db.map.put(argv[1], argv[2]);
+                        if (null == old) {
+                            System.out.println("new");
+                        } else {
+                            System.out.println("overwrite");
+                            System.out.println(old);
+                        }
                     }
-                    String old = db.map.put(argv[1], argv[2]);
-                    if (null == old) {
-                        System.out.println("new");
-                    } else {
-                        System.out.println("overwrite");
-                        System.out.println(old);
-                    }
-                }
 
-                @Override
-                public String name() {
-                    return "put";
-                }
-            };
+                    @Override
+                    public String name() {
+                        return "put";
+                    }
+                };
+
+            case "get":
+                return new Command() {
+                    @Override
+                    public void execute() throws CommandExecutionException, StopProcessingException {
+                        if (argv.length != 2) {
+                            throw new CommandExecutionException(this, "Unexpected number of arguments");
+                        }
+                        String value = db.map.get(argv[1]);
+                        System.out.println(value == null ? "not found" : value);
+                    }
+
+                    @Override
+                    public String name() {
+                        return "get";
+                    }
+                };
+
+            case "remove":
+                return new Command() {
+                    @Override
+                    public void execute() throws CommandExecutionException, StopProcessingException {
+                        if (argv.length != 2) {
+                            throw new CommandExecutionException(this, "Unexpected number of arguments");
+                        }
+                        System.out.println(db.map.remove(argv[1]) == null ? "not found" : "removed");
+                    }
+
+                    @Override
+                    public String name() {
+                        return "remove";
+                    }
+                };
+
+            case "list":
+                return new Command() {
+                    @Override
+                    public void execute() throws CommandExecutionException, StopProcessingException {
+                        if (argv.length != 1) {
+                            throw new CommandExecutionException(this, "Unexpected number of arguments");
+                        }
+                        for (String k : db.map.keySet()) {
+                            System.out.println(k);
+                        }
+                    }
+
+                    @Override
+                    public String name() {
+                        return "list";
+                    }
+                };
+
+            case "exit":
+                return new Command() {
+                    @Override
+                    public void execute() throws CommandExecutionException, StopProcessingException {
+                        if (argv.length != 1) {
+                            throw new CommandExecutionException(this, "Unexpected number of arguments");
+                        }
+                        throw new StopProcessingException();
+                    }
+
+                    @Override
+                    public String name() {
+                        return "exit";
+                    }
+                };
+
+            default:
+                throw new UnknownCommandException(argv[0]);
         }
-
-        if ("get".equals(argv[0])) {
-            return new Command() {
-                @Override
-                public void execute() throws CommandExecutionException, StopProcessingException {
-                    if (argv.length != 2) {
-                        throw new CommandExecutionException(this, "Unexpected number of arguments");
-                    }
-                    String value = db.map.get(argv[1]);
-                    System.out.println(value == null ? "not found" : value);
-                }
-
-                @Override
-                public String name() {
-                    return "get";
-                }
-            };
-        }
-
-        if ("remove".equals(argv[0])) {
-            return new Command() {
-                @Override
-                public void execute() throws CommandExecutionException, StopProcessingException {
-                    if (argv.length != 2) {
-                        throw new CommandExecutionException(this, "Unexpected number of arguments");
-                    }
-                    System.out.println(db.map.remove(argv[1]) == null ? "not found" : "removed");
-                }
-
-                @Override
-                public String name() {
-                    return "remove";
-                }
-            };
-        }
-
-        if ("list".equals(argv[0])) {
-            return new Command() {
-                @Override
-                public void execute() throws CommandExecutionException, StopProcessingException {
-                    if (argv.length != 1) {
-                        throw new CommandExecutionException(this, "Unexpected number of arguments");
-                    }
-                    for (String k : db.map.keySet()) {
-                        System.out.println(k);
-                    }
-                }
-
-                @Override
-                public String name() {
-                    return "list";
-                }
-            };
-        }
-
-        if ("exit".equals(argv[0])) {
-            return new Command() {
-                @Override
-                public void execute() throws CommandExecutionException, StopProcessingException {
-                    if (argv.length != 1) {
-                        throw new CommandExecutionException(this, "Unexpected number of arguments");
-                    }
-                    throw new StopProcessingException();
-                }
-
-                @Override
-                public String name() {
-                    return "exit";
-                }
-            };
-        }
-
-        throw new UnknownCommandException(argv[0]);
     }
 }
