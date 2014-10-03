a1edfe870e7681eaaee894c1b730ed1f3c81a186
spec45as <spec45as@gmail.com>
2014-10-03 20:37:34 +0400

    fix

--- src/ru/fizteh/fivt/students/maxim_rep/shell/commands/EmptyCommand.java
+++ src/ru/fizteh/fivt/students/maxim_rep/shell/commands/EmptyCommand.java
@@ -3,6 +3,6 @@ package ru.fizteh.fivt.students.maxim_rep.shell.commands;
 public class EmptyCommand implements ShellCommand {
     @Override
     public boolean execute() {
-        return true;
+        return false;
     }
 }
