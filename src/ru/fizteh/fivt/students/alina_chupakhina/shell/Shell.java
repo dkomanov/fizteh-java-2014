eb4413d2e9cd69f4b4f31fdd4c7b63c88e8633d5
chav0 <chav0@mail.ru>
2014-10-03 17:03:44 +0400

    Update Shell.java

--- src/ru/fizteh/fivt/students/alina_chupakhina/shell/Shell.java
+++ src/ru/fizteh/fivt/students/alina_chupakhina/shell/Shell.java
@@ -13,7 +13,7 @@ public class Shell {
     static private String current;
     static private boolean out;
 
-    public static void main(String[] args){
+    public static void main(final String[] args) {
         current = System.getProperty("user.dir");
         try {
             if (args.length > 0) {
@@ -27,43 +27,45 @@ public class Shell {
         }
     }
 
-    public static void interactive(){
+    public static void interactive() {
         out = false;
         Scanner sc = new Scanner(System.in);
         try {
             while (!out) {
                 System.out.print("$ ");
                 String s = sc.nextLine();
-                docom(s);
+                docom(s, false);
             }
             System.exit(0);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.exit(-1);
         }
     }
 
-    public static void conapp (String[] args) {
+    public static void conapp(final String[] args) {
         String arg;
         arg = args[0];
-        for (int i = 1; i != args.length; i++) arg = arg + ' ' + args[i];
+        for (int i = 1; i != args.length; i++) {
+            arg = arg + ' ' + args[i];
+        }
         String[] commands = arg.trim().split(";");
         try {
             for (int i = 0; i != commands.length; i++) {
-                docom(commands[i]);
+                docom(commands[i], true);
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(-1);
         }
-        interactive ();
+        interactive();
     }
 
-    public static void docom(String command) throws Exception {
+    public static void docom(final String command, boolean mode)
+            throws Exception {
         String[] args = command.trim().split("\\s+");
         try {
             if (args[0].equals("pwd")) {
-                pwd();
+                pwd(args);
 
             } else if (args[0].equals("cd")) {
                 cd(args);
@@ -87,7 +89,7 @@ public class Shell {
                 mv(args);
 
             } else if (args[0].equals("exit")) {
-                exit();
+                exit(args);
 
             } else if (args[0].equals("")) {
             } else {
@@ -95,39 +97,64 @@ public class Shell {
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            System.exit(-1);
+            if (mode) {
+                System.exit(-1);
+            }
         }
     }
 
-    private static void mv(String[] args) throws Exception {
-        if (args.length != 3){
-            throw new Exception("cp: Incorrect number of arguments");
+    private static void mv(final String[] args) throws Exception {
+        if (args.length != 3) {
+            throw new Exception("cp: invalid number of arguments");
         }
-        File source = new File(current + File.separator + args[args.length - 2]);
-        File destination = new File(current + File.separator + args[args.length - 1]);
-        if (source.getParent().equals(destination.getParent()) && !destination.exists()) {
+        File source = new File(
+                current + File.separator + args[args.length - 2]);
+        File destination = new File(
+                current + File.separator + args[args.length - 1]);
+        if (source.getParent().equals(destination.getParent())
+                && !destination.exists()) {
             source.renameTo(destination);
             return;
         }
-        if (!destination.exists() || !source.exists())
-            throw new Exception("cp: File or Directory not found");
-        if (destination.isFile())
-            throw new Exception("cp: Destination is file");
-        copy(source, destination);
-        remove(source);
+        if (!destination.exists() || !source.exists()) {
+            throw new Exception("mv: File or Directory not found");
+        }
+        if (destination.isFile()) {
+            throw new Exception("mv: " + args[args.length - 1] + " is file");
+        }
+        try {
+            copy(source, destination);
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            msg = msg.replaceFirst("cp", "mv");
+            throw new Exception(msg);
+        }
+        try {
+            remove(source);
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            msg = msg.replaceFirst("rm", "mv");
+            throw new Exception(msg);
+        }
     }
 
-    private static void exit() {
+    private static void exit(final String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new Exception(
+                    "mkdir: Invalid number of arguments");
+        }
         out = true;
     }
 
-    private static void copy (File source, File destination) throws Exception {
-        String to = destination.getCanonicalPath() + File.separator + source.getName();
+    private static void copy(final File source, File destination)
+            throws Exception {
+        String to = destination.getCanonicalPath()
+                + File.separator + source.getName();
         destination = new File(to);
         if (source.isDirectory()) {
-            if (!destination.exists())
-                if (!destination.mkdir())
-                    throw new Exception("cp: Can not create directory");
+            if (!destination.exists()) {
+                destination.mkdir();
+            }
             File [] children = source.listFiles();
             if (!children.equals(null)) {
                 for (int i = 0; i != children.length; i++) {
@@ -135,34 +162,41 @@ public class Shell {
                 }
             }
         } else {
-            Files.copy(source.toPath(), destination.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+            Files.copy(source.toPath(), destination.toPath(),
+                    StandardCopyOption.COPY_ATTRIBUTES);
         }
 
     }
 
-    private static void cp(String[] args) throws Exception {
-        if (4 < args.length || args.length < 3){
-            throw new Exception("cp: Incorrect number of arguments");
+    private static void cp(final String[] args) throws Exception {
+        if (4 < args.length || args.length < 3) {
+            throw new Exception("cp: invalid number of arguments");
         }
         boolean rec = false;
-        if (args.length == 4){
+        if (args.length == 4) {
             if (!args[1].equals("-r")) {
-                throw new Exception("cp: Incorrect number of arguments");
-            } else
+                throw new Exception("cp: invalid number of arguments");
+            } else {
                 rec = true;
+            }
         }
-        File source = new File(current + File.separator + args[args.length - 2]);
-        File destination = new File(current + File.separator + args[args.length - 1]);
+        File source = new File(current
+                + File.separator + args[args.length - 2]);
+        File destination = new File(current
+                + File.separator + args[args.length - 1]);
         if (!source.exists()) {
-            throw new Exception("cp: File or Directory not found");
+            throw new Exception("cp: " + args[args.length - 2]
+                    + " No such file or directory");
         }
         if (!destination.exists()) {
             destination.mkdir();
         }
         if (destination.isFile()) {
-            throw new Exception("cp: Destination is file");
+            throw new Exception("cp: " + args[args.length - 1]
+                    + " is not directory");
         }
-        if ((destination.getAbsolutePath().contains(source.getAbsolutePath()))) {
+        if ((destination.getAbsolutePath().
+                contains(source.getAbsolutePath()))) {
             throw new Exception("cp: Сan not be copied");
         }
         if (source.isDirectory() && !rec) {
@@ -171,60 +205,77 @@ public class Shell {
         copy(source, destination);
     }
 
-    private static void rm(String[] args) throws Exception {
+    private static void rm(final String[] args) throws Exception {
         boolean rec;
-        if (args.length < 2 || args.length > 3){
-            throw new Exception("rm: Incorrect number of arguments");
+        if (args.length < 2 || args.length > 3) {
+            throw new Exception("rm: invalid number of arguments");
         }
-        if (args[1].equals("-r") && args.length == 3){
+        if (args[1].equals("-r") && args.length == 3) {
             rec = true;
         } else {
-            if (args[1].equals("-r") && args.length == 2){
-                throw new Exception("rm: Incorrect function call");
+            if (args[1].equals("-r") && args.length == 2) {
+                throw new Exception("rm: invalid number of arguments");
             } else {
                 rec = false;
             }
         }
-        File file = new File(current+ File.separator + args[args.length - 1]);
+        File file = new File(current + File.separator + args[args.length - 1]);
         if (!file.exists()) {
-            throw new Exception("rm: " + file.getAbsolutePath() + " No such file or directory");
+            throw new Exception("rm: " + file.getAbsolutePath()
+                    + " No such file or directory");
         }
         if (file.isDirectory() && !rec) {
-            throw new Exception("rm: " + file.getAbsolutePath() + " It's directory");
+            throw new Exception("rm: " + file.getAbsolutePath()
+                    + " is a directory (not copied)");
         }
         remove(file);
     }
 
-    public static void remove(File file) throws Exception{
+    public static void remove(final File file) throws Exception {
         if (file.isDirectory()) {
             File [] children = file.listFiles();
+            if (children == null) {
+                throw new Exception(
+                        "rm: cannot remove");
+            }
             for (int i = 0; i != children.length; i++) {
                     remove(children[i]);
             }
-            file.delete();
-        } else if (!file.delete())
+        }
+        if (!file.delete()) {
             throw new Exception(
-                "rm:" + file.getName() + ". Can not delete the file");
+                    "rm:" + file.getName() + ". Can not delete the file");
+        }
+        return;
     }
 
-    public static void pwd() {
+    public static void pwd(final String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new Exception(
+                    "mkdir: Invalid number of arguments");
+        }
         System.out.println(current);
     }
 
-    public static void makedir(String[] args) throws Exception{
+    public static void makedir(final String[] args) throws Exception {
         String name = args[1];
+        for (int i = 2; i != args.length; i++) {
+            name = name + ' ' + args[i];
+        }
         String path = current + File.separator + name;
         File dir = new File(path);
         if (dir.exists() && dir.isDirectory()) {
-            throw new Exception("mkdir: Directory already exist");
+            throw new Exception("mkdir: " + args[1]
+                    + ": Directory already exist");
         }
         if (!dir.mkdir()) {
-            throw new Exception("mkdir: Can not create directory");
+            throw new Exception("mkdir: " + args[1]
+                    + ": Can not create directory");
         }
 
     }
 
-    public static void cd(String[] args) {
+    public static void cd(final String[] args) throws Exception {
         String path = args[1];
         for (int i = 2; i != args.length; i++) {
             path = path + ' ' + args[i];
@@ -232,31 +283,43 @@ public class Shell {
         File dir = new File(path);
         if (!path.equals(dir.getAbsolutePath())) {
             path = current + File.separator + path;
-            dir = new File (path);
+            dir = new File(path);
+        }
+        if (dir.isFile()) {
+            throw new Exception("cd: " + args[1] + ": is not directory");
         }
         if (!dir.exists()) {
-            System.out.println("cd: '" + path + " No such file or directory");
+            throw new Exception("cd: '" + path + ": No such file or directory");
         } else {
             current = path;
         }
     }
 
-    public static void cat(String[] args) throws Exception {
+    public static void cat(final String[] args) throws Exception {
+        if ((args.length != 2)) {
+            throw new Exception(
+                    "cat: Invalid number of arguments");
+        }
         File f = new File(args[1]);
         if (!f.exists()) {
-            throw new Exception("cat: File not exist");
+            throw new Exception("cat: " + args[1] + ": File not exist");
+        }
+        if (f.isDirectory()) {
+            throw new Exception("cat: " + args[1] + ": is a directory");
         }
         BufferedReader fin = new BufferedReader(new FileReader(f));
         String line;
         try {
-            while ((line = fin.readLine()) != null) System.out.println(line);
+            while ((line = fin.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             System.out.println(e.getMessage());
             System.exit(-1);
         }
     }
 
-    public static void ls(String[] args) throws Exception{
+    public static void ls(final String[] args) throws Exception {
         if (args.length == 1) {
             File dir = new File(current);
             File[] children = dir.listFiles();
@@ -269,7 +332,7 @@ public class Shell {
         } else {
             File dir = new File(args[1]);
             if (dir.isFile()) {
-                throw new Exception("ls: It is not directory");
+                throw new Exception("ls: " + args[1] + " is not directory");
             }
             File[] children = dir.listFiles();
             int i;
