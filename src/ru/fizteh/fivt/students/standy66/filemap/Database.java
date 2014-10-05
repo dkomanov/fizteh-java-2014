6f09d11923130460134fdaba87d6d0cf365a629f
Andrew Stepanov <karn9050@gmail.com>
2014-10-05 13:16:09 +0400

    Tabs converted to spaces

--- src/ru/fizteh/fivt/students/standy66/filemap/Database.java
+++ src/ru/fizteh/fivt/students/standy66/filemap/Database.java
@@ -12,85 +12,86 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/** Simple database implementation. <br>
+/**
+ * Simple database implementation. <br>
  * Working mechanism is specified at {@link ru.fizteh.fivt.students.standy66.filemap} <br>
  * Created by astepanov on 26.09.14.
  */
 public class Database {
 
-	private String dbFilePath;
-	private Map<String, String> cache;
+    private String dbFilePath;
+    private Map<String, String> cache;
 
-	public Database(@NotNull final String databaseFilePath) {
-		dbFilePath = databaseFilePath;
-		cache = new HashMap<>();
-		reload();
-	}
+    public Database(@NotNull final String databaseFilePath) {
+        dbFilePath = databaseFilePath;
+        cache = new HashMap<>();
+        reload();
+    }
 
-	private void reload() {
-		FileChannel channel = null;
-		try {
-			channel = new FileInputStream(dbFilePath).getChannel();
-			ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-			try {
-				while (buffer.remaining() > 0) {
-					int keySize = buffer.getInt();
-					byte[] key = new byte[keySize];
-					buffer.get(key);
-					int valueSize = buffer.getInt();
-					byte[] value = new byte[valueSize];
-					buffer.get(value);
-					cache.put(new String(key, "UTF-8"), new String(value, "UTF-8"));
-				}
-			} catch (BufferUnderflowException e) {
-				System.err.println("Corrupted file");
-				System.exit(1);
-			}
-			channel.close();
-		} catch (IOException e) {
-			System.err.println("Error reading file");
-			System.exit(2);
-		}
-	}
+    private void reload() {
+        FileChannel channel = null;
+        try {
+            channel = new FileInputStream(dbFilePath).getChannel();
+            ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+            try {
+                while (buffer.remaining() > 0) {
+                    int keySize = buffer.getInt();
+                    byte[] key = new byte[keySize];
+                    buffer.get(key);
+                    int valueSize = buffer.getInt();
+                    byte[] value = new byte[valueSize];
+                    buffer.get(value);
+                    cache.put(new String(key, "UTF-8"), new String(value, "UTF-8"));
+                }
+            } catch (BufferUnderflowException e) {
+                System.err.println("Corrupted file");
+                System.exit(1);
+            }
+            channel.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file");
+            System.exit(2);
+        }
+    }
 
-	public Set<String> list() {
-		return cache.keySet();
-	}
+    public Set<String> list() {
+        return cache.keySet();
+    }
 
-	public String get(@NotNull final String key) {
-		return cache.get(key);
-	}
+    public String get(@NotNull final String key) {
+        return cache.get(key);
+    }
 
-	public void put(@NotNull final String key, @NotNull final String value) {
-		cache.put(key, value);
-	}
+    public void put(@NotNull final String key, @NotNull final String value) {
+        cache.put(key, value);
+    }
 
-	public boolean remove(@NotNull final String key) {
-		if (cache.containsKey(key)) {
-			cache.remove(key);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean remove(@NotNull final String key) {
+        if (cache.containsKey(key)) {
+            cache.remove(key);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public void flush() {
-		try {
-			FileOutputStream fos = new FileOutputStream(dbFilePath, false);
-			ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
-			for (String key : cache.keySet()) {
-				String value = cache.get(key);
-				byte[] keyEncoded = key.getBytes("UTF-8");
-				fos.write(lengthBuffer.putInt(0, keyEncoded.length).array());
-				fos.write(keyEncoded);
-				byte[] valueEncoded = value.getBytes("UTF-8");
-				fos.write(lengthBuffer.putInt(0, valueEncoded.length).array());
-				fos.write(valueEncoded);
-			}
-			fos.close();
-		} catch (IOException e) {
-			System.err.println("Error writing file");
-			System.exit(4);
-		}
-	}
+    public void flush() {
+        try {
+            FileOutputStream fos = new FileOutputStream(dbFilePath, false);
+            ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+            for (String key : cache.keySet()) {
+                String value = cache.get(key);
+                byte[] keyEncoded = key.getBytes("UTF-8");
+                fos.write(lengthBuffer.putInt(0, keyEncoded.length).array());
+                fos.write(keyEncoded);
+                byte[] valueEncoded = value.getBytes("UTF-8");
+                fos.write(lengthBuffer.putInt(0, valueEncoded.length).array());
+                fos.write(valueEncoded);
+            }
+            fos.close();
+        } catch (IOException e) {
+            System.err.println("Error writing file");
+            System.exit(4);
+        }
+    }
 }
--- src/ru/fizteh/fivt/students/standy66/filemap/FileMapMain.java
+++ src/ru/fizteh/fivt/students/standy66/filemap/FileMapMain.java
@@ -4,18 +4,19 @@ import java.io.ByteArrayInputStream;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/** Class containing entry point. <br>
+/**
+ * Class containing entry point. <br>
  * Created by astepanov on 26.09.14.
  */
 public class FileMapMain {
-	public static void main(String[] args) {
-		FileMapShell fileMapShell;
-		if (args.length == 0) {
-			fileMapShell = new FileMapShell(System.in, true);
-		} else {
-			String params = Stream.of(args).collect(Collectors.joining(" "));
-			fileMapShell = new FileMapShell(new ByteArrayInputStream(params.getBytes()), false);
-		}
-		fileMapShell.run();
-	}
+    public static void main(String[] args) {
+        FileMapShell fileMapShell;
+        if (args.length == 0) {
+            fileMapShell = new FileMapShell(System.in, true);
+        } else {
+            String params = Stream.of(args).collect(Collectors.joining(" "));
+            fileMapShell = new FileMapShell(new ByteArrayInputStream(params.getBytes()), false);
+        }
+        fileMapShell.run();
+    }
 }
