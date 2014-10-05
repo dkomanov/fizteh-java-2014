84fd9c426975640a8df3a74c8bcf8631da655c59
kagudkov <kagudkov@gmail.com>
2014-10-05 18:48:27 +0400

    modern

--- src/ru/fizteh/fivt/students/gudkov394/map/Remove.java
+++ src/ru/fizteh/fivt/students/gudkov394/map/Remove.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Remove {
     public Remove(final String[] currentArgs, final Map ct) {
         if (currentArgs.length != 2) {
-            System.err.println("wrong number of argument to Get");
+            System.err.println("wrong number of argument to Remove");
             System.exit(1);
         }
         if (ct.remove(currentArgs[1]) != null) {
