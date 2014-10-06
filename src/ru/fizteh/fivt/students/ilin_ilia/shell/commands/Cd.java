44e6d88adaa52b0f9d406990ea7773132f34fdc5
TheronSG <ilinie@yandex.ru>
2014-10-06 19:45:49 +0400

    Second commit

---
+++ src/ru/fizteh/fivt/students/ilin_ilia/shell/commands/Cd.java
@@ -0,0 +1,29 @@
+package ru.fiztech.fivt.students.theronsg.shell.commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class Cd {
+    public static void run(final String path) {
+        try {
+            File curPath = Paths.get(path).normalize().toFile();
+            if (!curPath.isAbsolute()) {
+                curPath = Paths.get(System.getProperty("user.dir"),
+                        curPath.getPath()).normalize().toFile();
+            }
+            if (curPath.exists()) {
+                if (curPath.isDirectory()) {
+                    System.setProperty("user.dir", curPath.getPath());
+                } else {
+                    System.out.println("\"" + path + "\"" + "isn't a dir");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Can't change current dir");
+            System.exit(-1);
+        }
+    }
+
+}
