a7d159f3405f042debf24b4a5b8e4134a0caa6bf
tonmit <mitin@phystech.edu>
2014-10-07 12:32:12 +0400

    Update Shell.java

--- src/ru/fizteh/fivt/students/tonmit/Shell.java
+++ src/ru/fizteh/fivt/students/tonmit/Shell.java
@@ -132,7 +132,7 @@ public class Shell {
     private boolean copyErrorCheck(final String[] args) {
     	if (args.length > 4 || args.length < 3) {
     		printError("cp: Wrong number of arguments.");
-    		return false;
+                return false;
     	}
     	File currFile = createFile(args[1]);
     	File destFile = createFile(args[2]);
