3bb44f8cd46dcd88f027d2b37a2bc28f9b4b9276
Артем <artem@MacBook-Air-Artem.local>
2014-10-07 13:02:32 +0400

    Final version

--- src/ru/fizteh/fivt/students/artem/new_shele/TO/TEST/second.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-	int a=5;
-	std:: cout<<a ; 
-	return 0;
-}
--- src/ru/fizteh/fivt/students/artem_gritsay/Shell/Funcshell.java
+++ src/ru/fizteh/fivt/students/artem_gritsay/Shell/Funcshell.java
@@ -1,11 +1,12 @@
-package com.example.new_shele;
+package ru.fizteh.fivt.students.artem_gritsay.Shell;
 import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
 import java.nio.file.StandardCopyOption;
 
 
-public class Func_shele {
+public class Funcshell {
+
     public String currentDirectory;
 
     public void mkdir(String[] args) {
@@ -13,7 +14,9 @@ public class Func_shele {
             File f = new File(currentDirectory + File.separator + args[1]);
             if (!f.exists()) {
                 try {
-                    if (!f.mkdir()) System.out.println("Folder isn't create");
+                    if (!f.mkdir()) {
+                        System.out.println("Folder isn't create");
+                    }
                 } catch (Exception e) {
                     System.err.println(e.getMessage());
                 }
@@ -35,7 +38,7 @@ public class Func_shele {
     public void cat(String[] args) throws IOException {
         if (args.length == 2) {
             File f = new File(currentDirectory + File.separator + args[1]);
-            if(f.exists()) {
+            if (f.exists()) {
                 BufferedReader br = null;
                 try {
                     br = new BufferedReader((new FileReader(f)));
@@ -46,102 +49,121 @@ public class Func_shele {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        if (line != null)
+                        if (line != null) {
                             System.out.println(line);
+                        }
                     }
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
-            } else System.out.println("cat: '"+f.getName()+"': No such file or directory");
-        } else System.out.println("Incorrect arguments");
+            } else { System.out.println("cat: '" + f.getName() + "': No such file or directory"); }
+        } else { System.out.println("Incorrect arguments"); }
     }
 
-    public void delete_file_directories(File f) throws IOException {
-        if(f.isDirectory()) {
-           System.out.println(f.getName());
+    public void deletefiledirectories(File f) throws IOException {
+        if (f.isDirectory()) {
+            System.out.println(f.getName());
             File[] files = f.listFiles();
-            if(files!=null) {
-                delete_file_directories(f);
+            if (files != null) {
+                deletefiledirectories(f);
             }
-            else return;
+            else {
+                System.out.println("not perform to delete");
+                return;
+            }
         }
 
-        if(!f.delete()) System.out.println("not perform to delete");
-   }
+        if (!f.delete()) { System.out.println("not perform to delete"); }
+    }
     public void rm(String[] args) throws IOException {
         if ((args.length == 3 && args[1].equals("-r")) || (args.length == 2)) {
             File f;
-            if (args.length == 3) f = new File(currentDirectory + File.separator + args[2]);
-            else f = new File(currentDirectory + File.separator + args[1]);
+            if (args.length == 3) {
+                f = new File(currentDirectory + File.separator + args[2]);
+            }
+            else  {
+                f = new File(currentDirectory + File.separator + args[1]);
+            }
             if (f.exists()) {
                 if (f.isDirectory()) {
-                        if (args.length == 3)
-                            delete_file_directories(f);
-                        else System.out.println("rm: "+f.getName()+": is a directory");
-                } else if (!f.delete()) System.out.println("not perform to delete");
-            } else  System.out.println("rm: cannot remove '"+f.getName()+"': No such file or directory");
-        } else System.out.println("Incorrect arguments");
-        }
+                    if (args.length == 3) {
+                        deletefiledirectories(f);
+                        }
+                    else { System.out.println("rm: " + f.getName() + ": is a directory"); }
+                } else if (!f.delete()) { System.out.println("not perform to delete"); }
+            } else { System.out.println("rm: cannot remove '" + f.getName() + "': No such file or directory"); }
+        } else { System.out.println("Incorrect arguments"); }
+    }
 
 
     public void recursiveCopy(File f1, File f2) throws IOException {
         File d = null;
-        if(f2.isDirectory())
-        d = new File(f2.getCanonicalPath()+ File.separator + f1.getName());
-        else return;
-        Files.copy(f1.toPath(),d.toPath(),StandardCopyOption.COPY_ATTRIBUTES);
-        if(f2.isDirectory()) {
+        if (f2.isDirectory()) {
+            d = new File(f2.getCanonicalPath() + File.separator + f1.getName());
+        }
+        else { return; }
+        Files.copy(f1.toPath(), d.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+        if (f2.isDirectory()) {
             File[] files = f1.listFiles();
-            if(files!=null) {
-                for(File f:files)
-                    recursiveCopy(f,d);
+            if (files != null) {
+                for (File f:files) {
+                    recursiveCopy(f, d); }
             }
         }
     }
 
     public void cp(String[] args) throws IOException {
         if ((args.length == 4 && args[1].equals("-r")) || (args.length == 3)) {
-            File f1, f2;
-            f1 = new File(currentDirectory + File.separator + args[args.length - 2]);
-            f2 = new File(currentDirectory + File.separator + args[args.length - 1]);
+
+            File f1 = new File(currentDirectory + File.separator + args[args.length - 2]);
+            File f2 = new File(currentDirectory + File.separator + args[args.length - 1]);
             if (Files.exists(f1.toPath()) && Files.exists(f2.toPath())) {
-                if(!f2.toPath().startsWith(f1.toPath())&&!Files.exists(Paths.get(f2.toString(),f1.getName()))) {
+                if (!f2.toPath().startsWith(f1.toPath()) && !Files.exists(Paths.get(f2.toString(), f1.getName()))) {
                     if (args.length == 4) {
                         recursiveCopy(f1, f2);
                     } else {
-                        if (f1.isDirectory())
+                        if (f1.isDirectory()) {
                             System.out.println("cp: " + f1.getName() + " is a directory (not copied).");
+                        }
                     }
                 }
-                else System.out.println("File is already exist");
+                else { System.out.println("File is already exist"); }
             } else {
-                if (!Files.exists(f1.toPath()))
-                    System.out.println("cp: cannot copied '" + args[args.length - 2] + "': No such file or directory ");
-                if (!Files.exists(f2.toPath()))
-                    System.out.println("cp: cannot copied to '" + args[args.length - 1] + "': No such file or directory ");
+                if (!Files.exists(f1.toPath())) {
+                    System.out.println("cp: cannot copied '" + args[args.length - 2]
+                            + "': No such file or directory ");
+                }
+                if (!Files.exists(f2.toPath())) {
+                    System.out.println("cp: cannot copied to '" + args[args.length - 1]
+                            + "': No such file or directory ");
+                }
             }
-        } else System.out.println("Incorrect arguments");
+        } else { System.out.println("Incorrect arguments"); }
     }
 
     public void mv(String[] args) throws IOException {
         if (args.length == 3) {
             File f1 = new File(currentDirectory + File.separator + args[args.length - 2]);
             File f2 = new File(currentDirectory + File.separator + args[args.length - 1]);
-            if(f1.exists()&&f2.exists()) {
-                if(f1.toString().equals(f2.toString())) System.out.println("files is identify");
+            if (f1.exists() && f2.exists()) {
+                if (f1.toString().equals(f2.toString())) { System.out.println("files is identify"); }
                 else {
-                    if(!f2.toPath().startsWith(f1.toPath())) {
-                        recursiveCopy(f1,f2);
-                        delete_file_directories(f1);
+                    if (!f2.toPath().startsWith(f1.toPath())) {
+                        recursiveCopy(f1, f2);
+                        deletefiledirectories(f1);
                     }
-                    else System.out.println("unable to move");
+                    else { System.out.println("unable to move"); }
                 }
             }
             else {
-                if(!f1.exists()) System.out.println("mv: cannot move '" + args[args.length - 2] + "': No such file or directory ");
-                if(!f2.exists()) System.out.println("mv: cannot move '" + args[args.length - 1] + "': No such file or directory ");
+                if (!f1.exists()) { System.out.println("mv: cannot move '" + args[args.length - 2]
+                        + "': No such file or directory ");
+                }
+                if (!f2.exists()) { System.out.println("mv: cannot move '" + args[args.length - 1]
+                        + "': No such file or directory ");
+                }
             }
-        } else System.out.println("Incorrect arguments");
+        } else { System.out.println("Incorrect arguments"); }
     }
 
     public void cd(String[] args) {
@@ -163,13 +185,14 @@ public class Func_shele {
             } catch (IOException e) {
                 e.getMessage();
             }
-        } else System.out.println("Incorrect arguments");
+        } else { System.out.println("Incorrect arguments"); }
     }
 
     public void pwd(String[] args) {
-        if (args.length == 1)
+        if (args.length == 1) {
             System.out.println(currentDirectory);
-        else System.out.println("Incorrect arguments");
+        }
+        else { System.out.println("Incorrect arguments"); }
     }
 
 
@@ -178,7 +201,7 @@ public class Func_shele {
 
     }
 
-    public void ShellSwitch(String com) throws ShellexecException, IOException {
+    public void shellSwitch(String com) throws ShellexecException, IOException {
         com = com.trim();
         String[] s = com.split("\\s+");
         try {
@@ -187,26 +210,30 @@ public class Func_shele {
             }
             else if (s[0].equals("pwd")) {
                 pwd(s);
+            }
 
-            } else if (s[0].equals("ls")) {
+            else if (s[0].equals("ls")) {
                 ls();
+            }
 
-            } else if (s[0].equals("cat")) {
+            else if (s[0].equals("cat")) {
                 cat(s);
-
-            } else if (s[0].equals("mkdir")) {
+            }
+            else if (s[0].equals("mkdir")) {
                 mkdir(s);
-
-            } else if (s[0].equals("rm")) {
+            }
+            else if (s[0].equals("rm")) {
                 rm(s);
-
-            } else if (s[0].equals("cp")) {
+            }
+            else if (s[0].equals("cp")) {
                 cp(s);
+            }
 
-            } else if (s[0].equals("mv")) {
+            else if (s[0].equals("mv")) {
                 mv(s);
+            }
 
-            } else if (s[0].equals("exit")) {
+            else if (s[0].equals("exit")) {
                 exit();
             }
         } catch (IOException e) {
@@ -215,12 +242,13 @@ public class Func_shele {
 
 
     }
-    public void SwitchCommands(String[] s) {
+    public void switchCommands(String[] s) {
         try {
-            for (String command : s)
-                    ShellSwitch(command);
+            for (String command : s) {
+                shellSwitch(command);
+            }
 
-        }catch (ShellexecException e) {
+        } catch (ShellexecException e) {
             System.exit(0);
         }
         catch (IOException e) {
@@ -237,7 +265,7 @@ public class Func_shele {
     }
     public void switchLine(String line) {
         String[] commands = line.trim().split(";");
-        SwitchCommands(commands);
+        switchCommands(commands);
     }
 
     public void interactive() {
@@ -256,5 +284,4 @@ public class Func_shele {
 
 }
 class ShellexecException extends Exception {}
-
 
