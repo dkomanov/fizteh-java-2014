713897e66a3f7fa439274d31ce7e29aaf0085394
Egor Lunichkin <e.lunichkin@gmail.com>
2014-10-08 01:56:06 +0400

    Calculator 1.0.3
    Small fixes in CalculatorException.java

--- src/ru/fizteh/fivt/students/EgorLunichkin/calculator/CalculatorException.java
+++ src/ru/fizteh/fivt/students/EgorLunichkin/calculator/CalculatorException.java
@@ -2,6 +2,6 @@ package ru.fizteh.fivt.students.EgorLunichkin.calculator;
 
 public class CalculatorException extends Exception {
     public CalculatorException(String msg) {
-        super("ERROR: " + msg);
+        super(msg);
     }
 }
