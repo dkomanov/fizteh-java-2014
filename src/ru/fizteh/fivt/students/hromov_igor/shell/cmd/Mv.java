debf04d6da20b93a8e8ee1e7cd1614568f3302dd
hromov-igor <hromov.igor@gmail.com>
2014-10-07 14:28:21 +0400

    style checked

--- src/ru/fizteh/fivt/students/hromov_igor/shell/cmd/Mv.java
+++ src/ru/fizteh/fivt/students/hromov_igor/shell/cmd/Mv.java
@@ -1,24 +1,24 @@
 package ru.fizteh.fivt.students.hromov_igor.shell.cmd;
 
 public class Mv {
-	public static void run(String[] args) throws Exception {
-		if (args.length != 3) {
-			throw new Exception("rm : wrong number of arguments");
-		}
+    public static void run(String[] args) throws Exception {
+        if (args.length != 3) {
+            throw new Exception("rm : wrong number of arguments");
+        }
 
-		String[] newArgs = new String[4];
+        String[] newArgs = new String[4];
 
-		newArgs[0] = args[0];
-		newArgs[1] = "-r";
-		newArgs[2] = args[1];
-		newArgs[3] = args[2];
+        newArgs[0] = args[0];
+        newArgs[1] = "-r";
+        newArgs[2] = args[1];
+        newArgs[3] = args[2];
 
-		Cp.run(newArgs);
+        Cp.run(newArgs);
 
-		newArgs = new String[3];
-		newArgs[0] = args[0];
-		newArgs[1] = "-r";
-		newArgs[2] = args[1];
-		Rm.run(newArgs);
-	}
+        newArgs = new String[3];
+        newArgs[0] = args[0];
+        newArgs[1] = "-r";
+        newArgs[2] = args[1];
+        Rm.run(newArgs);
+    }
 }
