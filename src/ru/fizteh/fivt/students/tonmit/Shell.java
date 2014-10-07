5aa55a7a4d9da7b564676ef5427af2c581ef7221
tonmit <mitin@phystech.edu>
2014-10-07 13:13:50 +0400

    Update Shell.java

--- src/ru/fizteh/fivt/students/tonmit/Shell.java
+++ src/ru/fizteh/fivt/students/tonmit/Shell.java
@@ -98,7 +98,7 @@ public class Shell {
     private void makeDirectory(String arg) {
         File currFile = createFile(arg);
         if (currFile.exists()) {
-        	printError("mkdir: cannot create a directory '" + arg + "': such directory exists");
+            printError("mkdir: cannot create a directory '" + arg + "': such directory exists");
         } else if (!currFile.mkdirs()) {
             printError("mkdir cannot create a directory'" + arg + "'");
         }
@@ -130,204 +130,204 @@ public class Shell {
     }
 
     private boolean copyErrorCheck(final String[] args) {
-    	if (args.length > 4 || args.length < 3) {
-    		printError("cp: Wrong number of arguments.");
-    		return false;
-    	}
-    	File currFile = createFile(args[1]);
-    	File destFile = createFile(args[2]);
-    	if	(args.length == 4 && !args[1].equals("-r")) {
-    		printError("cp: Wrong arguments.");
-    		return false;
-    	}
-    	String tmpName = args[1];
-    	if (args.length == 4) {
-    		currFile = createFile(args[2]);
-    		destFile = createFile(args[3]);
-    		tmpName = args[2];
-    	}
-    	if (args.length == 3) {
-    		currFile = createFile(args[1]);
-    		destFile = createFile(args[2]);
-    		if (currFile.isDirectory()) {
-    			printError("cp: " + args[1] + " is a directory (not copied).");
-    			return false;
-        	}
-    	}
-    	if (!currFile.exists()) {
-    		printError("cp: " + tmpName + ": No such file or directory.");
-    		return false;
-    	}
-    	if (!destFile.exists()) {
-    	    return true;
-    	}
-    	try {
-    		if (destFile.getCanonicalPath().startsWith(currFile.getCanonicalPath())) {
-    			printError("cp: copy into self.");
-    			return false;
-    		}
-    		if (Paths.get(destFile.getCanonicalPath(),
-    		        currFile.getName()).toString().equals(currFile.getCanonicalPath())) {
-	    		printError("cp: paths are equal.");
-	    		return false;
-	    	}
-    	} catch (IOException e1) {
-    		printError("cp: copy into self.");
-			return false;
-    	}
-    	return true;
+        if (args.length > 4 || args.length < 3) {
+            printError("cp: Wrong number of arguments.");
+            return false;
+        }
+        File currFile = createFile(args[1]);
+        File destFile = createFile(args[2]);
+        if  (args.length == 4 && !args[1].equals("-r")) {
+            printError("cp: Wrong arguments.");
+            return false;
+        }
+        String tmpName = args[1];
+        if (args.length == 4) {
+            currFile = createFile(args[2]);
+            destFile = createFile(args[3]);
+            tmpName = args[2];
+        }
+        if (args.length == 3) {
+            currFile = createFile(args[1]);
+            destFile = createFile(args[2]);
+            if (currFile.isDirectory()) {
+                printError("cp: " + args[1] + " is a directory (not copied).");
+                return false;
+            }
+        }
+        if (!currFile.exists()) {
+            printError("cp: " + tmpName + ": No such file or directory.");
+            return false;
+        }
+        if (!destFile.exists()) {
+            return true;
+        }
+        try {
+            if (destFile.getCanonicalPath().startsWith(currFile.getCanonicalPath())) {
+                printError("cp: copy into self.");
+                return false;
+            }
+            if (Paths.get(destFile.getCanonicalPath(),
+                    currFile.getName()).toString().equals(currFile.getCanonicalPath())) {
+                printError("cp: paths are equal.");
+                return false;
+            }
+        } catch (IOException e1) {
+            printError("cp: copy into self.");
+            return false;
+        }
+        return true;
     }
 
     private void copy(String[] args) {
-    	if (!copyErrorCheck(args)) {
-    		return;
-    	}
-    	File currFile = createFile(args[1]);
-    	File destFile = createFile(args[2]);
-    	if (args.length == 4) {
-    		currFile = createFile(args[2]);
-    		destFile = createFile(args[3]);
-    	}
-    	if (!currFile.isDirectory() && !destFile.exists()) {
-    	    try {
-    	        Files.createFile(destFile.toPath());
-        	    Files.copy(currFile.toPath(), destFile.toPath(), StandardCopyOption.COPY_ATTRIBUTES,
+        if (!copyErrorCheck(args)) {
+            return;
+        }
+        File currFile = createFile(args[1]);
+        File destFile = createFile(args[2]);
+        if (args.length == 4) {
+            currFile = createFile(args[2]);
+            destFile = createFile(args[3]);
+        }
+        if (!currFile.isDirectory() && !destFile.exists()) {
+            try {
+                Files.createFile(destFile.toPath());
+                Files.copy(currFile.toPath(), destFile.toPath(), StandardCopyOption.COPY_ATTRIBUTES,
                         StandardCopyOption.REPLACE_EXISTING);
-        	    return;
-    	    } catch (IOException e1) {
-    	        printError("cp: Incorrect arguments.");
-    	        return;
-    	    }
-    	}
-    	if (args.length == 3 || !currFile.isDirectory()) {
-    		try {
-    			destFile = Paths.get(destFile.toString(),
-    			        currFile.toPath().getFileName().toString()).normalize().toFile();
-    			Files.copy(currFile.toPath(), destFile.toPath(), StandardCopyOption.COPY_ATTRIBUTES,
+                return;
+            } catch (IOException e1) {
+                printError("cp: Incorrect arguments.");
+                return;
+            }
+        }
+        if (args.length == 3 || !currFile.isDirectory()) {
+            try {
+                destFile = Paths.get(destFile.toString(),
+                        currFile.toPath().getFileName().toString()).normalize().toFile();
+                Files.copy(currFile.toPath(), destFile.toPath(), StandardCopyOption.COPY_ATTRIBUTES,
                     StandardCopyOption.REPLACE_EXISTING);
-    		} catch (IOException e1) {
-    			printError("cp: Incorrect arguments.");
-    			return;
-    		}
-    	} else {
-    		try {
-	    		String[] fileList = currFile.list();
-	    		String directoryName = currFile.getName();
-	    		File destDir = createFile(destFile.getCanonicalPath() + File.separator + directoryName);
-	    		if (!destFile.exists()) {
-	    		    destDir = createFile(destFile.toString());
-	    		}
-	    		makeDirectory(destDir.getCanonicalPath());
-	    		String[] argStr = new String[]{"cp", "-r", "", destDir.getCanonicalPath()};
-	    		for (int i = 0; i < fileList.length; i++) {
-	    			argStr[2] = currFile.getCanonicalPath() + File.separator + fileList[i];
-	    			copy(argStr);
-	    		}
-    		} catch (IOException e1) {
-	    			printError("cp: Incorrect arguments.");
-	    			return;
-	    		}
-    	}
+            } catch (IOException e1) {
+                printError("cp: Incorrect arguments.");
+                return;
+            }
+        } else {
+            try {
+                String[] fileList = currFile.list();
+                String directoryName = currFile.getName();
+                File destDir = createFile(destFile.getCanonicalPath() + File.separator + directoryName);
+                if (!destFile.exists()) {
+                    destDir = createFile(destFile.toString());
+                }
+                makeDirectory(destDir.getCanonicalPath());
+                String[] argStr = new String[]{"cp", "-r", "", destDir.getCanonicalPath()};
+                for (int i = 0; i < fileList.length; i++) {
+                    argStr[2] = currFile.getCanonicalPath() + File.separator + fileList[i];
+                    copy(argStr);
+                }
+            } catch (IOException e1) {
+                    printError("cp: Incorrect arguments.");
+                    return;
+                }
+        }
 
     }
 
     private void remove(String[] args) {
-    	if (args.length > 3 || args.length < 2) {
-    		printError("rm: Wrong number of parametres.");
-    		return;
-    	}
-    	if (args.length == 3 && !args[1].equals("-r")) {
-    		printError("rm: Wrong arguments.");
-    		return;
-    	}
-    	File fileToDelete = createFile(args[1]);
-    	String tmpname = args[1];
-    	if (args.length == 3) {
-    		fileToDelete = createFile(args[2]);
-    		tmpname = args[2];
-    	}
-    	if (!fileToDelete.exists()) {
-    		printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
-    		return;
-    	}
+        if (args.length > 3 || args.length < 2) {
+            printError("rm: Wrong number of parametres.");
+            return;
+        }
+        if (args.length == 3 && !args[1].equals("-r")) {
+            printError("rm: Wrong arguments.");
+            return;
+        }
+        File fileToDelete = createFile(args[1]);
+        String tmpname = args[1];
+        if (args.length == 3) {
+            fileToDelete = createFile(args[2]);
+            tmpname = args[2];
+        }
+        if (!fileToDelete.exists()) {
+            printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
+            return;
+        }
 
         if (!fileToDelete.isDirectory()) {
-    		if (!fileToDelete.delete()) {
-    			printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
+            if (!fileToDelete.delete()) {
+                printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
                 return;
             }
-    	} else {
-    	    try {
-	    		String[] fileList = fileToDelete.list();
-	    		String[] argStr = new String[]{"rm", "-r", ""};
-	    		for (int i = 0; i < fileList.length; i++) {
-	    			argStr[2] = fileToDelete.getCanonicalPath() + File.separator + fileList[i];
-	    			remove(argStr);
-	    		}
-	    		if (!fileToDelete.delete()) {
-	    			printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
-	                return;
-	            }
-    		} catch (IOException e1) {
-    			printError("rm: Incorrect arguments.");
-    			return;
-    		}
-    	}
+        } else {
+            try {
+                String[] fileList = fileToDelete.list();
+                String[] argStr = new String[]{"rm", "-r", ""};
+                for (int i = 0; i < fileList.length; i++) {
+                    argStr[2] = fileToDelete.getCanonicalPath() + File.separator + fileList[i];
+                    remove(argStr);
+                }
+                if (!fileToDelete.delete()) {
+                    printError("rm: cannot remove '" + tmpname + "':No such file or directory.");
+                    return;
+                }
+            } catch (IOException e1) {
+                printError("rm: Incorrect arguments.");
+                return;
+            }
+        }
     }
 
     private void move(final String[] args) {
-    	if (args.length != 3) {
-    		printError("mv: Wrong number of arguments.");
-    		return;
-    	}
-    	File currFile = createFile(args[1]);
-    	File destFile = createFile(args[2]);
-    	if (!currFile.exists()) {
-    		printError("mv : cannot move: '" + args[1] + "': No such file or directory");
-    		return;
-    	}
-    	if (destFile.isDirectory()) {
-    		destFile = createFile(args[2] + File.separator + currFile.getName());
-    	}
-    	try {
-    		if (destFile.getCanonicalPath().startsWith(currFile.getCanonicalPath())) {
-    			printError("mv: cannot move: '" + args[1] + "': Sourse is parent of destination");
+        if (args.length != 3) {
+            printError("mv: Wrong number of arguments.");
+            return;
+        }
+        File currFile = createFile(args[1]);
+        File destFile = createFile(args[2]);
+        if (!currFile.exists()) {
+            printError("mv : cannot move: '" + args[1] + "': No such file or directory");
+            return;
+        }
+        if (destFile.isDirectory()) {
+            destFile = createFile(args[2] + File.separator + currFile.getName());
+        }
+        try {
+            if (destFile.getCanonicalPath().startsWith(currFile.getCanonicalPath())) {
+                printError("mv: cannot move: '" + args[1] + "': Sourse is parent of destination");
                 return;
-    		}
-    		if (currFile.getCanonicalPath().equals(destFile.getCanonicalPath())) {
-    			printError("mv: cannot move: '" + args[1] + "': Sourse is the same as destination");
-	        	return;
-	        }
+            }
+            if (currFile.getCanonicalPath().equals(destFile.getCanonicalPath())) {
+                printError("mv: cannot move: '" + args[1] + "': Sourse is the same as destination");
+                return;
+            }
 
-    		Files.move(currFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-	    } catch (IOException e1) {
-	    		printError("mv: cannot move '" + args[1] + "'");
-	    		return;
-	    }
+            Files.move(currFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e1) {
+                printError("mv: cannot move '" + args[1] + "'");
+                return;
+        }
     }
 
     private void cat(final String[] args) {
-    	try {
-    		if (args.length != 2) {
-    			printError("cat: Wrong number of arguments.");
-    			return;
-    		}
-    		File currFile = createFile(args[1]);
-    		if (!currFile.exists()) {
-    			printError("cat: '" + args[1] + "': No such file.");
-    			return;
-    		}
-    		String absPath = currFile.getCanonicalPath();
-    		FileReader fin = new FileReader(absPath);
-    		int text;
-    		while ((text = fin.read()) != -1) {
-    			System.out.print((char) text);
-    		}
-    		System.out.println();
-    		fin.close();
-    	} catch (IOException e1) {
-    		printError("cat: Error occured.");
-    	}
+        try {
+            if (args.length != 2) {
+                printError("cat: Wrong number of arguments.");
+                return;
+            }
+            File currFile = createFile(args[1]);
+            if (!currFile.exists()) {
+                printError("cat: '" + args[1] + "': No such file.");
+                return;
+            }
+            String absPath = currFile.getCanonicalPath();
+            FileReader fin = new FileReader(absPath);
+            int text;
+            while ((text = fin.read()) != -1) {
+                System.out.print((char) text);
+            }
+            System.out.println();
+            fin.close();
+        } catch (IOException e1) {
+            printError("cat: Error occured.");
+        }
     }
 
     private void execProc(final String[] args) {
@@ -357,7 +357,7 @@ public class Shell {
                 remove(args);
                 break;
             case "mv":
-            	move(args);
+                move(args);
                 break;
             case "cp":
                     copy(args);
