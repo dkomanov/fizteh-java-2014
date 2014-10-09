73e007fcfb0ec81acb5ca07aa7deb0a2e5b64cf4
ilivanov <ilivanov.se@ya.ru>
2014-10-10 01:32:30 +0400

    filemap deleted from old branch

--- src/ru/fizteh/fivt/students/ilivanov/FileMap/CommandDb.java
+++
@@ -1,5 +0,0 @@
-package ru.fizteh.fivt.students.ilivanov.FileMap;
-
-public interface CommandDb {
-    int execute();
-}
--- src/ru/fizteh/fivt/students/ilivanov/FileMap/CommandDbExit.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.ilivanov.FileMap;
-
-public class CommandDbExit implements CommandDb {
-    @Override
-    public final int execute() {
-        return 7;
-    }
-}
