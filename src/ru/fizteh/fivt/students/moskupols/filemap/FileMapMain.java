71604f7bb7ab2bb9cd6fa1d6825e5043f0da9915
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-10-09 15:44:18 +0400

    Added unexpected EOF check

--- src/ru/fizteh/fivt/students/moskupols/filemap/FileMapMain.java
+++ src/ru/fizteh/fivt/students/moskupols/filemap/FileMapMain.java
@@ -8,18 +8,33 @@ import java.io.IOException;
  * Created by moskupols on 26.09.14.
  */
 public class FileMapMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String dbPath = System.getProperty("db.file");
         if (dbPath == null) {
-            System.err.println("Specify database file in properties.");
+            System.err.println("Specify database file in property `db.file'.");
             System.exit(1);
         }
 
-        DataBase db = new DataBase();
+/*
+        final DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(dbPath));
+        outputStream.writeBytes(Integer.);
+        outputStream.writeInt(5);
+        outputStream.writeInt(20);
+        outputStream.writeByte(20);
+        outputStream.close();
+        final DataInputStream inputStream = new DataInputStream(new FileInputStream(dbPath));
+        System.out.println(inputStream.readInt());
+        System.out.println(inputStream.readInt());
+        System.out.println(inputStream.readInt());
+        System.out.println(inputStream.readByte());
+        System.exit(0);
+*/
+
+        DataBaseTable db = null;
         try {
-            db.load(dbPath);
+            db = new DataBaseTable(dbPath);
         } catch (Exception e) {
-            System.err.println("Error while loading database:");
+            System.err.format("Error while loading database %s:\n", dbPath);
             System.err.println(e.getMessage());
             System.exit(1);
         }
@@ -32,7 +47,7 @@ public class FileMapMain {
         }
 
         try {
-            commandProcessor.process(new FileMapCommandFactory(db));
+            commandProcessor.process(new TableCommandFactory(db));
         } catch (CommandExecutionException | UnknownCommandException e) {
             System.err.println(e.getMessage());
             System.exit(1);
