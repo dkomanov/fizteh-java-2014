c12dc3e3c4d0de1347bb6abbae0418f29d00f48b
kagudkov <kagudkov@gmail.com>
2014-10-10 11:01:59 +0400

    first commit

---
+++ src/ru/fizteh/fivt/students/gudkov394/Junit/MyMapJunit.java
@@ -0,0 +1,14 @@
+package ru.fizteh.fivt.students.gudkov394.Junit;
+
+import ru.fizteh.fivt.students.gudkov394.MultiMap.MyMap;
+
+/**
+ * Created by kagudkov on 09.10.14.
+ */
+public class MyMapJunit extends MyMap {
+
+    public MyMapJunit(String[] args) {
+        super(args);
+    }
+
+}
