4c630fe42af8763901536f1d56b65c77d88c7b11
taftafa <TafTafa@gmail.com>
2014-10-11 01:55:50 +0400

    Fixed filemap.

--- src/ru/fizteh/fivt/students/kotsurba/filemap/database/DataBaseFile.java
+++ src/ru/fizteh/fivt/students/kotsurba/filemap/database/DataBaseFile.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-<<<<<<< HEAD
 import java.nio.file.Files;
-=======
->>>>>>> origin/master
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,11 +14,7 @@ public class DataBaseFile {
 
     protected final String fileName;
     protected File file;
-<<<<<<< HEAD
     protected Map<String, String> data;
-=======
-    protected Map<String,String> data;
->>>>>>> origin/master
 
     public DataBaseFile(final String newFileName) {
         fileName = newFileName;
@@ -54,11 +47,7 @@ public class DataBaseFile {
         }
 
         StringBuilder s = new StringBuilder();
-<<<<<<< HEAD
         for (Byte t : key) {
-=======
-        for(Byte t : key) {
->>>>>>> origin/master
             byte x = t;
             s.append((char) x);
         }
@@ -80,10 +69,7 @@ public class DataBaseFile {
         try {
             RandomAccessFile inputFile = new RandomAccessFile(fileName, "rw");
             if (inputFile.length() == 0) {
-<<<<<<< HEAD
                 inputFile.close();
-=======
->>>>>>> origin/master
                 return;
             }
             List<String> keys = new ArrayList<String>();
@@ -99,20 +85,12 @@ public class DataBaseFile {
 
             data.clear();
 
-<<<<<<< HEAD
             for (int i = 0; i < keys.size(); ++i) {
-=======
-            for(int i = 0; i < keys.size(); ++i) {
->>>>>>> origin/master
                 int l = (int) inputFile.length();
                 if (i != keys.size() - 1) {
                     l = delta.get(i + 1);
                 }
-<<<<<<< HEAD
                 data.put(keys.get(i), getValueLoad(inputFile, delta.get(i), l));
-=======
-                data.put(keys.get(i),getValueLoad(inputFile, delta.get(i), l));
->>>>>>> origin/master
             }
 
             inputFile.close();
@@ -126,46 +104,29 @@ public class DataBaseFile {
     public void save() {
         try {
             if (data.size() == 0) {
-<<<<<<< HEAD
                 try {
                     if (!Files.deleteIfExists(file.toPath())) {
                         throw new DataBaseException("Cannot delete a file!");
                     }
                 } catch (IOException e) {
                     throw new DataBaseException(e.getMessage());
-=======
-                if (!file.delete()) {
-                    throw new DataBaseException("Cannot delete a file!");
->>>>>>> origin/master
                 }
             } else {
                 RandomAccessFile outputFile = new RandomAccessFile(fileName, "rw");
                 try {
                     int t = 0;
-<<<<<<< HEAD
                     for (String x : data.keySet()) {
                         t += x.length() + 5;
                     }
 
                     for (String x : data.keySet()) {
-=======
-                    for(String x : data.keySet()) {
-                        t += x.length() + 5;
-                    }
-
-                    for(String x : data.keySet()) {
->>>>>>> origin/master
                         outputFile.write(x.getBytes());
                         outputFile.writeByte(0);
                         outputFile.writeInt(t);
                         t += data.get(x).length();
                     }
 
-<<<<<<< HEAD
                     for (String x : data.values()) {
-=======
-                    for(String x : data.values()) {
->>>>>>> origin/master
                         outputFile.write(x.getBytes());
                     }
 
@@ -181,11 +142,7 @@ public class DataBaseFile {
     }
 
     public String put(final String keyStr, final String valueStr) {
-<<<<<<< HEAD
         return data.put(keyStr, valueStr);
-=======
-        return data.put(keyStr,valueStr);
->>>>>>> origin/master
     }
 
     public String get(final String keyStr) {
@@ -199,11 +156,7 @@ public class DataBaseFile {
     public String getKeyList() {
         StringBuilder s = new StringBuilder();
 
-<<<<<<< HEAD
         for (String v : data.keySet()) {
-=======
-        for(String v : data.keySet()) {
->>>>>>> origin/master
             s.append(v).append(" ");
         }
         if (s.length() != 0) s.deleteCharAt(s.length() - 1);
