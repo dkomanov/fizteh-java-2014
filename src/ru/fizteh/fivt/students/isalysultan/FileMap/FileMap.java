2376c0c31a74dc687ef20678b0b86569c5ca7496
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-12 00:53:12 +0400

    Create FileMap.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/FileMap.java
@@ -0,0 +1,19 @@
+package ru.fizteh.fivt.students.IsalySultan.FileMap;
+
+import java.io.IOException;
+
+public class FileMap {
+
+    public static void main(String[] argv) {
+        try {
+            Table object = new Table();
+            if (argv.length > 0) {
+                PocketParser.batchMode(object, argv);
+            } else {
+                Interactive.interactiveMode(object);
+            }
+        } catch (IOException e) {
+            System.out.println("error create table");
+        }
+    }
+}
