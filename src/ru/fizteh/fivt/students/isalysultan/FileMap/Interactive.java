933ac40a8698843239932ec9171381338a305c20
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-12 00:54:05 +0400

    Create Interactive.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/Interactive.java
@@ -0,0 +1,41 @@
+package ru.fizteh.fivt.students.IsalySultan.FileMap;
+
+import java.util.Scanner;
+
+public class Interactive {
+
+    private Interactive() {
+       // Disable instantiation to this class.
+    }
+
+    public static void interactiveMode(Table tables) {
+        Scanner in = new Scanner(System.in);
+        String[] parserComand;
+        Comands object = new Comands();
+        while (true) {
+            String comand;
+            comand = in.nextLine();
+            parserComand = comand.split(" ");
+            switch (parserComand[0]) {
+            case "put":
+                object.put(parserComand[1], parserComand[2], tables);
+                break;
+            case "get":
+                object.get(parserComand[1], tables);
+                break;
+            case "remove":
+                object.remove(parserComand[1], tables);
+                break;
+            case "list":
+                object.list(tables);
+                break;
+            case "exit":
+                tables.writeFile();
+                System.out.println("exit");
+                return;
+            default:
+                System.out.println("команда не распознана");
+            }
+        }
+    }
+}
