ec528c2207922e8141cec9f45554055922dd32c0
TheronSG <Ilinie@yandex.ru>
2014-10-11 15:00:37 +0400

    Update FileMapMain.java

--- src/ru/fizteh/fivt/students/ilin_ilia/filemap/FileMapMain.java
+++ src/ru/fizteh/fivt/students/ilin_ilia/filemap/FileMapMain.java
@@ -1,7 +1,7 @@
 package ru.fiztech.fivt.students.theronsg.filemap;
 
 public class FileMapMain {
-    public static void main (final String[] args) {
+    public static void main(final String[] args) {
         if (args.length == 0) {
             FileMapDistributor.interactiveMode();
         } else {
