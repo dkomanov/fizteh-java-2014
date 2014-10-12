468b7ac2b02e7ddf9ceb5b928ba668b5e56c17ca
chav0 <chav0@mail.ru>
2014-10-12 14:09:28 +0400

    Update FileMap.java

--- src/ru/fizteh/fivt/students/alina_chupakhina/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/alina_chupakhina/filemap/FileMap.java
@@ -6,27 +6,24 @@ import java.util.*;
 public class FileMap {
 
     private static String path;
-    private static TreeMap<String, String> fm;
+    private static Map<String, String> fm;
     private static boolean out;
     private static RandomAccessFile file;
+    private static String message = "invalid number of arguments";
 
     public static void main(final String[] args) {
         try {
             fm = new TreeMap<String, String>();
+            path = System.getProperty("db.file");
             try {
-                path = System.getProperty("db.file");
-                try {
-                    file = new RandomAccessFile(path, "r");
-                    getFile();
-                } catch (FileNotFoundException e) {
-                    File f = new File(path);
-                    f.createNewFile();
-                }
-            } catch (Exception e) {
-                throw new Exception("Can't find or create data base file");
+                file = new RandomAccessFile(path, "r");
+                getFile();
+            } catch (FileNotFoundException e) {
+                File f = new File(path);
+                f.createNewFile();
             }
             if (args.length > 0) {
-                conapp(args);
+                batch(args);
             } else {
                 interactive();
             }
@@ -63,17 +60,20 @@ public class FileMap {
     public static void putFile() throws Exception {
         String key;
         String value;
-        file.setLength(0);
+        DataOutputStream outStream  = new DataOutputStream(
+                    new FileOutputStream(path));
         for (Map.Entry<String, String> i : fm.entrySet()) {
             key = i.getKey();
             value = i.getValue();
             try {
                 byte[] byteWord = key.getBytes("UTF-8");
-                file.writeInt(byteWord.length);
-                file.write(byteWord);
+                outStream.writeInt(byteWord.length);
+                outStream.write(byteWord);
+                outStream.flush();
                 byteWord = value.getBytes("UTF-8");
-                file.writeInt(byteWord.length);
-                file.write(byteWord);
+                outStream.writeInt(byteWord.length);
+                outStream.write(byteWord);
+                outStream.flush();
             } catch (Exception e) {
                 throw new Exception("Error with writing");
             }
@@ -87,7 +87,7 @@ public class FileMap {
             while (!out) {
                 System.out.print("$ ");
                 String s = sc.nextLine();
-                docom(s, false);
+                doCommand(s, false);
             }
             System.exit(0);
         } catch (Exception e) {
@@ -95,7 +95,7 @@ public class FileMap {
         }
     }
 
-    public static void conapp(final String[] args) {
+    public static void batch(final String[] args) {
         String arg;
         arg = args[0];
         for (int i = 1; i != args.length; i++) {
@@ -104,7 +104,7 @@ public class FileMap {
         String[] commands = arg.trim().split(";");
         try {
             for (int i = 0; i != commands.length; i++) {
-                docom(commands[i], true);
+                doCommand(commands[i], true);
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
@@ -113,25 +113,20 @@ public class FileMap {
         interactive();
     }
 
-    public static void docom(final String command, boolean mode)
+    public static void doCommand(final String command, boolean isBatch)
             throws Exception {
         String[] args = command.trim().split("\\s+");
         try {
             if (args[0].equals("put")) {
                 put(args);
-
             } else if (args[0].equals("get")) {
                 get(args);
-
             } else if (args[0].equals("remove")) {
                 remove(args);
-
             } else if (args[0].equals("list")) {
                 list(args);
-
             } else if (args[0].equals("exit")) {
                 exit(args);
-
             } else if (args[0].equals("")) {
                 out = false;
             } else {
@@ -139,7 +134,7 @@ public class FileMap {
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            if (mode) {
+            if (isBatch) {
                 System.exit(-1);
             }
         }
@@ -147,12 +142,11 @@ public class FileMap {
 
     public static void put(String[] args) throws Exception {
         if (args.length != 3) {
-            throw new Exception("put: invalid number of arguments");
+            throw new IllegalArgumentException("put: " + message);
         }
         String key = args[1];
         String value = args[2];
-        String s;
-        s = fm.put(key, value);
+        String s = fm.put(key, value);
         if (s != null) {
             System.out.println("overwrite");
             System.out.println(s);
@@ -163,7 +157,7 @@ public class FileMap {
 
     public static void get(String[] args) throws Exception {
         if (args.length != 2) {
-            throw new Exception("get: invalid number of arguments");
+            throw new IllegalArgumentException("get: " + message);
         }
         String key = args[1];
         String s = fm.get(key);
@@ -177,7 +171,7 @@ public class FileMap {
 
     public static void remove(String[] args) throws Exception {
         if (args.length != 2) {
-            throw new Exception("remove: invalid number of arguments");
+            throw new IllegalArgumentException("remove: " + message);
         }
         String key = args[1];
         String s = fm.remove(key);
@@ -189,24 +183,27 @@ public class FileMap {
     }
 
     public static void list(String[] args) throws Exception {
-        if (args.length != 2) {
-            throw new Exception("list: invalid number of arguments");
+        if (args.length != 1) {
+            throw new IllegalArgumentException("list: " + message);
         }
-        String key = args[1];
-        Set keySet = fm.keySet();
-        Iterator it = keySet.iterator();
-        while (it.hasNext()) {
-            System.out.print(it.next() + ", ");
+        Set<String> keySet = fm.keySet();
+        int counter = 0;
+        for (String current : keySet) {
+            ++counter;
+            System.out.print(current);
+            if (counter != keySet.size()) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
 
     public static void exit(String[] args) throws Exception {
         if (args.length != 1) {
-            throw new Exception("exit: invalid number of arguments");
+            throw new IllegalArgumentException("exit: " + message);
         }
         putFile();
-        fm.clear();
         System.exit(0);
     }
 }
+
