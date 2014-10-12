fdfc96bfe433744fcfb94a74f7a0cd0f37efcb71
Hoderu <hoderu@yandex.ru>
2014-10-13 01:04:27 +0400

    Staff changed

--- src/ru/fizteh/fivt/students/ru/fizteh/fivt/students/andrey_reshetnikov/fileMap/CommandProcess.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.ru.fizteh.fivt.students.andrey_reshetnikov.fileMap;
-
-/**
- * Created by Hoderu on 09.10.14.
- */
-public interface CommandProcess{
-    void process(CommandContainer commandFromString) throws UnknownCommand;
-}
--- src/ru/fizteh/fivt/students/ru/fizteh/fivt/students/andrey_reshetnikov/fileMap/IncorrectInputException.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.ru.fizteh.fivt.students.andrey_reshetnikov.fileMap;
-
-/**
- * Created by Hoderu on 13.10.14.
- */
-public class IncorrectInputException extends Exception {}
