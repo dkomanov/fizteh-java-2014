141d9546d1035e35fcd1f30f396e9b0351e73436
taftafa <TafTafa@gmail.com>
2014-10-12 13:36:19 +0400

    Fixed filemap again.

--- src/ru/fizteh/fivt/students/kotsurba/filemap/DbMain.java
+++ src/ru/fizteh/fivt/students/kotsurba/filemap/DbMain.java
@@ -16,13 +16,13 @@ public class DbMain {
     static final int END_OF_INPUT = -1;
     static final int END_OF_TRANSMISSION = 4;
 
-    private static boolean isTerminativeSymbol(final int character) {
+    private static boolean isTerminalSymbol(final int character) {
         return ((character == END_OF_INPUT) || (character == END_OF_TRANSMISSION));
     }
 
-    public static boolean checkTerminate(final String str) {
+    public static boolean hasTerminalSymbol(final String str) {
         for (int i = 0; i < str.length(); ++i) {
-            if (isTerminativeSymbol(str.charAt(i))) {
+            if (isTerminalSymbol(str.charAt(i))) {
                 return true;
             }
         }
@@ -61,7 +61,7 @@ public class DbMain {
 
                         String command = scanner.nextLine();
 
-                        if (checkTerminate(command)) {
+                        if (hasTerminalSymbol(command)) {
                             System.exit(0);
                         }
 
