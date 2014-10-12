003dcd7e8ccdfc0eb6abade078b0520a20cf91bb
unknown <novikov@phystech.edu>
2014-10-12 07:25:32 +0400

    WrongNuberOfArgumentsException fixed

--- src/ru/fizteh/fivt/students/dnovikov/filemap/WrongNumberOfArgumentsException.java
+++ src/ru/fizteh/fivt/students/dnovikov/filemap/WrongNumberOfArgumentsException.java
@@ -1,13 +1,8 @@
 package ru.fizteh.fivt.students.dnovikov.filemap;
 
 public class WrongNumberOfArgumentsException extends IllegalArgumentException {
-    private final String cmd;
 
     WrongNumberOfArgumentsException(String cmdName) {
-        cmd = new String(cmdName);
-    }
-
-    public String getMessage() {
-        return new String(cmd + ": wrong number of arguments");
+        super(cmdName + ": wrong number of arguments");
     }
 }
