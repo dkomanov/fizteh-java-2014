ca49a82fe62723b9186e7a6ce8b4f66359b33136
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-13 14:41:46 +0400

    Update PocketParser.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/PocketParser.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/PocketParser.java
@@ -1,45 +1,34 @@
 package ru.fizteh.fivt.students.isalysultan.FileMap;
 
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PocketParser {
-    
+
     PocketParser() {
-     // Disable instantiation to this class.
+        // Disable instantiation to this class.
     }
 
-    public static void batchMode(Table object, String[] argv) throws IOException {
+    public static void batchMode(Table object, String[] argv)
+            throws IOException {
         Scanner in = new Scanner(System.in);
-        String comand = new String();
-        int comandCount = 0;
-        Comands newComand = new Comands();
-        while (comandCount <= argv.length) {
-            switch (argv[comandCount]) {
-            case "put":
-                newComand.put(argv[comandCount + 1], argv[comandCount + 2],
-                        object);
-                comandCount = comandCount + 3;
-                break;
-            case "get":
-                newComand.get(argv[comandCount + 1], object);
-                comandCount = comandCount + 2;
-                break;
-            case "remove":
-                newComand.remove(argv[comandCount + 1], object);
-                comandCount = comandCount + 2;
-                break;
-            case "list":
-                newComand.list(object);
-                comandCount = comandCount + 1;
-                break;
-            case "exit":
-                object.writeFile();
-                System.out.println("exit");
-                return;
-            default:
-                System.out.println("Command is not recognized.");
+        int commandCount = 0;
+        Commands newCommand = new Commands();
+        StringBuilder allStringBuild = new StringBuilder();
+        for (String argument : argv) {
+            if (allStringBuild.length() != 0) {
+                allStringBuild.append(' ');
             }
+            allStringBuild.append(argument);
+        }
+        String allString = allStringBuild.toString();
+        String[] commands = allString.split(";");
+        int i = 0;
+        Parser newParser = new Parser();
+        while (i <= commands.length) {
+            String[] command = commands[i].split(" ");
+            ++i;
+            newParser.myParser(object,command);
         }
     }
 }
