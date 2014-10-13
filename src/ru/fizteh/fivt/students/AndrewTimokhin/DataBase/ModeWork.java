51ba482b2be7433ee6ff3e35b4948b1328993059
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2014-10-13 05:53:04 +0400

    Update ModeWork.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/DataBase/ModeWork.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/DataBase/ModeWork.java
@@ -3,9 +3,9 @@ package ru.fizteh.fivt.students.AndrewTimokhin.FileMap;
 import java.util.*;
 
 class ModeWork {
-    private HashMap<String, String> map;
+    private Map<String, String> map;
 
-    public ModeWork(HashMap<String, String> time) {
+    public ModeWork(Map<String, String> time) {
         map = time;
     }
 
@@ -33,23 +33,23 @@ class ModeWork {
     void usermode() {
         Functional func = new Functional(map);
         String str = new String();
-        String[] mass;
+        String[] commandAgregat;
         Scanner rd = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
             str = rd.nextLine().toString();
-            mass = str.trim().split(" ");
-            if (stepmode(mass, func, 0) == -1) {
+            commandAgregat = str.trim().split(" ");
+            if (stepmode(commandAgregat, func, 0) == -1) {
                 break;
             }
         }
 
     }
 
-    void consol(String[] mass) {
+    void interactive(String[] mass) {
         Functional func = new Functional(map);
         String str = new String();
-        String[] filtr = mass[0].toString().trim().split("\\s+");
+        String[] filtr = mass[0].trim().split("\\s+");
         int i = 0;
         while (true) {
             str = filtr[i].toString();
