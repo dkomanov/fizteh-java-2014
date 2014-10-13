94d3d2d3a37201fac6e83b15e726fab67a25a136
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-13 17:29:29 +0400

    Create CommandExecutor.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/CommandExecutor.java
@@ -0,0 +1,30 @@
+package ru.fizteh.fivt.students.isalysultan.FileMap;
+
+import java.io.IOException;
+
+public class CommandExecutor {
+    public void execute(Table tables, String[] command) throws IOException {
+        Commands newCommand = new Commands();
+        switch (command[0]) {
+        case "put":
+            newCommand.put(command[1], command[2], tables);
+            break;
+        case "get":
+            newCommand.get(command[1], tables);
+            break;
+        case "remove":
+            newCommand.remove(command[1], tables);
+            break;
+        case "list":
+            newCommand.list(tables);
+            break;
+        case "exit":
+            tables.writeFile();
+            System.out.println("exit");
+            System.exit(0);
+        default:
+            System.out.println("Command is not recognized.");
+        }
+    }
+
+}
