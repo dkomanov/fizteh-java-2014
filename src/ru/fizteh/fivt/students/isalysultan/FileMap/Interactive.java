264c9361c3437b5d157f54228a9ce0a44dc5bc3d
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-13 14:41:06 +0400

    Update Interactive.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/Interactive.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/Interactive.java
@@ -1,42 +1,24 @@
 package ru.fizteh.fivt.students.isalysultan.FileMap;
 
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Interactive {
 
     private Interactive() {
-       // Disable instantiation to this class.
+        // Disable instantiation to this class.
     }
 
     public static void interactiveMode(Table tables) throws IOException {
         Scanner in = new Scanner(System.in);
-        String[] parserComand;
-        Comands object = new Comands();
+        String[] parserCommand;
+        Commands object = new Commands();
+        Parser newParser = new Parser();
         while (true) {
-            String comand;
-            comand = in.nextLine();
-            parserComand = comand.split(" ");
-            switch (parserComand[0]) {
-            case "put":
-                object.put(parserComand[1], parserComand[2], tables);
-                break;
-            case "get":
-                object.get(parserComand[1], tables);
-                break;
-            case "remove":
-                object.remove(parserComand[1], tables);
-                break;
-            case "list":
-                object.list(tables);
-                break;
-            case "exit":
-                tables.writeFile();
-                System.out.println("exit");
-                return;
-            default:
-                System.out.println("Command is not recognized.");
-            }
+            String command;
+            command = in.nextLine();
+            parserCommand = command.split(" ");
+            newParser.myParser(tables, parserCommand);
         }
     }
 }
