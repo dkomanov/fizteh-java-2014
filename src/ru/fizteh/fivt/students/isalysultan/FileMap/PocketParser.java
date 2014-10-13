1c07914764139aabb379e82e58592a551e49a735
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-14 01:04:07 +0400

    Update PocketParser.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/PocketParser.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/PocketParser.java
@@ -2,6 +2,7 @@ package ru.fizteh.fivt.students.isalysultan.FileMap;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class PocketParser {
 
@@ -25,10 +26,23 @@ public class PocketParser {
         String[] commands = allString.split(";");
         int i = 0;
         CommandExecutor newParser = new CommandExecutor();
+        boolean firstElement = true;
         while (i <= commands.length) {
-            String[] command = commands[i].split(" ");
+            if (firstElement == true) {
+                String[] command = commands[0].split(" ");
+                newParser.execute(object, command);
+                firstElement = false;
+            } else {
+                String[] command = commands[i].trim().split(",");
+                int j = 1;
+                String newString = command[0];
+                while (j < command.length) {
+                    newString = newString + " " + command[j];
+                }
+                String[] endCommand = newString.trim().split(" ");
+                newParser.execute(object, endCommand);
+            }
             ++i;
-            newParser.execute(object, command);
         }
     }
 }
