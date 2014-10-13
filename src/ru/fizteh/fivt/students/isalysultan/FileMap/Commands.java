690ebd60d0881593626125263fcf9a7da0eb7d1f
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-13 17:25:09 +0400

    Update Commands.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/Commands.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/Commands.java
@@ -48,7 +48,7 @@ public class Commands {
     void list(Table tables) {
         Set<String> result = tables.storage.keySet();
         Iterator<String> it = result.iterator();
-        String answer =   String.join(",", result);
+        String answer = String.join(",", result);
         System.out.print(answer);
     }
 }
