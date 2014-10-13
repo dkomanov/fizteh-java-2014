bf5d9e5d922c9bfbc6c232ec5b64500f1800a53d
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-13 14:43:26 +0400

    Create Parser.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/Parser.java
@@ -0,0 +1,32 @@
+package ru.fizteh.fivt.students.isalysultan.FileMap;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Parser {
+   public void myParser(Table tables,String[] command) throws IOException {
+        Commands newCommand=new Commands();
+        switch (command[0]) {
+        case "put":
+            newCommand.put(command[1], command[2],
+                    tables);
+            break;
+        case "get":
+            newCommand.get(command[1], tables);
+            break;
+        case "remove":
+            newCommand.remove(command[1], tables);
+            break;
+        case "list":
+            newCommand.list(tables);
+            break;
+        case "exit":
+            tables.writeFile();
+            System.out.println("exit");
+            return;
+        default:
+            System.out.println(" Command is not recognized.");
+        }
+    }
+
+}
