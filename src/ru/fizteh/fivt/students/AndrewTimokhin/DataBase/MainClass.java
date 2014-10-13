5d84fa9d945eb10227ba996c9f0d48ae1e643724
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2014-10-13 05:52:36 +0400

    Update MainClass.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/DataBase/MainClass.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/DataBase/MainClass.java
@@ -7,19 +7,17 @@ public class MainClass {
 
     public static void main(String[] args) {
         Reader rd = new Reader(System.getProperty("db.file").toString());
-        HashMap<String, String> coll = new HashMap<String, String>();
+        Map<String, String> coll = new HashMap<String, String>();
         coll = rd.read();
         Set<String> show = coll.keySet();
         ModeWork mw = new ModeWork(coll);
         if (args.length == 0) {
             mw.usermode();
         } else {
-            mw.consol(args);
+            mw.interactive(args);
         }
         Functional fn = new Functional(coll);
-        fn.list();
-        Collection<String> value = coll.values();
-        Write wr = new Write(args[0]);
+        Write wr = new Write(System.getProperty("db.file").toString());
         wr.write(coll);
     }
 }
