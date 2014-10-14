66b6418a0d84bbb5e2ac90ebcb5f356ed53ee7ad
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-15 00:12:08 +0400

    Update Commands.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/Commands.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/Commands.java
@@ -6,21 +6,18 @@ import java.util.Set;
 public class Commands {
     void put(String key, String value, Table tables) {
         boolean newElement = false;
-        if (!tables.storage.containsValue(value)) {
+        if (!tables.storage.containsKey(key)) {
             System.out.println("new");
+        }
+        if (!tables.storage.containsValue(value)) {
             newElement = true;
         }
         String result = tables.storage.put(key, value);
         if (result == null) {
             return;
-        } else {
-            if ((result.equals(value))) {
-                return;
-            } else if (newElement) {
-                System.out.println("overwrite");
-                System.out.println(result);
-            }
-            return;
+        } else if (newElement && !result.equals(value)) {
+            System.out.println("overwrite");
+            System.out.println(result);
         }
     }
 
@@ -28,11 +25,9 @@ public class Commands {
         String result = tables.storage.get(key);
         if (result == null) {
             System.out.println("not found");
-            return;
         } else {
             System.out.println("found");
             System.out.println(result);
-            return;
         }
     }
 
