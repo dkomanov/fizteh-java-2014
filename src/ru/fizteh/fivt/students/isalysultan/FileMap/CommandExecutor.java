e7a076067f75d8f32f266a62567424502f1534d9
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-15 00:11:36 +0400

    Update CommandExecutor.java

--- src/ru/fizteh/fivt/students/isalysultan/FileMap/CommandExecutor.java
+++ src/ru/fizteh/fivt/students/isalysultan/FileMap/CommandExecutor.java
@@ -3,8 +3,13 @@ package ru.fizteh.fivt.students.isalysultan.FileMap;
 import java.io.IOException;
 
 public class CommandExecutor {
+    Commands newCommand;
+
+    CommandExecutor() {
+        newCommand = new Commands();
+    }
+
     public void execute(Table tables, String[] command) throws IOException {
-        Commands newCommand = new Commands();
         switch (command[0]) {
         case "put":
             newCommand.put(command[1], command[2], tables);
