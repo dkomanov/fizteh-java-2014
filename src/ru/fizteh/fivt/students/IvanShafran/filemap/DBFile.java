12633ce2f210ea3c94c79b2b203619e7cf1a8fd0
Ivan Shafran <vanobox07@mail.ru>
2014-10-15 13:42:11 +0400

    надесюь можно будет сделать merge

---
+++ src/ru/fizteh/fivt/students/IvanShafran/filemap/DBFile.java
@@ -0,0 +1,58 @@
+package ru.fizteh.fivt.students.IvanShafran.filemap;
+
+
+import java.io.*;
+import java.util.HashMap;
+
+public class DBFile {
+    private File workingFile;
+    private HashMap<String, String> hashMap;
+
+    public File getWorkingFile() throws Exception {
+        return workingFile;
+    }
+
+    public void setWorkingFile(File file) {
+        workingFile = file;
+    }
+
+    public HashMap<String, String> getHashMap() {
+            return hashMap;
+    }
+
+    DBFile(File file) {
+        setWorkingFile(file);
+    }
+
+    public void readFile() throws Exception {
+        HashMap readingHashMap = new HashMap();
+
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(workingFile))) {
+
+            while (dataInputStream.available() != 0) {
+                String key = dataInputStream.readUTF();
+                String value = dataInputStream.readUTF();
+
+                readingHashMap.put(key, value);
+            }
+        }
+        catch (Exception e) {
+            throw new Exception("file didn't read");
+        }
+
+        hashMap = readingHashMap;
+    }
+
+    public void writeHashMapToFile() throws Exception {
+        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(workingFile))) {
+            for (String key : hashMap.keySet()) {
+                dataOutputStream.writeUTF(key);
+                dataOutputStream.writeUTF(hashMap.get(key));
+            }
+        }
+        catch (Exception e) {
+            throw new Exception("error during writing string");
+        }
+    }
+
+}
---
+++ src/ru/fizteh/fivt/students/IvanShafran/filemap/FileMap.java
@@ -0,0 +1,64 @@
+package ru.fizteh.fivt.students.IvanShafran.filemap;
+
+import ru.fizteh.fivt.students.IvanShafran.filemap.commands.*;
+import ru.fizteh.fivt.students.IvanShafran.filemap.abstractShell.AbstractShell;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class FileMap extends AbstractShell {
+    private DBFile dataBaseFile;
+
+    private void checkWorkingFile() throws Exception {
+        File workingFile;
+        try {
+            workingFile =
+                    Paths.get(System.getProperty("user.dir")).resolve(System.getProperty("db.file")).toFile();
+        } catch (Exception e) {
+            throw new Exception("wrong path to db file");
+        }
+
+        if (!workingFile.exists() || workingFile.isDirectory()) {
+            throw new Exception("wrong path to db file");
+        }
+    }
+
+    private void initCommands() {
+        command = new HashMap<>();
+
+        command.put("put", new CommandPut(dataBaseFile));
+        command.put("exit", new CommandExit(dataBaseFile));
+        command.put("get", new CommandGet(dataBaseFile));
+        command.put("list", new CommandList(dataBaseFile));
+        command.put("remove", new CommandRemove(dataBaseFile));
+    }
+
+    private void initDBFile() throws Exception {
+        File workingFile;
+        try {
+            workingFile =
+                    Paths.get(System.getProperty("user.dir")).resolve(System.getProperty("db.file")).toFile();
+        } catch (Exception e) {
+            throw new Exception("wrong path to db file");
+        }
+
+        dataBaseFile = new DBFile(workingFile);
+
+        try {
+            dataBaseFile.readFile();
+        } catch (Exception e) {
+            printException("file didn't read");
+        }
+    }
+
+    FileMap() {
+        try {
+            checkWorkingFile();
+            initDBFile();
+            initCommands();
+        } catch (Exception e) {
+            printException(e.getMessage());
+        }
+    }
+}
