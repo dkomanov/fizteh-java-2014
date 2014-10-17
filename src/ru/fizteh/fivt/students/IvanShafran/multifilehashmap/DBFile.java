d88ecaf2897652b5bb8568ca59dabf9455e157ff
Ivan Shafran <vanobox07@mail.ru>
2014-10-17 18:09:44 +0400

    readUtf->самопильный велосипед

--- src/ru/fizteh/fivt/students/IvanShafran/multifilehashmap/DBFile.java
+++ src/ru/fizteh/fivt/students/IvanShafran/multifilehashmap/DBFile.java
@@ -1,4 +1,4 @@
-package ru.fizteh.fivt.students.IvanShafran.multifilehashmap;
+package ru.fizteh.fivt.students.IvanShafran.filemap;
 
 
 import java.io.*;
@@ -24,34 +24,61 @@ public class DBFile {
         setWorkingFile(file);
     }
 
+    private String readString(DataInputStream inputStream) throws Exception {
+        try {
+            int length = inputStream.readInt();
+            if (length <= 0) {
+                throw new Exception("read from database failed");
+            }
+            if (length > (1 << 20)) {
+                throw new Exception("read from database failed: string too big");
+            }
+            byte[] byteString = new byte[length];
+            inputStream.readFully(byteString);
+            return new String(byteString, "UTF-8");
+        } catch (Exception e) {
+            throw new Exception("read from database failed");
+        }
+    }
+
     public void readFile() throws Exception {
         HashMap readingHashMap = new HashMap();
 
         try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(workingFile))) {
 
             while (dataInputStream.available() != 0) {
-                String key = dataInputStream.readUTF();
-                String value = dataInputStream.readUTF();
+                String key = readString(dataInputStream);
+                String value = readString(dataInputStream);
 
                 readingHashMap.put(key, value);
             }
         }
         catch (Exception e) {
-            throw new Exception("file " + workingFile.toString() + " didn't read");
+            throw new Exception(e.getMessage());
         }
 
         hashMap = readingHashMap;
     }
 
+    private void writeString(DataOutputStream outputStream, String string) throws Exception {
+        try {
+            byte[] stringByte = string.getBytes("UTF-8");
+            outputStream.writeInt(stringByte.length);
+            outputStream.write(stringByte);
+        } catch (Exception e) {
+            throw new Exception("writing to database failed");
+        }
+    }
+
     public void writeHashMapToFile() throws Exception {
         try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(workingFile))) {
             for (String key : hashMap.keySet()) {
-                dataOutputStream.writeUTF(key);
-                dataOutputStream.writeUTF(hashMap.get(key));
+                writeString(dataOutputStream, key);
+                writeString(dataOutputStream, hashMap.get(key));
             }
         }
         catch (Exception e) {
-            throw new Exception("error during writing string");
+            throw new Exception(e.getMessage());
         }
     }
 
