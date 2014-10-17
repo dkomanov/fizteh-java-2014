f1b1d92f7cc2af6c93accdf7618f8bccadce9cf3
Ivan Shafran <vanobox07@mail.ru>
2014-10-17 17:38:37 +0400

    написал проверку сущ. ук. директории

--- src/ru/fizteh/fivt/students/IvanShafran/multifilehashmap/MultiFileHashMap.java
+++ src/ru/fizteh/fivt/students/IvanShafran/multifilehashmap/MultiFileHashMap.java
@@ -9,34 +9,28 @@ import java.util.HashMap;
 
 public class MultiFileHashMap extends AbstractShell {
 
-    private void checkWorkingFile() throws Exception {
-        File workingFile;
+    private void checkWorkingDirectory() throws Exception {
+        File workingDirectory;
         try {
-            workingFile =
-                    Paths.get(System.getProperty("user.dir")).resolve(System.getProperty("db.file")).toFile();
+            workingDirectory =
+                    Paths.get(System.getProperty("user.dir")).resolve(System.getProperty("fizteh.db.dir")).toFile();
         } catch (Exception e) {
-            throw new Exception("wrong path to db file");
+            throw new Exception("wrong path to working directory");
         }
 
-        if (!workingFile.exists() || workingFile.isDirectory()) {
-            throw new Exception("wrong path to db file");
+        if (!workingDirectory.exists()) {
+            throw new Exception("wrong path to working directory");
+        } else {
+            if (!workingDirectory.isDirectory()) {
+                throw new Exception(workingDirectory.toString() + "is not directory");
+            }
         }
     }
 
-    private void initCommands() {
-        command = new HashMap<>();
-
-        command.put("put", new CommandPut(dataBaseFile));
-        command.put("exit", new CommandExit(dataBaseFile));
-        command.put("get", new CommandGet(dataBaseFile));
-        command.put("list", new CommandList(dataBaseFile));
-        command.put("remove", new CommandRemove(dataBaseFile));
-    }
 
     MultiFileHashMap() {
         try {
-            checkWorkingFile();
-            initCommands();
+            checkWorkingDirectory();
         } catch (Exception e) {
             printException(e.getMessage());
         }
