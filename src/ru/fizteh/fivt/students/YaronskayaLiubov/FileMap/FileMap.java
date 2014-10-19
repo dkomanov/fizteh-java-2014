70fefe5973f2d558c857c4e7e02b685436e4caf4
lyaronskaya <luba.yaronskaya@gmail.com>
2014-10-20 01:28:24 +0400

    add creating files, checkstyle fixed

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/FileMap/FileMap.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/FileMap/FileMap.java
@@ -24,6 +24,13 @@ public class FileMap {
             return false;
         }
         curDB = new File(curDBPath);
+        if (!curDB.exists()) {
+            try {
+                curDB.createNewFile();
+            } catch (IOException e) {
+                System.err.println("cann't create file " + curDBPath) ;
+            }
+        }
         fileMapCommands = new HashMap<String, Command>();
         data = new HashMap<String, String>();
         fileMapCommands.put("exit", new ExitCommand());
@@ -54,7 +61,9 @@ public class FileMap {
                         continue;
                     }
                     if (!fileMapCommands.get(curCommand).execute(
-                            argv)) { errorOccurred = true; }
+                            argv)) {
+                        errorOccurred = true;
+                    }
                 }
             }
         } else {
