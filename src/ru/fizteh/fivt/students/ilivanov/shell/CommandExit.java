250397f955f9e806515787a7537f9f4d04b55299
ilivanov <ilivanov.se@ya.ru>
2014-10-21 21:28:05 +0400

    deleted Shell

--- src/ru/fizteh/fivt/students/ilivanov/shell/CommandExit.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.ilivanov.shell;
-
-public class CommandExit implements Command {
-    @Override
-    public int execute() {
-        Shell.exit();
-        return 0;
-    }
-}
