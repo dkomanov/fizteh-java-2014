983d5596508410b7845572487e760676ce623ad1
ryad0m <m.ryadom@gmail.com>
2014-10-21 15:02:35 +0000

    No param fix

--- src/ru/fizteh/fivt/students/ryad0m/filemap/Main.java
+++ src/ru/fizteh/fivt/students/ryad0m/filemap/Main.java
@@ -5,10 +5,15 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    public static TableNode tableNode = new TableNode(
-            Paths.get(System.getProperty("db.file")).toAbsolutePath().normalize());
+    public static TableNode tableNode;
 
     public static void main(String[] args) {
+        if (System.getProperty("db.file") == null) {
+            System.out.println("Param db.file not set");
+            System.exit(1);
+        }
+        tableNode = new TableNode(
+                Paths.get(System.getProperty("db.file")).toAbsolutePath().normalize());
         Shell shell = new Shell();
         try {
             Main.tableNode.load();
