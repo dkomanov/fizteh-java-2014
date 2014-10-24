cc7b410bc515f412ea0534d0d889325f39f9a78e
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-10-24 16:00:57 +0400

    Add command factory and main. Grand troubleshoot ahead

---
+++ src/ru/fizteh/fivt/students/moskupols/multifilehashmap/MultiFileMapMain.java
@@ -0,0 +1,44 @@
+package ru.fizteh.fivt.students.moskupols.multifilehashmap;
+
+import ru.fizteh.fivt.students.moskupols.cliutils.*;
+
+import java.nio.file.Paths;
+
+/**
+ * Created by moskupols on 19.10.14.
+ */
+public class MultiFileMapMain {
+    public static final String DB_DIR_PROPERTY = "fizteh.db.dir";
+
+    public static void main(String[] args) {
+        String dbPath = System.getProperty(DB_DIR_PROPERTY);
+        if (dbPath == null) {
+            System.err.println(String.format("Specify database file in property %s.", DB_DIR_PROPERTY));
+            System.exit(1);
+        }
+
+        final MultiFileHashMapCommandFactory commandFactory;
+        try {
+            commandFactory = new MultiFileHashMapCommandFactory(
+                    new DataBaseCursor(),
+                    new MultiFileMapProvider(Paths.get(dbPath)));
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        CommandProcessor commandProcessor;
+        if (args.length == 0) {
+            commandProcessor = new InteractiveCommandProcessor("$ ");
+        } else {
+            commandProcessor = new PackageCommandProcessor(args);
+        }
+
+        try {
+            commandProcessor.process(commandFactory);
+        } catch (CommandExecutionException | UnknownCommandException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
