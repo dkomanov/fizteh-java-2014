3be7bfa0d755eb593dd1a7d7eeb8db564de7fb8b
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-24 23:09:20 +0400

    Create InteractiveMode.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/MultiFIleHashMap/InteractiveMode.java
@@ -0,0 +1,20 @@
+package ru.fizteh.fivt.students.isalysultan.MultiFileHashMap;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InteractiveMode {
+
+    public static void Interactive(RootDirectory direct) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String[] parserCommand;
+        CommandInterpretator newParser = new CommandInterpretator();
+        while (true) {
+            String command;
+            System.out.print("$ ");
+            command = in.nextLine();
+            parserCommand = command.split(" ");
+            newParser.executeCommand(direct, parserCommand);
+        }
+    }
+}
