2103b50eca4733f214927f68bbb8c614324d8ee8
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-24 23:10:34 +0400

    Create MainMultiFileHashMap.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/MultiFIleHashMap/MainMultiFileHashMap.java
@@ -0,0 +1,15 @@
+package ru.fizteh.fivt.students.isalysultan.MultiFileHashMap;
+
+import java.io.IOException;
+
+public class MainMultiFileHashMap {
+
+    public static void main(String[] argv) throws IOException {
+        RootDirectory newDirect = new RootDirectory();
+        if (argv.length == 0) {
+            InteractiveMode.Interactive(newDirect);
+        } else {
+            BatchMode.BatchParser(newDirect, argv);
+        }
+    }
+}
