b7141a705f2cb7771953280ad5ebbedf995a2d2c
IgorTitov396 <1hp_phoenix@mail.ru>
2014-10-25 06:11:50 +0400

    Create ArrayOfHashMaps.java
    v1.0

---
+++ src/ru/fizteh/fivt/students/titov/multifilehashmap/ArrayOfHashMaps.java
@@ -0,0 +1,9 @@
+package ru.fizteh.fivt.students.titov.multifilehashmap;
+
+import java.util.HashMap;
+
+public class ArrayOfHashMaps {
+    
+    public HashMap<String, String> arrayOfHashMaps;
+
+}
