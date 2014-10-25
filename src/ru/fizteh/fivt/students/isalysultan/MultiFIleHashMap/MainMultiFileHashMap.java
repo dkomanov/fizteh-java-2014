d7c6fdf081889029f172c733ed5e3da59d68c726
IsalySultan <IsalySultan@users.noreply.github.com>
2014-10-25 23:52:59 +0400

    Update MainMultiFileHashMap.java

--- src/ru/fizteh/fivt/students/isalysultan/MultiFIleHashMap/MainMultiFileHashMap.java
+++ src/ru/fizteh/fivt/students/isalysultan/MultiFIleHashMap/MainMultiFileHashMap.java
@@ -7,9 +7,10 @@ public class MainMultiFileHashMap {
     public static void main(String[] argv) throws IOException {
         RootDirectory newDirect = new RootDirectory();
         if (argv.length == 0) {
-            InteractiveMode.Interactive(newDirect);
+            InteractiveMode.interactive(newDirect);
         } else {
             BatchMode.batchParser(newDirect, argv);
         }
     }
 }
+
