ed140acb857c8789d24cfbdeb6897c34bc3dbaa6
IgorTitov396 <1hp_phoenix@mail.ru>
2014-10-26 23:37:24 +0300

    Update MultiFileHashMap.java
    v1.6.2

--- src/ru/fizteh/fivt/students/titov/multifilehashmap/MultiFileHashMap.java
+++ src/ru/fizteh/fivt/students/titov/multifilehashmap/MultiFileHashMap.java
@@ -16,7 +16,7 @@ public class MultiFileHashMap {
 
     }
 
-    private static final Integer defaultValue = 16;
+    private static Integer defaultValue = 16;
     private static File table;
     private static String nameOfDir = "";
     private static ArrayOfHashMaps[][] arrayOfHashMaps;
