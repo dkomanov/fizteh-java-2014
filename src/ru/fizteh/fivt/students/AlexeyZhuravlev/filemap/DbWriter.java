191f903835e5b82b058cc4bc253b22b3c15efd0f
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-10-28 18:26:06 +0300

    +some checkstyle

--- src/ru/fizteh/fivt/students/AlexeyZhuravlev/filemap/DbWriter.java
+++ src/ru/fizteh/fivt/students/AlexeyZhuravlev/filemap/DbWriter.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /**
  * @author AlexeyZhuravlev
  */
-class DbWriter implements AutoCloseable{
+class DbWriter implements AutoCloseable {
 
     private DataOutputStream stream;
 
