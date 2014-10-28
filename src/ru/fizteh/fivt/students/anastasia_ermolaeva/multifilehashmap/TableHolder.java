ca1b34c41f4acdd890686389cc278851f0598309
skye17 <skye17@users.noreply.github.com>
2014-10-28 21:14:38 +0400

    Update TableHolder.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/multifilehashmap/TableHolder.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/multifilehashmap/TableHolder.java
@@ -11,11 +11,14 @@ import java.util.Map;
 import java.util.Set;
 
 
-public class TableHolder implements Map<String, Table>, AutoCloseable {
+public class TableHolder implements AutoCloseable {
     private Path rootPath;
     private Map<String, Table> tableMap;
 
-    public TableHolder(final Path rootPath) throws ExitException {
+    public Path getRootPath(){
+        return rootPath;
+    }
+    public TableHolder(final Path rootPath) {
         this.rootPath = rootPath;
         this.tableMap = new HashMap<>();
         File rootDirectory = rootPath.toFile();
@@ -24,7 +27,8 @@ public class TableHolder implements Map<String, Table>, AutoCloseable {
             File currentDir = new File(rootPath.toAbsolutePath().toString()
                     + File.separator + s);
             if (!currentDir.isDirectory()) {
-                System.err.println("Child directory is not a dir");
+                System.err.println("Child directories are not directories");
+                System.exit(1);
             } else {
                 String tableName = currentDir.getName();
                 tableMap.put(tableName, new Table(rootPath, tableName));
@@ -35,70 +39,10 @@ public class TableHolder implements Map<String, Table>, AutoCloseable {
         return tableMap;
     }
     @Override
-    public void close() throws Exception {
+    public void close() throws ExitException {
         for (Map.Entry<String, Table> entry : tableMap.entrySet()) {
             entry.getValue().close();
         }
         tableMap.clear();
-    }
-
-    @Override
-    public int size() {
-        return tableMap.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return tableMap.isEmpty();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return tableMap.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return tableMap.containsValue(value);
-    }
-
-    @Override
-    public Table get(Object key) {
-        return tableMap.get(key);
-    }
-
-    @Override
-    public Table put(String key, Table value) {
-        return tableMap.put(key, value);
-    }
-
-    @Override
-    public Table remove(Object key) {
-        return tableMap.remove(key);
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends Table> m) {
-        tableMap.putAll(m);
-    }
-
-    @Override
-    public void clear() {
-        tableMap.clear();
-    }
-
-    @Override
-    public Set<String> keySet() {
-        return tableMap.keySet();
-    }
-
-    @Override
-    public Collection<Table> values() {
-        return tableMap.values();
-    }
-
-    @Override
-    public Set<Entry<String, Table>> entrySet() {
-        return tableMap.entrySet();
     }
 }
