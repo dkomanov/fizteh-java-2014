6699ec25297a9f00e3f03b7d2350904f74ff7d69
TheronSG <Ilinie@yandex.ru>
2014-10-30 01:17:27 +0300

    Update Table.java

--- src/ru/fizteh/fivt/students/ilin_ilia/multifilehashmap/Table.java
+++ src/ru/fizteh/fivt/students/ilin_ilia/multifilehashmap/Table.java
@@ -1,7 +1,6 @@
 package ru.fizteh.fivt.students.theronsg.multifilehashmap;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Paths;
