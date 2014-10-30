c7df6638d5f35e4aae9ddf06c5abfadf965a8347
AndreosGhost <andreos-ghost@ya.ru>
2014-10-30 16:25:55 +0300

    Rollback changes in interfaces

--- src/ru/fizteh/fivt/storage/strings/TableProviderFactory.java
+++ src/ru/fizteh/fivt/storage/strings/TableProviderFactory.java
@@ -1,20 +1,18 @@
 package ru.fizteh.fivt.storage.strings;
-
 /**
- * Представляет интерфейс для создание экземпляров {@link ru.fizteh.fivt.storage.strings.TableProvider}.
- * Предполагается, что реализация интерфейса фабрики будет иметь публичный конструктор без
- * параметров.
+ * Представляет интерфейс для создание экземпляров {@link TableProvider}. Предполагается, что реализация интерфейса
+ * фабрики будет иметь публичный конструктор без параметров.
+ *
  * @author Fedor Lavrentyev
  * @author Dmitriy Komanov
  */
 public interface TableProviderFactory {
     /**
      * Возвращает объект для работы с базой данных.
-     * @param dir
-     *         Директория с файлами базы данных.
+     *
+     * @param dir Директория с файлами базы данных.
      * @return Объект для работы с базой данных.
-     * @throws IllegalArgumentException
-     *         Если значение директории null или имеет недопустимое значение.
+     * @throws IllegalArgumentException Если значение директории null или имеет недопустимое значение.
      */
-    TableProvider create(String dir) throws IllegalArgumentException;
+    TableProvider create(String dir);
 }
