a8f9f927efbf8829b50acc81a6524f7a1d3bad19
skye17 <skye17@users.noreply.github.com>
2014-11-02 23:54:42 +0400

    Update TableState.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/TableState.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/TableState.java
@@ -1,38 +1,21 @@
 package ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util;
 
 import ru.fizteh.fivt.storage.strings.TableProvider;
-import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.DBTable;
-import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.TableHolder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TableState {
-    //private final Map<String,
-      //      Map<String, String>> map;
     private String currentTableName = "";
     private TableProvider holder;
 
     public TableState(final TableProvider tableHolder) {
         holder = tableHolder;
-        //map = new HashMap<>();
-        //Map<String, DbTable> m = tableHolder.getTableMap();
-        //for (Map.Entry<String, DbTable> entry : m.entrySet()) {
-         //   String key = entry.getKey();
-          //  Map<String, String> value = entry.getValue().getAllRecords();
-          //  map.put(key, value);
-        //}
     }
-    public TableProvider getTableHolder(){
+    public final TableProvider getTableHolder() {
         return holder;
     }
-    //public Map<String, Map<String, String>> getMap() {
-       // return map;
-    //}
-    public String getCurrentTableName() {
+    public final String getCurrentTableName() {
         return currentTableName;
     }
-    public void setCurrentTableName(String currentTableName) {
+    public final void setCurrentTableName(final String currentTableName) {
         this.currentTableName = currentTableName;
     }
 }
