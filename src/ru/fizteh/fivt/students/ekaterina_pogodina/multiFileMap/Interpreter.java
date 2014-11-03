fcf485e30269195b846655ba8f2167be892c91ee
EkaterinaPogodina <pogodina@phystech.edu>
2014-11-03 16:42:33 +0300

    multifile

--- src/ru/fizteh/fivt/students/ekaterina_pogodina/multiFileMap/Interpreter.java
+++ src/ru/fizteh/fivt/students/ekaterina_pogodina/multiFileMap/Interpreter.java
@@ -9,46 +9,30 @@ import java.util.Scanner;
 public class Interpreter {
     private String dir;
     public Interpreter() throws Exception {
-        List<String> cmdWithArgs = new ArrayList<String>();
         dir = System.getProperty("fizteh.db.dir");
         TableManager table = new TableManager(dir);
         try {
-            try (Scanner sc = new Scanner(System.in)) {
-                while (true) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                do {
                     System.out.print("$ ");
-                    String line = "";
-                    if (sc.hasNext()) {
-                        line = sc.nextLine();
-                    } else {
-                        System.exit(0);
-                    }
-                    String[] commands = line.split(" ; ");
-                    for (String command: commands) {
-                        int index;
-                        index = 0;
-                        for (int i = 0; i < command.length(); i++) {
-                            if (command.charAt(i) == ' ') {
-                                cmdWithArgs.add(command.substring(index, i));
-                                index = i + 1;
+                    String[] input = scanner.nextLine().split(";");
+                    for (int i = 0; i < input.length; ++i) {
+                        if (input[i].length() > 0) {
+                            String[] buffer = input[i].trim().split("\\s+");
+                            try {
+                                Parser.parse(buffer, table);
+                            } catch (Exception exception) {
+                                System.err.println(exception.getMessage());
                             }
                         }
-                        cmdWithArgs.add(command.substring(index, command.length()));
-                        String[] arg = new String[cmdWithArgs.size()];
-                        for (int i = 0; i < arg.length; i++) {
-                            arg[i] = cmdWithArgs.get(i);
-                        }
-                        try {
-                            Parser.parse(arg, table);
-                        } catch (Exception e) {
-                            System.err.println(e.getMessage());
-                        }
-                        cmdWithArgs.clear();
                     }
-                }
+                } while(true);
             } catch (Exception e) {
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
+            scanner.close();
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
@@ -62,50 +46,21 @@ public class Interpreter {
             table = new TableManager(System.getProperty("fizteh.db.dir"));
             if (args.length > 0) {
                 try {
-                    String line = "";
-                    for (int i = 0; i < args.length; i++) {
-                        line = line.concat(args[i]);
-                        if (i != args.length - 1) {
-                            line = line.concat(" ");
-                        }
+                    StringBuilder helpArray = new StringBuilder();
+                    for (int i = 0; i < args.length; ++i) {
+                        helpArray.append(args[i]).append(' ');
                     }
-                    String[] commands = line.split(" ; ");
-                    for (String command: commands) {
-                        int index;
-                        index = 0;
-                        for (int i = 0; i < command.length(); i++) {
-                            if (command.charAt(i) == ' ') {
-                                cmdWithArgs.add(command.substring(index, i));
-                                String[] buff = cmdWithArgs.get(cmdWithArgs.size() - 1).trim().split("\\s+");
-                                cmdWithArgs.remove(cmdWithArgs.size() - 1);
-                                for (int k = 0; k < buff.length; k++) {
-                                    if (!buff[k].equals(" ")) {
-                                        cmdWithArgs.add(buff[k]);
-                                    }
-                                }
-                                index = i + 1;
+                    String longStr = helpArray.toString();
+                    String[] input = longStr.split(";");
+                    for (int i = 0; i < input.length; ++i) {
+                        if (input[i].length() > 0) {
+                            String[] buffer = input[i].trim().split("\\s+");
+                            try {
+                                Parser.parse(buffer, table);
+                            } catch (Exception exception) {
+                                System.err.println(exception.getMessage());
                             }
                         }
-                        cmdWithArgs.add(command.substring(index, command.length()));
-                        String[] arg = new String [cmdWithArgs.size()];
-                        for (int i = 0; i < arg.length; i++) {
-                            arg[i] = cmdWithArgs.get(i);
-                        }
-                        try {
-                            Parser.parse(arg, table);
-                            BaseTable curTable = table.tables.get(table.currentTable);
-                            for (int i = 0; i < 16; i++) {
-                                            for (int j = 0; j < 16; j++) {
-                                                if (curTable.tableDateBase[i][j] != null) {
-                                                    curTable.tableDateBase[i][j].close();
-                                    }
-                                }
-                            }
-                        } catch (Exception e) {
-                            System.err.println(e.getMessage());
-                            System.exit(1);
-                        }
-                        cmdWithArgs.clear();
                     }
                 } catch (Exception e) {
                     System.err.println(e.getMessage());
