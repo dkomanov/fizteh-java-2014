e5e36b06fb9b820c50c1e0303d438f3f8b479acc
Ivan Ivanov <ivan.k.ivanob@phystech.edu>
2014-11-04 16:48:34 +0300

    Fixed show tables

--- src/ru/fizteh/fivt/students/ivan_ivanov/multifilehashmap/CmdShowTables.java
+++ src/ru/fizteh/fivt/students/ivan_ivanov/multifilehashmap/CmdShowTables.java
@@ -3,7 +3,9 @@ package ru.fizteh.fivt.students.ivan_ivanov.multifilehashmap;
 import ru.fizteh.fivt.students.ivan_ivanov.shell.Command;
 import ru.fizteh.fivt.students.ivan_ivanov.shell.Shell;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 public class CmdShowTables implements Command {
@@ -23,6 +25,18 @@ public class CmdShowTables implements Command {
         if (!args[0].equals("tables")) {
             throw new IOException("Can't find key");
         }
+        
+        if (((MultiFileHashMap) shell).getMFHMState().getFlag() == 1) {
+            File fileForWrite = ((MultiFileHashMapTable)
+                    ((MultiFileHashMap) shell).getMFHMState().getCurrentTable()).getDataFile();
+            Map<String, String> mapForWrite = ((MultiFileHashMapTable)
+                    ((MultiFileHashMap) shell).getMFHMState().getCurrentTable()).getDataBase();
+            MultiFileHashMapUtils.write(fileForWrite, mapForWrite);
+        }
+
+        /*if (((MultiFileHashMap) shell).getMFHMState().getFlag() == 0) {
+            ((MultiFileHashMap) shell).getMFHMState().changeFlag();
+        }*/
 
         System.out.println("table_name row_count");
 
