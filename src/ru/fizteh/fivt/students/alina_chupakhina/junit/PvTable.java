6e9ef7a1683b1164a43c8abd4744ef7a29bd4f0c
chav0 <chav0@mail.ru>
2014-11-04 15:19:34 +0300

    CheckStyle

--- src/ru/fizteh/fivt/students/alina_chupakhina/junit/PvTable.java
+++ src/ru/fizteh/fivt/students/alina_chupakhina/junit/PvTable.java
@@ -25,52 +25,62 @@ public class PvTable implements TableProvider {
      * @throws IllegalArgumentException Если название таблицы null или имеет недопустимое значение.
      */
     //@Override
-    public BdTable getTable(String name) throws Exception {
+    public BdTable getTable(String name) {
         BdTable t = new BdTable(name, JUnit.path);
-        File[] dirs = t.table.listFiles();
-        for (File dir : dirs) {
-            if (!dir.isDirectory()) {
-                throw new Exception(dir.getName()
-                        + " is not directory");
-            }
-            File[] dats = dir.listFiles();
-            if (dats.length == 0) {
-                System.err.println("Empty folders found");
-                System.exit(-1);
-            }
-            for (File dat : dats) {
-                int nDirectory = Integer.parseInt(dir.getName().substring(0,
-                        dir.getName().length() - 4));
-                int nFile = Integer.parseInt(dat.getName().substring(0,
-                        dat.getName().length() - 4));
-                String key;
-                String value;
-                RandomAccessFile file
-                        = new RandomAccessFile(dat.getAbsolutePath(), "r");
-                boolean end = false;
-                while (!end) {
+        try {
+            File[] dirs = t.table.listFiles();
+            for (File dir : dirs) {
+                if (!dir.isDirectory()) {
                     try {
-                        int length = file.readInt();
-                        byte[] bytes = new byte[length];
-                        file.readFully(bytes);
-                        key = new String(bytes, "UTF-8");
-                        length = file.readInt();
-                        bytes = new byte[length];
-                        file.readFully(bytes);
-                        value = new String(bytes, "UTF-8");
-                        t.numberOfElements++;
-                        t.fm.put(key, value);
-                        if (!(nDirectory ==  Math.abs(key.getBytes("UTF-8")[0] % MAGIC_NUMBER))
-                                || !(nFile == Math.abs((key.getBytes("UTF-8")[0] / MAGIC_NUMBER) % MAGIC_NUMBER))) {
-                            System.err.println("Error while reading table " + t.tablename);
-                            System.exit(-1);
-                        }
-                    } catch (IOException e) {
-                        end = true;
+                        throw new Exception(dir.getName()
+                                + " is not directory");
+                    } catch (Exception e) {
+                        System.err.println(e.getMessage());
+                        System.exit(-1);
                     }
                 }
-                file.close();
+                File[] dats = dir.listFiles();
+                if (dats.length == 0) {
+                    System.err.println("Empty folders found");
+                    System.exit(-1);
+                }
+                for (File dat : dats) {
+                    int nDirectory = Integer.parseInt(dir.getName().substring(0,
+                            dir.getName().length() - 4));
+                    int nFile = Integer.parseInt(dat.getName().substring(0,
+                            dat.getName().length() - 4));
+                    String key;
+                    String value;
+                    RandomAccessFile file
+                            = new RandomAccessFile(dat.getAbsolutePath(), "r");
+                    boolean end = false;
+                    while (!end) {
+                        try {
+                            int length = file.readInt();
+                            byte[] bytes = new byte[length];
+                            file.readFully(bytes);
+                            key = new String(bytes, "UTF-8");
+                            length = file.readInt();
+                            bytes = new byte[length];
+                            file.readFully(bytes);
+                            value = new String(bytes, "UTF-8");
+                            t.numberOfElements++;
+                            t.fm.put(key, value);
+                            if (!(nDirectory == Math.abs(key.getBytes("UTF-8")[0] % MAGIC_NUMBER))
+                                    || !(nFile == Math.abs((key.getBytes("UTF-8")[0] / MAGIC_NUMBER) % MAGIC_NUMBER))) {
+                                System.err.println("Error while reading table " + t.tablename);
+                                System.exit(-1);
+                            }
+                        } catch (IOException e) {
+                            end = true;
+                        }
+                    }
+                    file.close();
+                }
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage()); 
+            System.exit(-1);
         }
         return t;
     }
@@ -82,7 +92,7 @@ public class PvTable implements TableProvider {
      * @return Объект, представляющий таблицу. Если таблица уже существует, возвращает null.
      * @throws IllegalArgumentException Если название таблицы null или имеет недопустимое значение.
      */
-    public Table createTable(String name) throws Exception {
+    public Table createTable(String name) {
         String pathToTable = JUnit.path + File.separator + name;
         File table = new File(pathToTable);
         if (table.exists() && table.isDirectory()) {
@@ -102,7 +112,7 @@ public class PvTable implements TableProvider {
      * @throws IllegalArgumentException Если название таблицы null или имеет недопустимое значение.
      * @throws IllegalStateException Если таблицы с указанным названием не существует.
      */
-    public void removeTable(String name) throws Exception {
+    public void removeTable(String name) {
         if (name == null) {
             throw new IllegalArgumentException ("Name table is null");
         }
