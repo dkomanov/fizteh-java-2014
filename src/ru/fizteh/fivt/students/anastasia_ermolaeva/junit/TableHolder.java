e6a23f6160c478dff7be84885ceabf1034cae57b
skye17 <skye17@users.noreply.github.com>
2014-11-05 22:23:06 +0400

    Update TableHolder.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/TableHolder.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/TableHolder.java
@@ -3,6 +3,8 @@ package ru.fizteh.fivt.students.anastasia_ermolaeva.junit;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
