30f6090ecd8f2e43f08c8c2c8faabc217b6483fd
skye17 <skye17@users.noreply.github.com>
2014-11-05 14:17:41 +0400

    Update ExitException.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/multifilehashmap/util/ExitException.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/multifilehashmap/util/ExitException.java
@@ -2,10 +2,19 @@ package ru.fizteh.fivt.students.anastasia_ermolaeva.multifilehashmap.util;
 
 public class ExitException extends Exception {
     private final int status;
-    public  final int getStatus() {
+    private final String errMessage;
+
+    public final int getStatus() {
         return status;
     }
+
+    public ExitException(final String message, final int exitStatus) {
+        errMessage = message;
+        status = exitStatus;
+    }
+
     public ExitException(final int exitStatus) {
+        errMessage = "Error";
         status = exitStatus;
     }
 }
