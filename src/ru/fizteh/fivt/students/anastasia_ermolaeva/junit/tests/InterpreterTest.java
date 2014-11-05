e2d58f4e126f2d919af82ed625961f061eaf1344
skye17 <skye17@users.noreply.github.com>
2014-11-05 21:44:01 +0400

    Update InterpreterTest.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/tests/InterpreterTest.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/tests/InterpreterTest.java
@@ -4,13 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util.Command;
-import ru.fizteh.fivt.students.anastasia_ermolaeva.multifilehashmap.util.ExitException;
+import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util.ExitException;
 import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util.Interpreter;
 import ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util.TableState;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -19,40 +21,44 @@ public class InterpreterTest {
     private final String testCommand = "command";
     private final String testOutput = "TEST";
     private ByteArrayOutputStream outputStream;
+    private ByteArrayOutputStream errputStream;
     private PrintStream printStream;
+    private PrintStream printErrStream;
 
     @Before
     public void setUp() {
         outputStream = new ByteArrayOutputStream();
+        errputStream = new ByteArrayOutputStream();
         printStream = new PrintStream(outputStream);
+        printErrStream = new PrintStream(errputStream);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInterpreterThrowsIllegalArgumentExceptionIfGivenNullStreams() {
-        new Interpreter(null, new Command[] {}, null, null);
+        new Interpreter(null, new Command[]{}, null, null, null);
     }
 
     @Test
     public void testRunValidUserMode() {
-        Interpreter test = new Interpreter(null, new Command[] {
+        Interpreter test = new Interpreter(null, new Command[]{
                 new Command("command", 1, (TableState tableS, String[] arguments) -> printStream.println(testOutput))},
                 new ByteArrayInputStream(
-                (testCommand + newLine).getBytes()), printStream);
+                        (testCommand + newLine).getBytes()), printStream, printErrStream);
         try {
-            test.run(new String[] {});
+            test.run(new String[]{});
         } catch (ExitException e) {
             assertEquals(0, e.getStatus());
             assertEquals(test.PROMPT + testOutput + newLine + test.PROMPT,
-                    outputStream.toString());
+                   outputStream.toString());
         }
 
     }
 
     @Test
     public void testRunValidBatchMode() {
-        Interpreter test = new Interpreter(null, new Command[] {
+        Interpreter test = new Interpreter(null, new Command[]{
                 new Command("command", 1, (TableState tableS, String[] arguments) -> printStream.println(testOutput))},
-                new ByteArrayInputStream(new byte[] {}), printStream);
+                new ByteArrayInputStream(new byte[]{}), printStream, printErrStream);
         try {
             test.run(new String[]{testCommand + test.STATEMENT_DELIMITER, testCommand});
         } catch (ExitException e) {
@@ -62,21 +68,21 @@ public class InterpreterTest {
     }
 
     @Test
-    public void testBatchModeThrowsExitExceptionWithNonZeroStatusForUnexpectedCommand() {
-        Interpreter test = new Interpreter(null, new Command[] {},
-                new ByteArrayInputStream(new byte[] {}), printStream);
+    public void testBatchModePrintErrorMessageInErrStreamForUnexpectedCommand() {
+        Interpreter test = new Interpreter(null, new Command[]{},
+                new ByteArrayInputStream(new byte[]{}), printStream, printErrStream);
         try {
             test.run(new String[]{testCommand});
         } catch (ExitException e) {
             assertNotEquals(0, e.getStatus());
-            assertEquals(test.ERR_MSG + testCommand + newLine, outputStream.toString());
+            assertEquals(test.ERR_MSG + testCommand + newLine, errputStream.toString());
         }
     }
 
     @Test
     public void testUserModeWriteErrorInStreamForUnexpectedCommand() {
-        Interpreter test = new Interpreter(null, new Command[] {},
-                new ByteArrayInputStream((testCommand + newLine).getBytes()), printStream);
+        Interpreter test = new Interpreter(null, new Command[]{},
+                new ByteArrayInputStream((testCommand + newLine).getBytes()), printStream, printErrStream);
         try {
             test.run(new String[]{});
         } catch (ExitException e) {
@@ -87,20 +93,34 @@ public class InterpreterTest {
     }
 
     @Test
-    public void testBatchModeThrowsExitExceptionWithNonZeroStatusForCommandWithWrongNumberOfArguments() {
-        Interpreter test = new Interpreter(null, new Command[] {
+    public void testBatchModePrintErrorMessageInErrStreamForCommandWithWrongNumberOfArguments() {
+        Interpreter test = new Interpreter(null, new Command[]{
                 new Command("command", 1, (TableState tableS, String[] arguments) -> printStream.println(testOutput))},
-                new ByteArrayInputStream(new byte[] {}), printStream);
+                new ByteArrayInputStream(new byte[]{}), printStream, printErrStream);
         try {
             test.run(new String[]{testCommand + " argument"});
         } catch (ExitException e) {
+            assertEquals("Invalid number of arguments: 1 expected, 2 found." + newLine, errputStream.toString());
             assertNotEquals(0, e.getStatus());
         }
     }
-
+    @Test
+    public void testUserModePrintErrorMessageInOutStreamForCommandWithWrongNumberOfArguments() {
+        Interpreter test = new Interpreter(null, new Command[]{
+                new Command("command", 1, (TableState tableS, String[] arguments) -> printStream.println(testOutput))},
+                new ByteArrayInputStream((testCommand + " argument").getBytes()), printStream, printErrStream);
+        try {
+            test.run(new String[]{});
+        } catch (ExitException e) {
+            assertEquals(test.PROMPT + "Invalid number of arguments: 1 expected, 2 found." + newLine + test.PROMPT,
+                    outputStream.toString());
+        }
+    }
     @After
     public void tearDown() throws IOException {
         outputStream.close();
         printStream.close();
+        errputStream.close();
+        printErrStream.close();
     }
 }
