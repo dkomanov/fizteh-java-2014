b8bf3591145e74aad52fbd737415a4960ed3acc5
skye17 <skye17@users.noreply.github.com>
2014-11-05 14:35:07 +0400

    Update DBTable.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/DBTable.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/DBTable.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Collections;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
