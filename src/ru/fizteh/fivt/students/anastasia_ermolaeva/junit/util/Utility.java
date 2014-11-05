3bdb542259736716506a79af24d656b96f877ddf
skye17 <skye17@users.noreply.github.com>
2014-11-05 23:11:41 +0400

    Update Utility.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/Utility.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/Utility.java
@@ -1,14 +1,18 @@
 package ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Utility {
     public static void checkTableName(final String name) {
         if (name == null || name.contains(".")
-                || name.contains("\\") || name.contains("/")) {
+                || name.contains("\\") || name.contains("/"))
             throw new IllegalArgumentException("Table name is null or invalid");
-                }
     }
 
     public static void checkDirectorySubdirs(Path directory) {
@@ -20,6 +24,35 @@ public class Utility {
                             + "are not directories");
                 }
             }
+        }
+    }
+
+    public static void  recursiveDelete(Path directory) {
+        try {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException e)
+                        throws IOException {
+                    if (e == null) {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    } else {
+                            /*
+                             * Directory iteration failed.
+                              */
+                        throw e;
+                    }
+                }
+            });
+        } catch (IOException | SecurityException e) {
+            throw new DatabaseIOException(e.getMessage());
         }
     }
 }
