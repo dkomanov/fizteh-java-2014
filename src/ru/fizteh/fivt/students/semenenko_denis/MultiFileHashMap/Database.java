dbaabc9fa60a76e3b486d32b7c3ca8c24d8475b4
Semenenko Denix <semenenko@phystech.edu>
2014-11-06 21:12:11 +0300

    Trying to add files (have problem)

---
+++ src/ru/fizteh/fivt/students/semenenko_denis/MultiFileHashMap/Database.java
@@ -0,0 +1,156 @@
+package ru.fizteh.fivt.students.semenenko_denis.MultiFileHashMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javafx.util.Pair;
+import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.storage.strings.TableProvider;
+
+public class Database implements TableProvider {
+    private static final String CANT_CREATE_TABLE = "Can't create table";
+    private static final String CANT_CREATE_TABLE_MESSAGE = CANT_CREATE_TABLE + ": ";
+    private static final String INCORRECT_NAME_OF_TABLES = "This name is not correct, folder can't be created";
+    private File rootDirectory;
+    private Table usingTable;
+    private List<TableHash> tables = new ArrayList<>();
+    private Map<String, Table> tableNames = new TreeMap<>();
+
+    public Database(String path) throws DatabaseFileStructureException, LoadOrSaveException {
+        setRootDirectoryPath(path);
+        load();
+    }
+
+    Table getUsingTable() {
+        return usingTable;
+    }
+
+    void setUsingTable(String name) {
+        usingTable = getTable(name);
+    }
+
+    protected File getRootDirectory() throws DatabaseFileStructureException {
+        return rootDirectory;
+    }
+
+    protected Path getRootDirectoryPath() throws DatabaseFileStructureException {
+        return getRootDirectory().toPath();
+    }
+
+    private void setRootDirectoryPath(String directoryPath) throws DatabaseFileStructureException {
+        rootDirectory = new File(directoryPath);
+    }
+
+    public boolean containsTable(String name) {
+        return tableNames.containsKey(name);
+    }
+
+    protected void load() throws LoadOrSaveException, DatabaseFileStructureException {
+        File root = getRootDirectory();
+        try {
+            if (root.exists() && root.isDirectory()) {
+                File[] subfolders = getTablesFromRoot(root);
+                for (File folder : subfolders) {
+                    String name = folder.getName();
+                    TableHash table = new TableHash(name, this);
+                    tables.add(table);
+                    tableNames.put(name, table);
+                }
+            } else {
+                throw new DatabaseFileStructureException("Root directory not found");
+            }
+        } catch (SecurityException ex) {
+            throw new LoadOrSaveException("Error in loading, access denied: " + ex.getMessage(), ex);
+        }
+    }
+
+    protected File[] getTablesFromRoot(File root) throws DatabaseFileStructureException {
+        File[] subfolders = root.listFiles();
+        for (File folder : subfolders) {
+            if (!folder.isDirectory()) {
+                return whatToDoWithFiles(folder);
+            }
+        }
+        return subfolders;
+    }
+
+    private File[] whatToDoWithFiles(File folder) throws DatabaseFileStructureException {
+        throw new DatabaseFileStructureException("There is files in root folder. File'" + folder.getName() + "'");
+    }
+
+    public List<Pair<String, Integer>> listTables() throws LoadOrSaveException, DatabaseFileStructureException {
+        int size = tables.size();
+        List<Pair<String, Integer>> result = new ArrayList<>(size);
+        for (TableHash table : tables) {
+            result.add(new Pair<>(table.getTableName(), table.size()));
+        }
+        return result;
+    }
+
+    public void useTable(String name) throws TableNotFoundException, LoadOrSaveException,
+            DatabaseFileStructureException {
+        if (usingTable != null) {
+            ((TableHash) usingTable).save();
+        }
+        setUsingTable(name);
+    }
+
+    @Override
+    public Table getTable(String name) {
+        Table table = tableNames.get(name);
+        if (table == null) {
+            throw new IllegalArgumentException("Table not exists.");
+        } else {
+            return table;
+        }
+    }
+
+    @Override
+    public Table createTable(String name) {
+        try {
+            if (!containsTable(name)) {
+                TableHash table = new TableHash(name, this);
+                Path rootDirectoryPath = getRootDirectoryPath();
+                Path path = rootDirectoryPath.resolve(name);
+                if (path.startsWith(rootDirectoryPath) && path.getParent().equals(rootDirectoryPath)) {
+                    Files.createDirectory(path);
+                    tables.add(table);
+                    tableNames.put(name, table);
+                    return table;
+                } else {
+                    throw new DatabaseFileStructureException(INCORRECT_NAME_OF_TABLES);
+                }
+            } else {
+                return null;
+            }
+        } catch (UnsupportedOperationException e) {
+            throw new LoadOrSaveException(CANT_CREATE_TABLE_MESSAGE, e);
+        } catch (FileAlreadyExistsException e) {
+            throw new DatabaseFileStructureException(CANT_CREATE_TABLE + ", directory already exists", e);
+        } catch (InvalidPathException e) {
+            throw new DatabaseFileStructureException(
+                    CANT_CREATE_TABLE + ", directory name makes it impossible to create a directory", e);
+        } catch (IOException | SecurityException e) {
+            throw new LoadOrSaveException(CANT_CREATE_TABLE_MESSAGE, e);
+        }
+    }
+
+    @Override
+    public void removeTable(String name) {
+        TableHash table = (TableHash)getTable(name);
+        if (table == usingTable) {
+            usingTable = null;
+        }
+        table.drop();
+        tables.remove(table);
+        tableNames.remove(name);
+    }
+}
---
+++ src/ru/fizteh/fivt/students/semenenko_denis/MultiFileHashMap/DatabaseInterpreterState.java
@@ -0,0 +1,38 @@
+package ru.fizteh.fivt.students.semenenko_denis.MultiFileHashMap;
+
+import ru.fizteh.fivt.students.semenenko_denis.MultiFileHashMap.Interpreter.InterpreterState;
+
+import java.io.IOException;
+
+public class DatabaseInterpreterState extends InterpreterState{
+    Database database;
+
+    public DatabaseInterpreterState(Database database) {
+        this.database = database;
+    }
+
+    public Database getDatabase() {
+        return database;
+    }
+
+    public boolean tryToSave() {
+        try {
+            save();
+        } catch (TableNotFoundException ex) {
+            //Table not selected.
+        } catch (LoadOrSaveException | DatabaseFileStructureException ex) {
+            System.err.println(ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public void save() throws LoadOrSaveException, TableNotFoundException, DatabaseFileStructureException {
+        TableHash table = (TableHash) database.getUsingTable();
+        if (table != null) {
+            table.save();
+        } else {
+            throw new TableNotFoundException("Table isn't selected");
+        }
+    }
+}
