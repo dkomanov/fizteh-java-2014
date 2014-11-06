0c8e075434c54e9c50cd63ee934ba6ff6dd66c89
skye17 <skye17@users.noreply.github.com>
2014-11-06 19:00:30 +0400

    Update Utility.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/Utility.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/Utility.java
@@ -13,17 +13,21 @@ public class Utility {
         if (name == null || name.contains(".")
                 || name.contains("\\") || name.contains("/")) {
             throw new IllegalArgumentException("Table name is null or invalid");
-                }
+        }
     }
 
     public static void checkDirectorySubdirs(Path directory) {
         File dir = directory.toFile();
         if (dir.listFiles() != null) {
-            for (File subdir : dir.listFiles()) {
-                if (!subdir.isDirectory()) {
-                    throw new DatabaseIOException("Subdirectories of " + directory
-                            + "are not directories");
+            try {
+                for (File subdir : dir.listFiles()) {
+                    if (!subdir.isDirectory()) {
+                        throw new DatabaseIOException("Subdirectories of " + directory
+                                + "are not directories");
+                    }
                 }
+            } catch (NullPointerException n) {
+               throw  new DatabaseIOException("Access forbidden");
             }
         }
     }
