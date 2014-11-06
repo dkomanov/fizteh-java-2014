62946ba0c4d722cc2e5e328951f3a5986e226b6d
skye17 <skye17@users.noreply.github.com>
2014-11-06 19:13:42 +0400

    Update IllegalCommandException.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/IllegalCommandException.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/IllegalCommandException.java
@@ -1,12 +1,14 @@
 package ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util;
 
 public class IllegalCommandException extends IllegalArgumentException {
-    private String errMessage = "";
+    private final String errMessage;
     public String getErrMessage() {
         return errMessage;
     }
     public IllegalCommandException(String message) {
         errMessage = message;
     }
-    public IllegalCommandException() {}
+    public IllegalCommandException() {
+        errMessage = "";
+    }
 }
