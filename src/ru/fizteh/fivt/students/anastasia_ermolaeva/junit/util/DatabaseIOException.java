1e4e97dfbb80c3bc5341ef9968ba1b36b4c0ce90
skye17 <skye17@users.noreply.github.com>
2014-11-07 22:37:41 +0400

    Update DatabaseIOException.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/DatabaseIOException.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/DatabaseIOException.java
@@ -1,11 +1,7 @@
 package ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util;
 
 public class DatabaseIOException extends RuntimeException {
-    private final String errMessage;
-    public String getErrMessage() {
-        return  errMessage;
-    }
     public DatabaseIOException(String message) {
-        errMessage = message;
+        super(message);
     }
 }
