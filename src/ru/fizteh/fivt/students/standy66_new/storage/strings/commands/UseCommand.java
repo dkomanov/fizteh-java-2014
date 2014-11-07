d084534bedaa734d96a4db350897ccb3826ea677
Andrew <karn9050@gmail.com>
2014-11-07 10:09:01 +0300

    Added warned_unsaved constant

--- src/ru/fizteh/fivt/students/standy66_new/storage/strings/commands/UseCommand.java
+++ src/ru/fizteh/fivt/students/standy66_new/storage/strings/commands/UseCommand.java
@@ -9,6 +9,9 @@ import ru.fizteh.fivt.students.standy66_new.storage.strings.StringTable;
  * Created by astepanov on 20.10.14.
  */
 public class UseCommand extends ContextualCommand {
+
+    public static final boolean WARN_OF_UNSAVED_CHANGES = System.getProperty("warn_unsaved") != null;
+
     protected UseCommand(Context context) {
         super((x -> x == 2), context);
     }
@@ -21,7 +24,7 @@ public class UseCommand extends ContextualCommand {
         if (newTable == null) {
             throw new TableNotExistsException(String.format("%s not exists", arguments[1]));
         } else {
-            if (System.getProperty("junit") != null) {
+            if (WARN_OF_UNSAVED_CHANGES) {
                 if (currentTable != null && currentTable.unsavedChangesCount() > 0) {
                     throw new UncommitedChangesException(String.format("%d unsaved changes",
                             currentTable.unsavedChangesCount()));
