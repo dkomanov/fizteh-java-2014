3548e58f388f01d24581d13a8bb58f2ddf7c86ea
skye17 <skye17@users.noreply.github.com>
2014-11-07 22:38:24 +0400

    Update ExitException.java

--- src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/ExitException.java
+++ src/ru/fizteh/fivt/students/anastasia_ermolaeva/junit/util/ExitException.java
@@ -2,21 +2,17 @@ package ru.fizteh.fivt.students.anastasia_ermolaeva.junit.util;
 
 public class ExitException extends Exception {
     private final int status;
-    private final String errMessage;
 
     public final int getStatus() {
         return status;
     }
-    public final String  getMessage() {
-        return errMessage;
-    }
     public ExitException(final String message, final int exitStatus) {
-        errMessage = message;
+        super(message);
         status = exitStatus;
     }
 
     public ExitException(final int exitStatus) {
-        errMessage = "";
+        super("");
         status = exitStatus;
     }
 }
