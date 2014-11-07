cfdaa34a9412371ee09595024f1ebadb36d8f24d
Andrew <karn9050@gmail.com>
2014-11-07 13:42:01 +0300

    Added task5 test stub

---
+++ src/ru/fizteh/fivt/students/standy66_new/tests/task5/StructuredDatabaseFactoryTest.java
@@ -0,0 +1,23 @@
+package ru.fizteh.fivt.students.standy66_new.tests.task5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StructuredDatabaseFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+}
---
+++ src/ru/fizteh/fivt/students/standy66_new/tests/task5/StructuredDatabaseTest.java
@@ -0,0 +1,53 @@
+package ru.fizteh.fivt.students.standy66_new.tests.task5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StructuredDatabaseTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTable() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateTable() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveTable() throws Exception {
+
+    }
+
+    @Test
+    public void testDeserialize() throws Exception {
+
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateFor() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateFor1() throws Exception {
+
+    }
+}
