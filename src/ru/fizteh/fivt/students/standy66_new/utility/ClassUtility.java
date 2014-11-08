39e37571fca0a669ace714150a32e2c7f0bf6694
Andrew <karn9050@gmail.com>
2014-11-08 18:24:21 +0300

    Added class to string conversion

--- src/ru/fizteh/fivt/students/standy66_new/utility/ClassUtility.java
+++ src/ru/fizteh/fivt/students/standy66_new/utility/ClassUtility.java
@@ -24,4 +24,24 @@ public class ClassUtility {
                 throw new IllegalArgumentException("className is not supported");
         }
     }
+
+    public static String toString(Class<?> cls) {
+        if (cls == Integer.class) {
+            return "int";
+        } else if (cls == Long.class) {
+            return "long";
+        } else if (cls == Byte.class) {
+            return "byte";
+        } else if (cls == Float.class) {
+            return "float";
+        } else if (cls == Double.class) {
+            return "double";
+        } else if (cls == Boolean.class) {
+            return "boolean";
+        } else if (cls == String.class) {
+            return "String";
+        } else {
+            throw new IllegalArgumentException("class is not supported");
+        }
+    }
 }
