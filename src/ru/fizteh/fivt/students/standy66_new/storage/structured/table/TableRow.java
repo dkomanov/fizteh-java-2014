c7bf6963203825e6ca9ccfb475a5483785df92d6
Andrew <karn9050@gmail.com>
2014-11-08 17:32:15 +0300

    Implemented TableRow & TableSignature

--- src/ru/fizteh/fivt/students/standy66_new/storage/structured/table/TableRow.java
+++ src/ru/fizteh/fivt/students/standy66_new/storage/structured/table/TableRow.java
@@ -1,7 +1,81 @@
 package ru.fizteh.fivt.students.standy66_new.storage.structured.table;
 
+import ru.fizteh.fivt.storage.structured.ColumnFormatException;
+import ru.fizteh.fivt.storage.structured.Storeable;
+
 /**
  * Created by andrew on 07.11.14.
  */
-public class TableRow {
+public class TableRow implements Storeable {
+    private TableSignature tableSignature;
+    private Object[] row;
+
+    public TableRow(TableSignature tableSignature) {
+        this.tableSignature = tableSignature;
+        row = new Object[tableSignature.size()];
+    }
+
+    @Override
+    public void setColumnAt(int columnIndex, Object value) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (value != null) {
+            assertClassesEqualityAtIndex(value.getClass(), columnIndex);
+        }
+        row[columnIndex] = value;
+    }
+
+    @Override
+    public Object getColumnAt(int columnIndex) throws IndexOutOfBoundsException {
+        return row[columnIndex];
+    }
+
+    @Override
+    public Integer getIntAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Integer.class, columnIndex);
+        return (Integer) row[columnIndex];
+    }
+
+    @Override
+    public Long getLongAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Long.class, columnIndex);
+        return (Long) row[columnIndex];
+    }
+
+    @Override
+    public Byte getByteAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Byte.class, columnIndex);
+        return (Byte) row[columnIndex];
+    }
+
+    @Override
+    public Float getFloatAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Float.class, columnIndex);
+        return (Float) row[columnIndex];
+    }
+
+    @Override
+    public Double getDoubleAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Double.class, columnIndex);
+        return (Double) row[columnIndex];
+    }
+
+    @Override
+    public Boolean getBooleanAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(Boolean.class, columnIndex);
+        return (Boolean) row[columnIndex];
+    }
+
+    @Override
+    public String getStringAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        assertClassesEqualityAtIndex(String.class, columnIndex);
+        return (String) row[columnIndex];
+    }
+
+    private void assertClassesEqualityAtIndex(Class desired, int columnIndex) throws ColumnFormatException {
+        Class columnClass = tableSignature.getClassAt(columnIndex);
+        if (desired != columnClass) {
+            throw new ColumnFormatException(
+                    String.format("column class at: %d is %s, desired: %s", columnIndex,
+                            columnClass.getSimpleName(), desired.getSimpleName()));
+        }
+    }
 }
--- src/ru/fizteh/fivt/students/standy66_new/storage/structured/table/TableSignature.java
+++ src/ru/fizteh/fivt/students/standy66_new/storage/structured/table/TableSignature.java
@@ -4,5 +4,18 @@ package ru.fizteh.fivt.students.standy66_new.storage.structured.table;
  * Created by andrew on 07.11.14.
  */
 public class TableSignature {
+    private Class<?>[] storedObjectClasses;
 
+    public TableSignature(Class<?>... storedObjectClasses) {
+        //TODO: check if some of them is null
+        this.storedObjectClasses = storedObjectClasses;
+    }
+
+    public Class<?> getClassAt(int columnIndex) throws IndexOutOfBoundsException {
+        return storedObjectClasses[columnIndex];
+    }
+
+    public int size() {
+        return storedObjectClasses.length;
+    }
 }
