d7777bf020457cfc6c2609bce79bea3ce958dbb1
unknown <novikov@phystech.edu>
2014-11-09 14:39:59 +0300

    commit

--- src/ru/fizteh/fivt/students/dnovikov/junit/DataBaseProviderTest.java
+++ src/ru/fizteh/fivt/students/dnovikov/junit/DataBaseProviderTest.java
@@ -3,7 +3,10 @@ package ru.fizteh.fivt.students.dnovikov.junit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+<<<<<<< Updated upstream
 import ru.fizteh.fivt.storage.strings.TableProvider;
+=======
+>>>>>>> Stashed changes
 import ru.fizteh.fivt.students.dnovikov.junit.Exceptions.LoadOrSaveException;
 
 import java.io.File;
@@ -12,25 +15,42 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.*;
-
 public class DataBaseProviderTest {
     private final Path testDir = Paths.get(System.getProperty("java.io.tmpdir"), "DbTestDir");
     private final File testFile = Paths.get(testDir.toString(), "TestFile").toFile();
     private final String dirName = "test";
     private final Path dirPath = testDir.resolve(dirName);
 
+=======
+public class DataBaseProviderTest {
+    private final Path testDir = Paths.get(System.getProperty("java.io.tmpdir"), "DbTestDir");
+    private final File testFile = Paths.get(testDir.toString(), "TestFile").toFile();
+    private final String workingDir = "test";
+    private final Path workingPath = testDir.resolve(workingDir);
+
+    @Before
+>>>>>>> Stashed changes
     public void setUp() throws IOException {
         Files.createDirectory(testDir);
     }
 
     @Test(expected = LoadOrSaveException.class)
+<<<<<<< Updated upstream
     public void testDataBaseProviderThrowsExceptionOfNonexistentDirectory() {
         new DataBaseProvider(testDir.resolve(dirPath).toString());
     }
 
     @Test(expected = LoadOrSaveException.class)
     public void testDataBaseProviderThrowsExceptionCreatedNotDirectory() {
+=======
+    public void testDataBaseProviderThrowsExceptionDirectoryNotExists() {
+        new DataBaseProvider(testDir.resolve(workingDir).toString());zr
+    }
+
+    @Test(expected = LoadOrSaveException.class)
+    public void testDataBaseProviderThrowsExceptionCreatedNotDirectory() throws IOException {
+        testFile.createNewFile();
+>>>>>>> Stashed changes
         new DataBaseProvider(testFile.toString());
     }
 
@@ -41,6 +61,7 @@ public class DataBaseProviderTest {
 
     @Test(expected = LoadOrSaveException.class)
     public void testDataBaseProviderThrowsExceptionFileInDirectory() throws IOException {
+<<<<<<< Updated upstream
         if (!testDir.toFile().exists()) {
             Files.createDirectory(testDir);
         }
@@ -51,6 +72,12 @@ public class DataBaseProviderTest {
         new DataBaseProvider(dirPath.toString());
     }
 
+=======
+        Files.createDirectory(workingPath);
+        workingPath.resolve("fileName").toFile().createNewFile();
+        new DataBaseProvider(workingPath.toString());
+    }
+>>>>>>> Stashed changes
 //    @Test(expected = IllegalArgumentException.class)
 //    public void testDataBaseProviderThrowsExceptionCreateNullTableName() throws IOException {
 //        if (!testDir.toFile().exists()) {
@@ -62,8 +89,17 @@ public class DataBaseProviderTest {
 //        test.createTable(null);
 //    }
 
+<<<<<<< Updated upstream
     @After
     public void tearDown() {
 
+=======
+
+    @After
+    public void tearDown() {
+        workingPath.toFile().delete();
+        testFile.delete();
+        testDir.toFile().delete();
+>>>>>>> Stashed changes
     }
 }
