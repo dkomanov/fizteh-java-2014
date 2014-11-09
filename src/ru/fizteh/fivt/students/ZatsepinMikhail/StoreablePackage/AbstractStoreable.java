3ae474ab6ea276ad563b9425789e94fa11426463
ZatsepinMikhail <salamence1996@gmail.com>
2014-11-09 12:48:29 +0300

    AbstractStoreable (done)

--- src/ru/fizteh/fivt/students/ZatsepinMikhail/StoreablePackage/AbstractStoreable.java
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/StoreablePackage/AbstractStoreable.java
@@ -10,7 +10,81 @@ public class AbstractStoreable implements Storeable {
         values = newValues;
     }
 
+    @Override
     public void setColumnAt(int columnIndex, Object value) throws ColumnFormatException, IndexOutOfBoundsException {
-        if (values[])
+        if (values[columnIndex].getClass() != value.getClass()) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                + " , but got:" + value.getClass());
+        } else {
+            values[columnIndex] = value;
+        }
+    }
+
+    @Override
+    public Object getColumnAt(int columnIndex) throws IndexOutOfBoundsException {
+        return values[columnIndex];
+    }
+
+    @Override
+    public Integer getIntAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Integer.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Integer.class);
+        }
+        return (Integer) values[columnIndex];
+    }
+
+    @Override
+    public Long getLongAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Long.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Long.class);
+        }
+        return (Long) values[columnIndex];
+    }
+
+    @Override
+    public Byte getByteAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Byte.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Byte.class);
+        }
+        return (Byte) values[columnIndex];
+    }
+
+    @Override
+    public Float getFloatAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Float.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Float.class);
+        }
+        return (Float) values[columnIndex];
+    }
+
+    @Override
+    public Double getDoubleAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Double.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Double.class);
+        }
+        return (Double) values[columnIndex];
+    }
+
+    @Override
+    public Boolean getBooleanAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != Boolean.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + Boolean.class);
+        }
+        return (Boolean) values[columnIndex];
+    }
+
+    @Override
+    public String getStringAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (values[columnIndex].getClass() != String.class) {
+            throw new ColumnFormatException("expected:" + values[columnIndex].getClass()
+                    + " , but got:" + String.class);
+        }
+        return (String) values[columnIndex];
     }
 }
