bcd6860943b82b16fd68365d50a49fdd7e0d559c
ilivanov <ilivanov.se@ya.ru>
2014-11-09 17:03:34 +0300

    junit

---
+++ src/ru/fizteh/fivt/students/ilivanov/JUnit/Main.java
@@ -0,0 +1,36 @@
+package ru.fizteh.fivt.students.ilivanov.JUnit;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(final String[] args) throws Exception {
+        String root = System.getProperty("fizteh.db.dir");
+
+        if (root == null) {
+            System.err.println("root directory isn't specified");
+            System.exit(-1);
+        }
+
+        ShellBigBoss database = null;
+        try {
+            database = new ShellBigBoss(root);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+
+        Shell shell = new Shell();
+        database.integrate(shell);
+
+        int exitCode;
+        if (args.length != 0) {
+            exitCode = shell.runArgs(args);
+        } else {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            exitCode = shell.run(br);
+        }
+
+        System.exit(exitCode);
+    }
+}
