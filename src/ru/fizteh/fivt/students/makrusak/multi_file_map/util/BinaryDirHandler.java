274ff7d1ae58c80565143c6ceba062e307c4f398
Maxim Rusak <mak-rusak@yandex.ru>
2014-11-11 02:00:14 +0400

    3 task

---
+++ src/ru/fizteh/fivt/students/makrusak/multi_file_map/util/BinaryDirHandler.java
@@ -0,0 +1,76 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class BinaryDirHandler {
+  private File path;
+  private HashMap<String, BinaryFileHandler> binaryFileHandlers;
+
+  public BinaryDirHandler(File _path) throws IOException {
+    path = _path;
+    binaryFileHandlers = new HashMap<String, BinaryFileHandler>();
+    if (!path.isDirectory()) {
+      throw new IOException("Binary directory is not directory");
+    }
+    String[] parts = path.getName().split("\\.");
+    try {
+      int num = Integer.parseInt(parts[0]);
+      if (parts.length != 2 || !parts[1].equals("dir") || num < 0 || num >= 16) {
+        throw new Exception();
+      }
+    } catch (Exception e) {
+      throw new IOException("Incorrect directory name in table dir.");
+    }
+    if (path.listFiles() != null) {
+      for (File inFile : path.listFiles()) {
+        binaryFileHandlers.put(inFile.getName(), new BinaryFileHandler(inFile));  
+      }
+    }
+  }
+
+  public String work(String[] tokens) throws IOException {
+    if (tokens.length >= 2) {
+      String key = tokens[1];
+      int file = key.hashCode() / 16 % 16;
+      String fileStr = Integer.toString(file) + ".dat";
+      File fileFile = new File(path, fileStr);
+      if (!binaryFileHandlers.containsKey(fileStr)) {
+        fileFile.createNewFile();
+        binaryFileHandlers.put(fileStr, new BinaryFileHandler(fileFile));  
+      }
+      String res = binaryFileHandlers.get(fileStr).work(tokens);
+      if (binaryFileHandlers.get(fileStr).get_count() == 0) {
+        if (fileFile.listFiles() != null) {
+          for (File inFile : fileFile.listFiles()) {
+            inFile.delete();
+          }
+        }
+        fileFile.delete();
+        binaryFileHandlers.remove(fileStr);
+      }
+      return res;
+    } 
+    throw new IOException("Incorrect command");
+
+  }
+
+  public Set<String> get_list() {
+    Set<String> res = new TreeSet<String>();
+    for (BinaryFileHandler cur : binaryFileHandlers.values()) {
+      res.addAll(cur.get_list());
+    }
+    return res;
+  }
+
+  public int get_count() {
+    int res = 0;
+    for (BinaryFileHandler cur : binaryFileHandlers.values()) {
+      res += cur.get_count();
+    }
+    return res;
+  }
+}
---
+++ src/ru/fizteh/fivt/students/makrusak/multi_file_map/util/TableHandler.java
@@ -0,0 +1,73 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TableHandler {
+  private File path;
+  private HashMap<String, BinaryDirHandler> binaryDirHandlers;
+
+  public TableHandler(File _path) throws IOException {
+    path = _path;
+    binaryDirHandlers = new HashMap<String, BinaryDirHandler>();
+    if (!path.isDirectory()) {
+      throw new IOException("Table is not directory");
+    }
+    if (path.listFiles() != null) {
+      for (File inFile : path.listFiles()) {
+        binaryDirHandlers.put(inFile.getName(), new BinaryDirHandler(inFile));  
+      }
+    }
+  }
+
+  public String work(String[] tokens) throws IOException {
+    if (tokens.length == 1 && tokens[0].equals("list")) {
+      String resString = "";
+      Set<String> res = new TreeSet<String>();
+      for (BinaryDirHandler cur : binaryDirHandlers.values()) {
+        res.addAll(cur.get_list());
+      }
+      String[] keys = res.toArray(new String[res.size()]);
+
+      if (keys.length == 0) {
+        return resString;
+      }
+      resString += keys[0];
+      for (int i = 1; i < keys.length; i++) {
+        resString += ", " + keys[i];
+      }
+      return resString;
+    } else if (tokens.length >= 2) {
+      String key = tokens[1];
+      int dir = key.hashCode() % 16;
+      String dirStr = Integer.toString(dir) + ".dir";
+      File dirFile = new File(path, dirStr);
+      if (!binaryDirHandlers.containsKey(dirStr)) {
+        dirFile.mkdirs();
+        binaryDirHandlers.put(dirStr, new BinaryDirHandler(dirFile));  
+      }
+      String res = binaryDirHandlers.get(dirStr).work(tokens);
+      if (binaryDirHandlers.get(dirStr).get_count() == 0) {
+        for (File file : dirFile.listFiles()) {
+          file.delete();
+        }
+        dirFile.delete();
+        binaryDirHandlers.remove(dirStr);
+      }
+      return res;
+    } 
+    throw new IOException("Incorrect command");
+  }
+
+  public int get_count() {
+    int res = 0;
+    for (BinaryDirHandler cur : binaryDirHandlers.values()) {
+      res += cur.get_count();
+    }
+    return res;
+  }
+
+}
