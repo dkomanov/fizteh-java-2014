e867737fbcc3d91b457afe71b0acd411121dadf6
kagudkov <kagudkov@gmail.com>
2014-11-10 21:11:14 +0300

    style

--- src/ru/fizteh/fivt/students/gudkov394/Junit/src/Exit.java
+++ src/ru/fizteh/fivt/students/gudkov394/Junit/src/Exit.java
@@ -2,7 +2,6 @@ package ru.fizteh.fivt.students.gudkov394.Junit.src;
 
 
 public class Exit {
-
     public Exit(final String[] currentArgs) {
         if (currentArgs.length > 1) {
             System.err.println("extra arguments for exit");
