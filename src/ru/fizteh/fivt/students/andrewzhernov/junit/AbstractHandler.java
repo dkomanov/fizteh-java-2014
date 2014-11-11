7b95c9ac5d42067a36d1d2cb13b42a44ef6b96ba
shakarim <shakarim@1005.(none)>
2014-11-11 21:43:23 +0400

    Revert "Revert "Revert "Merge commit 'refs/pull/318/head' of https://github.com/dkomanov/fizteh-java-2014 into task4"""
    This reverts commit 39e4a26c92a22add9167a8b01d142c78a493beee.

--- src/ru/fizteh/fivt/students/andrewzhernov/junit/AbstractHandler.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.andrewzhernov.junit;
-
-interface AbstractHandler {
-    Object exec(TableProvider database, String[] args) throws Exception;
-    void print(Object returnValue) throws Exception;
-}
-
