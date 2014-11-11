5786c631ad4a9496cd937ae63d65904cd4e172a8
shakarim <shakarim@1005.(none)>
2014-11-11 21:41:35 +0400

    Revert "Merge commit 'refs/pull/318/head' of https://github.com/dkomanov/fizteh-java-2014 into task4"
    This reverts commit 8a785c8c96faf5a14ba45391e7c855f3217331a2, reversing
    changes made to a5f57cb38e72449b852b9b236a112b82b175dbc8.

--- src/ru/fizteh/fivt/students/andrewzhernov/junit/HandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.andrewzhernov.junit;
-
-public interface HandlerInterface {
-    Object execute(TableProvider database, String[] args) throws Exception;
-    void handle(Object object) throws Exception;
-}
-
