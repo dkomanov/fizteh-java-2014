e4ed98c955c33f02de7520f0e0b5b227cb985d3e
lyaronskaya <luba.yaronskaya@gmail.com>
2014-11-11 04:27:10 +0400

    after checkstyle

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/JUnit/TableTest.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/JUnit/TableTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
