f99695e7037c68979f50cbafb9586ce3d5e148f9
shakarim <shakarim@1005.(none)>
2014-11-13 01:56:42 +0400

    new branch commit

---
+++ src/ru/fizteh/fivt/students/kalandarovshakarim/commands/AbstractCommand.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.fizteh.fivt.students.kalandarovshakarim.commands;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author Shakarim
+ * @param <Type>
+ */
+public abstract class AbstractCommand<Type> implements Command {
+
+    private final String name;
+    private final int argNum;
+    protected final Type context;
+    protected final InputStream in;
+    protected final PrintStream out;
+    protected final PrintStream err;
+
+    public AbstractCommand() {
+        this.name = null;
+        this.argNum = 0;
+        this.context = null;
+        this.in = null;
+        this.out = null;
+        this.err = null;
+    }
+
+    public AbstractCommand(String name, int argNum, Type context) {
+        this(name, argNum, context, System.in, System.out, System.err);
+    }
+
+    public AbstractCommand(String name, int argNum, Type context, InputStream in, PrintStream out, PrintStream err) {
+        this.name = name;
+        this.argNum = argNum;
+        this.context = context;
+        this.in = in;
+        this.out = out;
+        this.err = err;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getArgsNum() {
+        return argNum;
+    }
+}
