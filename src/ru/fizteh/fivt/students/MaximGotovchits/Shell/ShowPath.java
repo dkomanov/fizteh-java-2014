75d07056308275624d79515d45802cdd3695ad59
MaximGotovchits <Gotovchits.m@mail.ru>
2014-11-13 03:47:50 +0300

    Готовчиц Максим, Shell, 396

---
+++ src/ru/fizteh/fivt/students/MaximGotovchits/Shell/ShowPath.java
@@ -0,0 +1,7 @@
+package fizteh.fivt.students.MaximGotovchits.Shell;
+
+public class ShowPath extends CommandTools {
+    void showPathFunction() {
+        System.out.println(currentDirectory);
+    }
+}
