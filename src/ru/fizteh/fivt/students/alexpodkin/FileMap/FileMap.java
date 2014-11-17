29ce5cff012ba2e1de424f64002318062bbc2875
Alex Podkin <alexpodkin@gmail.com>
2014-11-17 17:30:58 +0300

    2nd task

---
+++ src/ru/fizteh/fivt/students/alexpodkin/FileMap/FileMap.java
@@ -0,0 +1,27 @@
+package ru.fizteh.fivt.students.alexpodkin.FileMap;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class FileMap {
+
+    public static void main(String[] args) {
+        String fileMapPath = System.getProperty("db.file");
+        if (fileMapPath == null) {
+            System.exit(1);
+        }
+        Reader reader = new Reader(fileMapPath);
+        Writer writer = new Writer(fileMapPath);
+        HashMap<String, String> fileMap;
+        try {
+            fileMap = reader.readDataFromFile();
+            Launcher launcher = new Launcher(fileMap, writer);
+            if (!launcher.launch(args)) {
+                System.exit(1);
+            }
+        } catch (IOException e) {
+            System.err.print(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
