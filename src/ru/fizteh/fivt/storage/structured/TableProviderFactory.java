451d1bbcb94155370e6de309471e96858c2125df
kagudkov <kagudkov@gmail.com>
2014-11-18 20:57:41 +0300

    resolve merge conflicts

--- src/ru/fizteh/fivt/storage/structured/TableProviderFactory.java
+++ src/ru/fizteh/fivt/storage/structured/TableProviderFactory.java
@@ -12,11 +12,11 @@ public interface TableProviderFactory {
     /**
      * Возвращает объект для работы с базой данных.
      *
-     * @param dir Директория с файлами базы данных.
+     * @param path Директория с файлами базы данных.
      * @return Объект для работы с базой данных, который будет работать в указанной директории.
      *
      * @throws IllegalArgumentException Если значение директории null или имеет недопустимое значение.
      * @throws java.io.IOException В случае ошибок ввода/вывода.
      */
-    ru.fizteh.fivt.students.gudkov394.Storable.src.TableProviderClass create(String dir) throws IOException;
+    TableProvider create(String path) throws IOException;
 }
