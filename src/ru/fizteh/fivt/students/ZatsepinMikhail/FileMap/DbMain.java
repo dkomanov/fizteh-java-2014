f87a5d744cc108223e970f243c50529571b3996f
ZatsepinMikhail <salamence1996@gmail.com>
2014-11-18 18:09:03 +0300

    attempt to merge (2)

--- src/ru/fizteh/fivt/students/ZatsepinMikhail/FileMap/DbMain.java
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/FileMap/DbMain.java
@@ -1,27 +1,45 @@
 package ru.fizteh.fivt.students.ZatsepinMikhail.FileMap;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
  * FileMap
- * Version from 28.09.2014
+ * Version from 21.10.2014
  * Created by Mikhail Zatsepin, 395
  */
 
 public class DbMain {
     public static void main(String[] args) {
-        String dataBasePath = System.getProperty("db.file");
-        if (dataBasePath == null || !Files.exists(Paths.get(dataBasePath))) {
-            System.out.println("cannot access the database");
+        String dataBasePathString = System.getProperty("db.file");
+        if (dataBasePathString == null) {
+            System.out.println("need database file");
+            System.exit(2);
+        }
+        if (Files.isDirectory(Paths.get(dataBasePathString))) {
+            System.out.println("cannot access the database: it's a directory");
             System.exit(1);
         }
-        FileMap myFileMap = new FileMap(dataBasePath);
+
         boolean errorOccuried = false;
+        if (!Files.exists(Paths.get(dataBasePathString))) {
+            try {
+                Files.createFile(Paths.get(dataBasePathString));
+            } catch (IOException e) {
+                System.err.println("error while creating database file");
+                errorOccuried = true;
+            } finally {
+                if (errorOccuried) {
+                    System.exit(4);
+                }
+            }
+        }
+
+        FileMap myFileMap = new FileMap(dataBasePathString);
         if (!myFileMap.init()) {
             System.exit(3);
         }
-
         Shell<FileMap> myShell = new Shell<>(myFileMap);
         myShell.addCommand(new Get());
         myShell.addCommand(new List());
