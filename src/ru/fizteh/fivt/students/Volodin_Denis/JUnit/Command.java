da53d4d12c3eccbf4fb4815bd9244c8830a1a846
volodden <volodin@phystech.edu>
2014-11-18 04:33:06 +0300

    Update 11.

--- src/ru/fizteh/fivt/students/Volodin_Denis/JUnit/Command.java
+++ src/ru/fizteh/fivt/students/Volodin_Denis/JUnit/Command.java
@@ -7,65 +7,61 @@ import ru.fizteh.fivt.storage.strings.TableProvider;
 
 public class Command {
 
-    public static void commit(final String[] args, Table table) throws Exception {
-        if (args.length != 1) {
-            ErrorFunctions.wrongQuantityOfArguments("commit");
+    private static void checkQuantityOfArguments(final String[] args,
+            final int expectation, final String commandName) throws Exception {
+        if (args.length != expectation) {
+            ErrorFunctions.wrongQuantityOfArguments(commandName);
         }
+        if (args.length > 1) {
+            for (int i = 1; i < args.length; ++i) {
+                if (args[i].isEmpty()) {
+                    ErrorFunctions.wrongInput(commandName);
+                }
+            }
+        }
+    }
+    
+    private static void tableIsNull(final Table table) throws Exception {
+        if (table == null) {
+            throw new Exception("no table");
+        }
+    }
+    
+    public static void commit(final String[] args, Table table) throws Exception {
+        checkQuantityOfArguments(args, 1, "commit");
+        tableIsNull(table);
         
         table.commit();
     }
 
     public static void create(final String[] args, TableProvider tableProvider) throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongQuantityOfArguments("create");
-        }
-        if (args[1].isEmpty()) {
-            ErrorFunctions.wrongInput("create");
-        }
+        checkQuantityOfArguments(args, 2, "create");
         
         tableProvider.createTable(args[1]);
     }
 
     public static void drop(final String[] args, TableProvider tableProvider) throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongQuantityOfArguments("drop");
-        }
-        if (args[1].isEmpty()) {
-            ErrorFunctions.wrongInput("drop");
-        }
+        checkQuantityOfArguments(args, 2, "drop");
+        
         tableProvider.removeTable(args[1]);
     }
 
     public static void exit(final String[] args) throws Exception {
-        if (args.length != 1) {
-            ErrorFunctions.wrongQuantityOfArguments("exit");
-        }
+        checkQuantityOfArguments(args, 1, "exit");
+        
         System.exit(ReturnCodes.SUCCESS);
     }
 
     public static void get(final String[] args, Table table) throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongQuantityOfArguments("get");
-        }
-        if (args[1].isEmpty()) {
-            ErrorFunctions.wrongInput("get");
-        }
-        if (table == null) {
-            System.out.println("no table");
-            return;
-        }
+        checkQuantityOfArguments(args, 2, "get");
+        tableIsNull(table);
         
         table.get(args[1]);
     }
 
     public static void list(final String[] args, Table table) throws Exception {
-        if (args.length != 1) {
-            ErrorFunctions.wrongQuantityOfArguments("list");
-        }
-        if (table == null) {
-            System.out.println("no table");
-            return;
-        }
+        checkQuantityOfArguments(args, 1, "list");
+        tableIsNull(table);
         
         List<String> list = table.list();
         int i = list.size();
@@ -80,68 +76,44 @@ public class Command {
     }
 
     public static void put(final String[] args, Table table) throws Exception {
-        if (args.length != 3) {
-            ErrorFunctions.wrongQuantityOfArguments("put");
-        }
-        if ((args[1].isEmpty()) || (args[2].isEmpty())) {
-            ErrorFunctions.wrongInput("put");
-        }
-        if (table == null) {
-            System.out.println("no table");
-            return;
-        }
+        checkQuantityOfArguments(args, 3, "put");
+        tableIsNull(table);
         
         table.put(args[1], args[2]);
     }
 
     public static void remove(final String[] args, Table table) throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongQuantityOfArguments("remove");
-        }
-        if (args[1].isEmpty()) {
-            ErrorFunctions.wrongInput("remove");
-        }
-        if (table == null) {
-            System.out.println("no table");
-            return;
-        }
+        checkQuantityOfArguments(args, 2, "remove");
+        tableIsNull(table);
+        
         table.remove(args[1]);
     }
 
     public static void rollback(final String[] args, Table table) throws Exception {
-        if (args.length != 1) {
-            ErrorFunctions.wrongQuantityOfArguments("rollback");
-        }
+        checkQuantityOfArguments(args, 1, "rollback");
+        tableIsNull(table);
         
         table.rollback();
     }
 
     public static void size(final String[] args, Table table) throws Exception {
-        if (args.length != 1) {
-            ErrorFunctions.wrongInput("size");
-        }
+        checkQuantityOfArguments(args, 1, "size");
+        tableIsNull(table);
         
         table.size();
     }
     
     public static void showTables(final String[] args, TableProvider table)throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongInput("show tableProvider");
-        }
-        if (!args[1].equals("tableProvider")) {
-            ErrorFunctions.wrongInput("show tableProvider");
+        checkQuantityOfArguments(args, 2, "show tables");
+        if (!args[1].equals("tables")) {
+            ErrorFunctions.wrongInput("show tables");
         }
         
         table.showTables();
     }
     
     public static void use(final String[] args, TableProvider tableProvider, Table table) throws Exception {
-        if (args.length != 2) {
-            ErrorFunctions.wrongQuantityOfArguments("use");
-        }
-        if (args[1].isEmpty()) {
-            ErrorFunctions.wrongInput("use");
-        }
+        checkQuantityOfArguments(args, 2, "use");
         
         table = tableProvider.getTable(args[1]);
     }
