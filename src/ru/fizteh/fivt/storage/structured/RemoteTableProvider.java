4a83bad0035b422ca3bac79f1b7a311ba27e5b86
IgorTitov396 <1hp_phoenix@mail.ru>
2014-11-21 07:49:23 +0300

    Revert "junit"
    This reverts commit 214851de08371aecc3b50ee60c4b20301ccc9d21.

--- src/ru/fizteh/fivt/storage/structured/RemoteTableProvider.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.storage.structured;
-
-import java.io.Closeable;
-
-/**
- *  Расширенный интерфейс {@link TableProvider}, с предоставлением метода {@link #close()}.
- */
-public interface RemoteTableProvider extends TableProvider, Closeable {
-}
--- src/ru/fizteh/fivt/students/titov/FileMap/BadFileException.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.titov.FileMap;
-
-import java.io.IOException;
-
-public class BadFileException extends IOException {
-}
