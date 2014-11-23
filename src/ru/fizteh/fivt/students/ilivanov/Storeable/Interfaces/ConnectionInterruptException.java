f55d23d3c32dd7a340741fb0cc7525e783ba6ca2
ilivanov <ilivanov.se@ya.ru>
2014-11-23 13:11:14 +0300

    delete excess folder

--- src/ru/fizteh/fivt/students/ilivanov/Storeable/Interfaces/ConnectionInterruptException.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.ilivanov.Storeable.Interfaces;
-
-public class ConnectionInterruptException extends Exception {
-    public ConnectionInterruptException(String message) {
-        super(message);
-    }
-}
