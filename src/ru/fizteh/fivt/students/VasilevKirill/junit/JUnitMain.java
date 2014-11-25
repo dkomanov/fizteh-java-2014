8c2992ce75a34a1db16af29503e89be228374853
Kirill <kirivasile@yandex.ru>
2014-11-25 20:30:03 +0300

    JUnit: changed the name of the property

--- src/ru/fizteh/fivt/students/VasilevKirill/junit/JUnitMain.java
+++ src/ru/fizteh/fivt/students/VasilevKirill/junit/JUnitMain.java
@@ -30,7 +30,7 @@ public class JUnitMain {
             commands.put(new SizeCommand().toString(), new SizeCommand());
             try {
                 int retValue = 0;
-                String rootDirectory = System.getProperty("db.file");
+                String rootDirectory = System.getProperty("fizteh.db.dir");
                 if (rootDirectory == null) {
                     throw new IOException("Can't find the directory");
                 }
