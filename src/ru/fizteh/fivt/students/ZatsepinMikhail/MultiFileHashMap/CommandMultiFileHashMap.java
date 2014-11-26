303e0809f6ec1d0abb29ded2b5de1c85c9461131
ZatsepinMikhail <salamence1996@gmail.com>
2014-11-26 23:48:33 +0300

    tmp2

--- src/ru/fizteh/fivt/students/ZatsepinMikhail/MultiFileHashMap/CommandMultiFileHashMap.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.ZatsepinMikhail.MultiFileHashMap;
-
-import ru.fizteh.fivt.students.ZatsepinMikhail.FileMap.Command;
-
-public abstract class CommandMultiFileHashMap extends Command<MFileHashMap> {
-}
