e8341c4a7e33c9cdecafff731547ff59fd17546f
Andrew <karn9050@gmail.com>
2014-11-29 23:54:07 +0300

    Implemented Proxy

---
+++ src/ru/fizteh/fivt/students/standy66_new/proxy/JSONLoggingProxyFactory.java
@@ -0,0 +1,85 @@
+package ru.fizteh.fivt.students.standy66_new.proxy;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import ru.fizteh.fivt.proxy.LoggingProxyFactory;
+
+import java.io.Writer;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.IdentityHashMap;
+
+/**
+ * @author andrew
+ *         Created by andrew on 30.11.14.
+ */
+public class JSONLoggingProxyFactory implements LoggingProxyFactory {
+    @Override
+    public Object wrap(Writer writer, Object implementation, Class<?> interfaceClass) {
+        return Proxy.newProxyInstance(interfaceClass.getClassLoader(),
+                new Class<?>[]{interfaceClass}, new ProxyHandler(writer, implementation));
+
+    }
+
+    private static class ProxyHandler implements InvocationHandler {
+        private Writer writer;
+        private Object implementation;
+
+        public ProxyHandler(Writer writer, Object implementation) {
+            this.writer = writer;
+            this.implementation = implementation;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+            JSONObject log = new JSONObject();
+            log.put("timestamp", System.currentTimeMillis());
+            log.put("class", implementation.getClass().getName());
+            log.put("method", method.getName());
+            log.put("arguments", unwindIterable(Arrays.asList(args), new IdentityHashMap<>()));
+            Throwable targetException = null;
+            Object returnValue = null;
+            try {
+                returnValue = method.invoke(implementation, args);
+            } catch (InvocationTargetException e) {
+                targetException = e.getTargetException();
+            }
+
+            if (method.getReturnType() != void.class) {
+                log.put("returnValue", returnValue);
+            }
+            if (targetException != null) {
+                log.put("thrown", targetException);
+            }
+            log.write(writer);
+            writer.write(String.format("%n"));
+            if (targetException != null) {
+                throw targetException;
+            }
+            return returnValue;
+        }
+
+        private JSONArray unwindIterable(Iterable iterable, IdentityHashMap<Object, Object> processed) {
+            JSONArray result = new JSONArray();
+            iterable.forEach((obj) -> {
+                if (processed.containsKey(obj)) {
+                    result.put("cyclic");
+                } else {
+                    if (obj instanceof Iterable) {
+                        IdentityHashMap<Object, Object> copy = new IdentityHashMap<>(processed);
+                        copy.put(obj, obj);
+                        result.put(unwindIterable((Iterable) obj, copy));
+                    } else {
+                        result.put(obj);
+                    }
+                }
+            });
+            return result;
+        }
+
+    }
+}
---
+++ src/ru/fizteh/fivt/students/standy66_new/runners/Task7Runner.java
@@ -0,0 +1,54 @@
+package ru.fizteh.fivt.students.standy66_new.runners;
+
+import ru.fizteh.fivt.proxy.LoggingProxyFactory;
+import ru.fizteh.fivt.students.standy66_new.proxy.JSONLoggingProxyFactory;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author andrew
+ *         Created by andrew on 30.11.14.
+ */
+public class Task7Runner {
+    public static void main(String[] args) throws Throwable {
+        JSONLoggingProxyFactory factory = new JSONLoggingProxyFactory();
+        HashMap map = new HashMap();
+        List<Object> list = new ArrayList<>();
+        List<Object> nested = new ArrayList<>();
+        nested.add("str");
+        nested.add(false);
+        nested.add(3.14f);
+        nested.add(Long.MAX_VALUE);
+        nested.add(list);
+        list.add(nested);
+        list.add(list);
+
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map proxy = (Map) factory.wrap(bw, map, Map.class);
+
+        proxy.put("a", "b");
+        proxy.put("a", "c");
+        proxy.put("a", list);
+
+        LoggingProxyFactory fac1 = (LoggingProxyFactory) factory.wrap(bw, new LoggingProxyFactory() {
+            @Override
+            public Object wrap(Writer writer, Object implementation, Class<?> interfaceClass) {
+                throw new RuntimeException("azaza");
+            }
+        }, LoggingProxyFactory.class);
+        try {
+            fac1.wrap(null, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bw.flush();
+
+    }
+}
