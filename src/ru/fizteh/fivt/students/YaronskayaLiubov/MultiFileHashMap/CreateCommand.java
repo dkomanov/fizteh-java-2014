b9c2d98cd00f5e0b61252a8d6fac3e04bb191899
lyaronskaya <luba.yaronskaya@gmail.com>
2014-12-01 00:49:21 +0300

    Update CreateCommand.java

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/MultiFileHashMap/CreateCommand.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/MultiFileHashMap/CreateCommand.java
@@ -1,5 +1,6 @@
 package ru.fizteh.fivt.students.YaronskayaLiubov.MultiFileHashMap;
 
+
 /**
  * Created by luba_yaronskaya on 18.10.14.
  */
@@ -15,7 +16,7 @@ public class CreateCommand extends Command {
             return false;
         }
         if (MultiFileHashMap.tables.get(args[1]) == null) {
-            MultiFileHashMap.tables.put(args[1], new FileMap(args[1]));
+            MultiFileHashMap.tables.put(args[1], new FileMap(MultiFileHashMap.dbDir, args[1]));
             System.out.println("created");
         } else {
             System.out.println(args[1] + " exists");
