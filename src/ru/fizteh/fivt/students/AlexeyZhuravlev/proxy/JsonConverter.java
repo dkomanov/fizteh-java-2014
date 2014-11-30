3aa44af3769b26cc3e62a2639da98681fd7aa779
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-11-30 20:44:44 +0300

    cyclic checks added

--- src/ru/fizteh/fivt/students/AlexeyZhuravlev/proxy/JsonConverter.java
+++ src/ru/fizteh/fivt/students/AlexeyZhuravlev/proxy/JsonConverter.java
@@ -1,11 +1,37 @@
 package ru.fizteh.fivt.students.AlexeyZhuravlev.proxy;
 
+import org.json.JSONArray;
+
+import java.util.IdentityHashMap;
+
 /**
  * @author AlexeyZhuravlev
  */
 public class JsonConverter {
 
+    private static JSONArray searchCyclic(Iterable list, IdentityHashMap<Object, Object> visited) {
+        visited.put(list, null);
+        JSONArray result = new JSONArray();
+        for (Object object: list) {
+            if (object instanceof Iterable) {
+                if (visited.containsKey(object)) {
+                    result.put("cyclic");
+                } else {
+                    result.put(searchCyclic((Iterable) object, visited));
+                }
+            } else {
+                result.put(object);
+            }
+        }
+        visited.remove(list);
+        return result;
+    }
+
     public static Object convertObject(Object object) {
-        return null;
+        if (object instanceof Iterable) {
+            return searchCyclic((Iterable) object, new IdentityHashMap<>());
+        } else {
+            return object;
+        }
     }
 }
