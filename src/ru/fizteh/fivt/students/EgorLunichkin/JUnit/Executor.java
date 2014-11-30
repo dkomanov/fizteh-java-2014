f62e9d9c5d736cb489b30a2ebd07edb3d0e77e79
Egor Lunichkin <e.lunichkin@gmail.com>
2014-11-30 17:58:17 +0300

    JUnit 0.1
    Added a few classes

---
+++ src/ru/fizteh/fivt/students/EgorLunichkin/JUnit/Executor.java
@@ -0,0 +1,19 @@
+package ru.fizteh.fivt.students.EgorLunichkin.JUnit;
+
+public class Executor {
+    public Executor(String[] args) {
+
+    }
+
+    void interactiveMode() {
+
+    }
+
+    void packageMode(String[] args) {
+
+    }
+
+    void executeCommand(String cmd) {
+        
+    }
+}
