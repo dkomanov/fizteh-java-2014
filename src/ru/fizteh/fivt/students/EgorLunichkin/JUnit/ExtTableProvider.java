f6c13dc833e47bdfb98e83ceff1b0af0bbd11f78
Egor Lunichkin <e.lunichkin@gmail.com>
2014-11-30 18:17:31 +0300

    JUnit 0.1
    Implemented interfaces from ru.fizteh.fivt.storage.strings

---
+++ src/ru/fizteh/fivt/students/EgorLunichkin/JUnit/ExtTableProvider.java
@@ -0,0 +1,18 @@
+package ru.fizteh.fivt.students.EgorLunichkin.JUnit;
+
+import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.storage.strings.TableProvider;
+
+public class ExtTableProvider implements TableProvider {
+    public Table getTable(String name) {
+        return null;
+    }
+
+    public Table createTable(String name) {
+        return null;
+    }
+
+    public void removeTable(String name) {
+
+    }
+}
---
+++ src/ru/fizteh/fivt/students/EgorLunichkin/JUnit/ExtTableProviderFactory.java
@@ -0,0 +1,10 @@
+package ru.fizteh.fivt.students.EgorLunichkin.JUnit;
+
+import ru.fizteh.fivt.storage.strings.TableProvider;
+import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+
+public class ExtTableProviderFactory implements TableProviderFactory {
+    public TableProvider create(String dir) {
+        return null;
+    }
+}
