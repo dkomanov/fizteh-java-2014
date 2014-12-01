a15ce6b950f845cdb3a53b465af07c8d465d212a
IgorTitov396 <1hp_phoenix@mail.ru>
2014-12-01 23:16:02 +0300

    V 1.2
    Added tests, put all the folders in the JUnit., fixed the errors.

--- src/ru/fizteh/fivt/students/titov/FileMap/CommandFileMap.java
+++
@@ -1,4 +0,0 @@
-package ru.fizteh.fivt.students.titov.FileMap;
-
-public abstract class CommandFileMap extends Command<FileMap> {
-}
--- src/ru/fizteh/fivt/students/titov/MultiFileHashMap/CommandMultiFileHashMap.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.titov.MultiFileHashMap;
-
-import ru.fizteh.fivt.students.titov.FileMap.Command;
-
-public abstract class CommandMultiFileHashMap extends Command<MFileHashMap> {
-}
