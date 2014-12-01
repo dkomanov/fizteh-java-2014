c88f484e56e937d66108f3519054153da6c11095
Mark Ryabov <mir@epage.ru>
2014-12-01 13:37:29 +0300

    Another fix

--- src/ru/fizteh/fivt/students/ryad0m/junit/Main.java
+++ src/ru/fizteh/fivt/students/ryad0m/junit/Main.java
@@ -3,8 +3,6 @@ package ru.fizteh.fivt.students.ryad0m.junit;
 import ru.fizteh.fivt.storage.strings.TableProvider;
 import ru.fizteh.fivt.storage.strings.TableProviderFactory;
 
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -15,10 +13,8 @@ public class Main {
                 TableProvider tableProvider = tableProviderFactory.create(System.getProperty("fizteh.db.dir"));
                 Shell shell = new Shell(tableProvider);
                 shell.start(args);
-            } catch (IOException e) {
-                System.out.println("IO error");
-            } catch (BadFormatException e) {
-                System.out.println("Format error");
+            } catch (Exception e) {
+                System.out.println("Something went wrong");
             }
         } else {
             System.out.println("Obossan!");
