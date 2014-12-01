5f549fc83d71e7827f88b772c7f57090855bda68
ZatsepinMikhail <salamence1996@gmail.com>
2014-12-01 16:03:32 +0300

    begin testing logging factory

---
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/Proxy/JUnit/TestLoggingfactory.java
@@ -0,0 +1,20 @@
+package ru.fizteh.fivt.students.ZatsepinMikhail.Proxy.JUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.fizteh.fivt.storage.structured.*;
+import ru.fizteh.fivt.students.ZatsepinMikhail.Proxy.FileMap.FileMap;
+import ru.fizteh.fivt.students.ZatsepinMikhail.Proxy.MultiFileHashMap.MFileHashMap;
+import ru.fizteh.fivt.students.ZatsepinMikhail.Proxy.MultiFileHashMap.MFileHashMapFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestLoggingfactory {
+}
