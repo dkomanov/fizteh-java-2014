65ba30dfca50af7a9dd70e79d9d0fbdb8588b101
Norrius <norrius@gmail.com>
2014-12-02 22:54:59 +0300

    XML logger proxy.

--- src/ru/fizteh/fivt/students/andreyzakharov/proxedfilemap/XmlLoggingProxyFactory.java
+++ src/ru/fizteh/fivt/students/andreyzakharov/proxedfilemap/XmlLoggingProxyFactory.java
@@ -3,10 +3,13 @@ package ru.fizteh.fivt.students.andreyzakharov.proxedfilemap;
 import ru.fizteh.fivt.proxy.LoggingProxyFactory;
 
 import java.io.Writer;
+import java.lang.reflect.Proxy;
 
 public class XmlLoggingProxyFactory implements LoggingProxyFactory {
     @Override
     public Object wrap(Writer writer, Object implementation, Class<?> interfaceClass) {
-        return null;
+        return Proxy.newProxyInstance(implementation.getClass().getClassLoader(),
+                new Class[]{interfaceClass},
+                new XmlLoggingProxyInvocationHandler(writer, implementation));
     }
 }
