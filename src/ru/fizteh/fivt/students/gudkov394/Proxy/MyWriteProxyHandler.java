6483d5d2db025b32843635decf4e73f07340a93d
kagudkov <kagudkov@gmail.com>
2014-12-02 17:43:52 +0300

    finished

--- src/ru/fizteh/fivt/students/gudkov394/Proxy/MyWriteProxyHandler.java
+++ src/ru/fizteh/fivt/students/gudkov394/Proxy/MyWriteProxyHandler.java
@@ -1,21 +1,85 @@
 package ru.fizteh.fivt.students.gudkov394.Proxy;
 
 
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.IdentityHashMap;
-import java.util.List;
 
 /**
  * Created by kagudkov on 30.11.14.
  */
 public class MyWriteProxyHandler implements InvocationHandler {
+
     private Writer writer;
+    private Object implementation;
+    MyWriteProxyHandler(Writer wr, Object impl) {
+        writer = wr;
+        implementation = impl;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<invoke timestamp=\"" + System.currentTimeMillis() + "\" class=\""
+                + implementation.getClass().getCanonicalName() + "\" name=\"" + method.getName() + "\">");
+        if (args != null && args.length > 0) {
+            stringBuilder.append((new ParserXML()).parse(args));
+        } else {
+            stringBuilder.append("<arguments/>");
+        }
+        try {
+            Object result;
+            if (args != null) {
+                result = method.invoke(implementation, args);
+            } else {
+                result = method.invoke(implementation);
+            }
+            if (method.getReturnType() != void.class) {
+                String res;
+                if (result == null || result instanceof Iterable) {
+                    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                    Document document = builder.newDocument();
+                    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                    StringWriter stringWriter = new StringWriter();
+                    transformer.transform(new DOMSource((new ParserXML()).parseObject(result,
+                                    new IdentityHashMap<>(), document)),
+                            new StreamResult(stringWriter));
+                    res = stringWriter.toString();
+                } else {
+                    res = result.toString();
+                }
+                stringBuilder.append("<return>" + res + "</return>");
+            }
+            return result;
+        } catch (InvocationTargetException e) {
+            stringBuilder.append("<thrown>" + e.getTargetException() + "</thrown>");
+            throw e;
+        } finally {
+            stringBuilder.append("</invoke>");
+            try {
+                writer.write(stringBuilder.toString());
+            } catch (IOException e) {
+                System.out.println("Can't write log");
+            }
+            writer.flush();
+        }
+    }
+}
+  /*  private Writer writer;
     private Object object;
 
     public MyWriteProxyHandler(Writer writerTmp, Object implementation) {
@@ -94,4 +158,4 @@ public class MyWriteProxyHandler implements InvocationHandler {
             xmlStreamWriter.writeEndElement();
         }
     }
-}
+    */
--- src/ru/fizteh/fivt/students/gudkov394/Proxy/test/TestInterface.java
+++ src/ru/fizteh/fivt/students/gudkov394/Proxy/test/TestInterface.java
@@ -6,22 +6,15 @@ import java.util.List;
  * Created by kagudkov on 02.12.14.
  */
 public interface TestInterface {
-    void noArgumentsMethod();
 
+    void emptyArgumentMethod();
     void integerArgumentMethod(Integer a);
-
     void listArgumentMethod(List<Object> list);
-
-    void twoStringArgumentMethod(String str1, String str2);
-
-    void iOExceptionHiMethod() throws Exception;
-
-    void voidReturningMethod();
-
-    Integer zeroReturningMethod();
-
+    void stringArgumentsMethod(String str1, String str2);
+    void exceptionMethod() throws Exception;
+    void voidMethod();
+    Integer integerReturningMethod();
     Object nullReturningMethod();
-
     List<Object> cyclicListReturningMethod();
 }
 
