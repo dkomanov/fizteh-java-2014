fa9dc4f16a187c1142bbf1d061ccdbee03b8fe8e
DeSerg <sergpopov95@gmail.com>
2014-12-03 01:46:04 +0300

    FinalCommit

--- src/ru/fizteh/fivt/students/deserg/junit/DbTableProvider.java
+++ src/ru/fizteh/fivt/students/deserg/junit/DbTableProvider.java
@@ -4,7 +4,6 @@ import ru.fizteh.fivt.storage.strings.Table;
 import ru.fizteh.fivt.storage.strings.TableProvider;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
