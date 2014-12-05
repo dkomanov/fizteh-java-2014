618c2b719f83e36a2cdf95a316b03310cd2f94c0
Egor Lunichkin <e.lunichkin@gmail.com>
2014-12-05 21:27:41 +0300

    Storeable 0.5.4
    The rest of commands implemented

---
+++ src/ru/fizteh/fivt/students/EgorLunichkin/Storeable/commands/CommitCommand.java
@@ -0,0 +1,27 @@
+package ru.fizteh.fivt.students.EgorLunichkin.Storeable.commands;
+
+import ru.fizteh.fivt.students.EgorLunichkin.Storeable.StoreableException;
+import ru.fizteh.fivt.students.EgorLunichkin.Storeable.StoreableTableProvider;
+
+import java.io.IOException;
+
+public class CommitCommand implements Command {
+    public CommitCommand(StoreableTableProvider stp) {
+        sTableProvider = stp;
+    }
+
+    private StoreableTableProvider sTableProvider;
+
+    @Override
+    public void run() throws StoreableException{
+        if (sTableProvider.getUsing() == null) {
+            System.out.println("no table");
+        } else {
+            try {
+                System.out.println(sTableProvider.getUsing().commit());
+            } catch (IOException ex) {
+                throw new StoreableException(ex.getMessage());
+            }
+        }
+    }
+}
