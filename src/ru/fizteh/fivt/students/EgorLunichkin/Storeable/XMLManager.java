c8e16d34e6f36f4e8ba508fcb4de6a47be26d78d
Egor Lunichkin <e.lunichkin@gmail.com>
2014-12-05 03:18:07 +0300

    Storeable 0.4
    XMLManager implemented

--- src/ru/fizteh/fivt/students/EgorLunichkin/Storeable/XMLManager.java
+++ src/ru/fizteh/fivt/students/EgorLunichkin/Storeable/XMLManager.java
@@ -1,14 +1,83 @@
 package ru.fizteh.fivt.students.EgorLunichkin.Storeable;
 
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class XMLManager {
-    public static String serialize(List<Object> objects) {
-        return null;
+    public static String serialize(List<Object> objects) throws StoreableException {
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+            Element row = document.createElement("row");
+            document.appendChild(row);
+            for (Object object : objects) {
+                if (object == null) {
+                    Element nullElement = document.createElement("null");
+                    row.appendChild(nullElement);
+                } else {
+                    Element col = document.createElement("column");
+                    Text text = document.createTextNode(object.toString());
+                    col.appendChild(text);
+                    row.appendChild(col);
+                }
+            }
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter stringWriter = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+            return stringWriter.toString();
+        } catch (ParserConfigurationException | TransformerException ex) {
+            throw new StoreableException("Something bad while seriaizing");
+        }
     }
 
-    public static List<Object> deserialize(String data, List<Class<?>> types) {
-        return null;
+    public static List<Object> deserialize(String data, List<Class<?>> types)
+            throws ParseException, ParserConfigurationException {
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        try {
+            Document document = documentBuilder.parse(new InputSource(new StringReader(data)));
+            if (document.getChildNodes().getLength() != 1) {
+                throw new ParseException("One root in XML tree expected", 0);
+            }
+            Node row = document.getChildNodes().item(0);
+            if (!row.getNodeName().equals("row")) {
+                throw new ParseException("Root must have name \"row\"", 0);
+            }
+            NodeList cols = row.getChildNodes();
+            if (cols.getLength() != types.size()) {
+                throw new ParseException("Incorrect number of columns", 0);
+            }
+            List<Object> objectList = new ArrayList<>();
+            for (int ind = 0; ind < row.getChildNodes().getLength(); ++ind) {
+                Node currentNode = cols.item(ind);
+                if (currentNode.getNodeName().equals("column")) {
+                    if (currentNode.getChildNodes().getLength() > 1) {
+                        throw new ParseException("COLUMN must have text content only", ind);
+                    }
+                    objectList.add(parseValue(currentNode.getTextContent(), types.get(ind)));
+                } else if (currentNode.getNodeName().equals("null")) {
+                    if (currentNode.getChildNodes().getLength() > 0) {
+                        throw new ParseException("NULL must be a single tag", ind);
+                    }
+                    objectList.add(null);
+                } else {
+                    throw new ParseException("Incorrect tag inside row statement: " + currentNode.getNodeName(), ind);
+                }
+            }
+            return objectList;
+        } catch (SAXException | IOException ex) {
+            throw new ParseException(ex.getMessage(), 0);
+        }
     }
 
     private static Object parseValue(String value, Class type) {
