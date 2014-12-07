a79f0b613139b26a4010c413e6aa60fa65bb163f
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-12-07 14:06:07 +0300

    Fixed bug in list

--- src/ru/fizteh/fivt/students/AlexeyZhuravlev/parallel/ParallelTable.java
+++ src/ru/fizteh/fivt/students/AlexeyZhuravlev/parallel/ParallelTable.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
 
 /**
  * @author AlexeyZhuravlev
@@ -79,7 +80,10 @@ public class ParallelTable implements Table {
     public List<String> list() {
         lock.readLock().lock();
         try {
-            return originalTable.list();
+            List<String> list = originalTable.list();
+            list.addAll(diff.get().getCreated().stream().collect(Collectors.toList()));
+            list.removeAll(diff.get().getRemoved().stream().collect(Collectors.toList()));
+            return list;
         } finally {
             lock.readLock().unlock();
         }
