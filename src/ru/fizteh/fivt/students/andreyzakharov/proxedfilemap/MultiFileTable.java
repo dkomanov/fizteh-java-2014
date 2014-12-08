4c3e2ca44f7d9d1e0c247e5e9f44cb16c383484c
Norrius <norrius@gmail.com>
2014-12-08 19:36:14 +0300

    Fixed sync().

--- src/ru/fizteh/fivt/students/andreyzakharov/proxedfilemap/MultiFileTable.java
+++ src/ru/fizteh/fivt/students/andreyzakharov/proxedfilemap/MultiFileTable.java
@@ -319,6 +319,7 @@ public class MultiFileTable implements Table, AutoCloseable {
                     --diff.get().pending;
                 }
             }
+            version.set(stableVersion);
         }
     }
 
