b3830e10d71e7823913917a730fbd973f6258904
kagudkov <kagudkov@gmail.com>
2014-12-09 16:26:38 +0300

    I am exhausted

--- src/ru/fizteh/fivt/students/gudkov394/telnet/Server.java
+++ src/ru/fizteh/fivt/students/gudkov394/telnet/Server.java
@@ -116,125 +116,126 @@ public class Server {
         private String runCommand(String command) {
             String[] cmd = command.split("\\s(?![^\\(]*\\))(?![^\\[]*\\])");
             StringBuilder builder = new StringBuilder("");
-            if (command.equals("show tables")) {
-                provider.listTables().forEach(builder::append);
-                return builder.toString();
-            }
-            if (command.equals("get columns count")) {
-                if (currentT == null) {
-                    return ("No tables in usage");
-                }
-                return builder.append(currentT.getColumnsCount()).toString();
-            }
-            if (command.equals("get name")) {
-                if (currentT == null) {
-                    return ("No tables in usage");
-                }
-                return currentT.getName();
-            }
-            try {
-                switch (cmd[0]) {
-                    case "use":
-                        if (currentT != null && currentT.changesNumber() > 0) {
-                            return ("Can't use: " + currentT.changesNumber() + " unsaved shanges");
+            switch (command) {
+                case "show tables":
+                    provider.listTables().forEach(builder::append);
+                    return builder.toString();
+                case "get columns count":
+                    if (currentT == null) {
+                        return ("No tables in usage");
+                    }
+                    return builder.append(currentT.getColumnsCount()).toString();
+                case "get name":
+                    if (currentT == null) {
+                        return ("No tables in usage");
+                    }
+                    return currentT.getName();
+                default:
+                    try {
+                        switch (cmd[0]) {
+                            case "use":
+                                if (currentT != null && currentT.changesNumber() > 0) {
+                                    return ("Can't use: " + currentT.changesNumber() + " unsaved shanges");
+                                }
+                                if (provider.getTable(cmd[1]) != null) {
+                                    currentT = (CurrentTable) provider.getTable(cmd[1]);
+                                    currentT.check();
+                                    return ("Using " + cmd[1]);
+                                } else {
+                                    return ("Table doesn't exist");
+                                }
+                            case "drop":
+                                if (provider.getTable(cmd[1]) != null) {
+                                    if (currentT == provider.getTable(cmd[1])) {
+                                        currentT = null;
+                                    }
+                                    provider.removeTable(cmd[1]);
+                                } else {
+                                    return (cmd[1] + " doesn't exist");
+                                }
+                                break;
+                            case "create":
+                                if (cmd.length != 3) {
+                                    return ("wrong number of argument to Create");
+                                }
+                                String name = cmd[1];
+                                if (provider.getTable(name) != null) {
+                                    return ("Already exists");
+                                }
+                                String forSignature = cmd[2].replaceFirst("\\(", "");
+                                forSignature = forSignature.trim();
+                                forSignature = forSignature.substring(0, forSignature.length() - 1);
+                                Utils utils = new Utils();
+                                ArrayList<Class<?>> signature = utils.signature(forSignature);
+                                try {
+                                    provider.createTable(name, signature);
+                                    return ("created");
+                                } catch (IOException e) {
+                                    return ("I can't create table" + " " + e.getMessage());
+                                }
+                            case "get":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                if (cmd.length == 4 && cmd[1].equals("column") && cmd[2].equals("type")) {
+                                    return (currentT.getColumnType(Integer.parseInt(cmd[3]))).toString();
+                                }
+                                if (currentT.get(cmd[1]) == null) {
+                                    return null;
+                                }
+                                return currentT.get(cmd[1]).toString();
+                            case "put":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                if (currentT.put(cmd[1], new Parser().deserialize(currentT, cmd[2])) == null) {
+                                    return null;
+                                }
+                                return currentT.put(cmd[1], new Parser().deserialize(currentT, cmd[2])).toString();
+                            case "list":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                currentT.list().forEach(builder::append);
+                                return builder.toString();
+                            case "remove":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                if (currentT.remove(cmd[1]) == null) {
+                                    return null;
+                                }
+                                return currentT.remove(cmd[1]).toString();
+                            case "size":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                return builder.append(currentT.size()).toString();
+                            case "commit":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                return builder.append(currentT.commit()).toString();
+                            case "rollback":
+                                if (currentT == null) {
+                                    return ("No tables in usage");
+                                }
+                                return builder.append(currentT.rollback()).toString();
+                            default:
+                                return ("Unknown command");
                         }
-                        if (provider.getTable(cmd[1]) != null) {
-                            currentT = (CurrentTable) provider.getTable(cmd[1]);
-                            currentT.check();
-                            return ("Using " + cmd[1]);
-                        } else {
-                            return ("Table doesn't exist");
+                    } catch (IOException e) {
+                        return ("IOException " + e.getMessage());
+                    } catch (ParseException e) {
+                        builder.append("Wrong JSON format. Expected: ");
+                        for (int i = 0; i < currentT.getColumnsCount(); i++) {
+                            builder.append(currentT.getColumnType(i).getName()).append(" ");
                         }
-                    case "drop":
-                        if (provider.getTable(cmd[1]) != null) {
-                            if (currentT == provider.getTable(cmd[1])) {
-                                currentT = null;
-                            }
-                            provider.removeTable(cmd[1]);
-                        } else {
-                            return (cmd[1] + " doesn't exist");
-                        }
-                        break;
-                    case "create":
-                        if (cmd.length != 3) {
-                            return ("wrong number of argument to Create");
-                        }
-                        String name = cmd[1];
-                        if (provider.getTable(name) != null) {
-                            return ("Already exists");
-                        }
-                        String forSignature = cmd[2].replaceFirst("\\(", "");
-                        forSignature = forSignature.trim();
-                        forSignature = forSignature.substring(0, forSignature.length() - 1);
-                        Utils utils = new Utils();
-                        ArrayList<Class<?>> signature = utils.signature(forSignature);
-                        try {
-                            provider.createTable(name, signature);
-                            return ("created");
-                        } catch (IOException e) {
-                            return ("I can't create table" + " " + e.getMessage());
-                        }
-                    case "get":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        if (cmd.length == 4 && cmd[1].equals("column") && cmd[2].equals("type")) {
-                            return (currentT.getColumnType(Integer.parseInt(cmd[3]))).toString();
-                        }
-                        if (currentT.get(cmd[1]) == null) {
-                            return null;
-                        }
-                        return currentT.get(cmd[1]).toString();
-                    case "put":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        if (currentT.put(cmd[1], new Parser().deserialize(currentT, cmd[2])) == null) {
-                            return null;
-                        }
-                        return currentT.put(cmd[1], new Parser().deserialize(currentT, cmd[2])).toString();
-                    case "list":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        currentT.list().forEach(builder::append);
                         return builder.toString();
-                    case "remove":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        if (currentT.remove(cmd[1]) == null) {
-                            return null;
-                        }
-                        return currentT.remove(cmd[1]).toString();
-                    case "size":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        return builder.append(currentT.size()).toString();
-                    case "commit":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        return builder.append(currentT.commit()).toString();
-                    case "rollback":
-                        if (currentT == null) {
-                            return ("No tables in usage");
-                        }
-                        return builder.append(currentT.rollback()).toString();
-                    default:
-                        return ("Unknown command");
-                }
-            } catch (IOException e) {
-                return ("IOException " + e.getMessage());
-            } catch (ParseException e) {
-                builder.append("Wrong JSON format. Expected: ");
-                for (int i = 0; i < currentT.getColumnsCount(); i++) {
-                    builder.append(currentT.getColumnType(i).getName()).append(" ");
-                }
-                return builder.toString();
-            } catch (IndexOutOfBoundsException e) {
-                return ("Wrong command format");
+                    } catch (IndexOutOfBoundsException e) {
+                        return ("Wrong command format");
+                    }
+                    break;
             }
             return "";
         }
