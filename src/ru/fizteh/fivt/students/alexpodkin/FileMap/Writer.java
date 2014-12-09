9269fe00ef8b945d54e69e374a998591f10a8ba7
Alex Podkin <alexpodkin@gmail.com>
2014-12-09 17:50:26 +0300

    close method added and now data saves

--- src/ru/fizteh/fivt/students/alexpodkin/FileMap/Writer.java
+++ src/ru/fizteh/fivt/students/alexpodkin/FileMap/Writer.java
@@ -5,9 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class Writer {
+public class Writer implements AutoCloseable {
 
     private String fileMapPath;
+    private DataOutputStream dataOutputStream;
 
     public Writer(String path) {
         fileMapPath = path;
@@ -19,12 +20,16 @@ public class Writer {
         dataOutputStream.write(byteWord);
     }
 
-    public void writeDataToFile(HashMap<String, String> fileMap) throws IOException {
-        DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileMapPath));
+    public void writeDataToFile(HashMap<String, String> fileMap) throws Exception {
+        dataOutputStream = new DataOutputStream(new FileOutputStream(fileMapPath));
         for (HashMap.Entry<String, String> entry : fileMap.entrySet()) {
             writeWord(dataOutputStream, entry.getKey());
             writeWord(dataOutputStream, entry.getValue());
         }
     }
 
+    @Override
+    public void close() throws Exception {
+        dataOutputStream.close();
+    }
 }
