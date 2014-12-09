1fdcaeaaf61fbe3a94695bdfd2cb0dd3b6d2b305
lyaronskaya <luba.yaronskaya@gmail.com>
2014-12-09 14:28:21 +0300

    checkstyle

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/MultiFileHashMap/MultiFileHashMap.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/MultiFileHashMap/MultiFileHashMap.java
@@ -3,7 +3,6 @@ package ru.fizteh.fivt.students.YaronskayaLiubov.MultiFileHashMap;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Scanner;
