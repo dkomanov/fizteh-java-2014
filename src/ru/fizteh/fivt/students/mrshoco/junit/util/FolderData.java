19a9f1ca95143b0e57ef042dc7b0e2b269049447
mrshoco <karenmanuk@yandex.ru>
2014-12-10 05:56:37 +0400

    small bag

--- src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
+++ src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
@@ -1,23 +1,12 @@
 package junit.util;
 
 import java.io.File;
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Map;
-=======
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
 import java.util.HashMap;
 
 public class FolderData extends Data {
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static Map<String, String> checkFile(Map<String, String> hashMap, 
-                                                    int fileNumber) throws Exception {
-=======
     private static HashMap<String, String> checkFile(HashMap<String, String> hashMap, 
                                                     int fileNumber) throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         for (String key : hashMap.keySet()) {
             if (key.hashCode() / 16 % 16 != fileNumber) {
                 throw new IllegalArgumentException("Key was in wrong place !");
@@ -26,13 +15,8 @@ public class FolderData extends Data {
         return hashMap;
     }
     
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static Map<String, String> checkFolder(Map<String, String> hashMap, 
-                                                    int fileNumber) throws Exception {
-=======
     private static HashMap<String, String> checkFolder(HashMap<String, String> hashMap, 
                                                     int fileNumber) throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         for (String key : hashMap.keySet()) {
             if (key.hashCode() % 16 != fileNumber) {
                 throw new IllegalArgumentException("Key was in wrong place !");
@@ -41,28 +25,19 @@ public class FolderData extends Data {
         return hashMap;
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    public static Map<String, String> loadDb(File file) throws Exception {
-=======
     public static HashMap<String, String> loadDb(File file) throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         if (!file.isDirectory()) {
             throw new IllegalArgumentException("Db isn't a directory !");
         }
-        Map<String, String> data = new HashMap<String, String>();
+        HashMap<String, String> data = new HashMap<String, String>();
         for (int i = 0; i < 16; i++) {
             data.putAll(checkFolder(loadFolder(new File(file, i + ".dir")), i));
         }
         return data;
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static Map<String, String> loadFolder(File file) throws Exception {
-        Map<String, String> dataFromFolder = new HashMap<String, String>();
-=======
     private static HashMap<String, String> loadFolder(File file) throws IllegalArgumentException {
         HashMap<String, String> dataFromFolder = new HashMap<String, String>();
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         if (!file.exists()) {
             return dataFromFolder;
         }
@@ -75,13 +50,8 @@ public class FolderData extends Data {
         return dataFromFolder;
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static Map<String, String> loadFile(File file) throws Exception {
-        Map<String, String> dataFromFile = new HashMap<String, String>();
-=======
     private static HashMap<String, String> loadFile(File file) throws IllegalArgumentException {
         HashMap<String, String> dataFromFile = new HashMap<String, String>();
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         if (!file.exists()) {
             return dataFromFile;
         }
@@ -93,13 +63,9 @@ public class FolderData extends Data {
         return  dataFromFile;
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    public static void saveDb(Map<String, String> data, File file) throws Exception {
-=======
     public static void saveDb(HashMap<String, String> data, File file) throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         @SuppressWarnings("unchecked")
-        Map<String, String>[][] sortedData = new HashMap[16][16];
+        HashMap<String, String>[][] sortedData = new HashMap[16][16];
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 16; j++) {
                 sortedData[i][j] = new HashMap<String, String>();
@@ -116,13 +82,8 @@ public class FolderData extends Data {
         }
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static void saveFolder(Map<String, String>[] dataToFolder, File file) 
-                                                            throws Exception {
-=======
     private static void saveFolder(HashMap<String, String>[] dataToFolder, File file) 
                                                             throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         if (!file.exists()) {
             file.mkdir();
         } else if (!file.isDirectory()) {
@@ -138,13 +99,8 @@ public class FolderData extends Data {
         }
     }
 
-<<<<<<< HEAD:src/ru/fizteh/fivt/students/mrshoco/multifilehashmap/util/FolderData.java
-    private static boolean saveFile(Map<String, String> dataToFile, File file) 
-                                                            throws Exception {
-=======
     private static boolean saveFile(HashMap<String, String> dataToFile, File file) 
                                                             throws IllegalArgumentException {
->>>>>>> junit:src/ru/fizteh/fivt/students/mrshoco/junit/util/FolderData.java
         if (!file.exists()) {
             try {
                 file.createNewFile();
