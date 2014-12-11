b759080c27ef85d06860ecc17af65fa72196089e
Mark Ryabov <mir@epage.ru>
2014-12-12 00:17:57 +0300

    File change

--- src/ru/fizteh/fivt/students/ryad0m/parallel/MyTableProvider.java
+++ src/ru/fizteh/fivt/students/ryad0m/parallel/MyTableProvider.java
@@ -12,10 +12,12 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MyTableProvider implements TableProvider {
     private Path location;
     private HashMap<String, StructedTable> tables;
+    ReentrantReadWriteLock lock;
 
 
     public MyTableProvider(Path path) throws IOException {
@@ -38,14 +40,21 @@ public class MyTableProvider implements TableProvider {
 
     @Override
     public Table getTable(String name) {
-        if (name == null) {
-            throw new IllegalArgumentException();
+        Table res;
+        lock.readLock().lock();
+        try {
+            if (name == null) {
+                throw new IllegalArgumentException();
+            }
+            if (tables.containsKey(name)) {
+                res = new TransactionTable(tables.get(name));
+            } else {
+                res = null;
+            }
+        } finally {
+            lock.readLock().unlock();
         }
-        if (tables.containsKey(name)) {
-            return new TransactionTable(tables.get(name));
-        } else {
-            return null;
-        }
+        return res;
     }
 
     private boolean checkName(String s) {
@@ -58,55 +67,81 @@ public class MyTableProvider implements TableProvider {
 
     @Override
     public Table createTable(String name, List<Class<?>> columnTypes) throws IOException {
-        if (!checkName(name)) {
-            throw new IllegalArgumentException();
+        StructedTable res;
+        lock.writeLock().lock();
+        try {
+            if (!checkName(name)) {
+                throw new IllegalArgumentException();
+            }
+            if (tables.containsKey(name)) {
+                res = null;
+            }
+            res = new StructedTable(location.resolve(name).normalize(), columnTypes);
+            tables.put(name, res);
+        } finally {
+            lock.writeLock().unlock();
         }
-        if (tables.containsKey(name)) {
-            return null;
-        }
-        StructedTable t = new StructedTable(location.resolve(name).normalize(), columnTypes);
-        tables.put(name, t);
-        return new TransactionTable(t);
+        return new TransactionTable(res);
     }
 
     @Override
     public void removeTable(String name) throws IOException {
-        if (name == null) {
-            throw new IllegalArgumentException();
+        lock.writeLock().lock();
+        try {
+            if (name == null) {
+                throw new IllegalArgumentException();
+            }
+            if (!tables.containsKey(name)) {
+                throw new IllegalStateException();
+            }
+            tables.get(name).deleteData();
+            tables.remove(name);
+        } finally {
+            lock.writeLock().unlock();
         }
-        if (!tables.containsKey(name)) {
-            throw new IllegalStateException();
-        }
-        tables.get(name).deleteData();
-        tables.remove(name);
     }
 
     @Override
     public Storeable deserialize(Table table, String value) throws ParseException {
+        lock.readLock().lock();
         List<Class<?>> list = ((TransactionTable) table).getStructedTable().getColumnTypes();
+        lock.readLock().unlock();
         return new MyStorable(list, XmlSerializer.deserializeString(value, list));
     }
 
     @Override
     public String serialize(Table table, Storeable value) throws ColumnFormatException {
+        lock.readLock().lock();
         ((TransactionTable) table).getStructedTable().checkIntegrity(value);
+        lock.readLock().unlock();
         return XmlSerializer.serializeObjectList(((MyStorable) value).getValues());
     }
 
     @Override
     public Storeable createFor(Table table) {
+        lock.readLock().lock();
         List<Class<?>> list = ((TransactionTable) table).getStructedTable().getColumnTypes();
+        lock.readLock().unlock();
         return new MyStorable(list);
     }
 
     @Override
     public Storeable createFor(Table table, List<?> values) throws ColumnFormatException, IndexOutOfBoundsException {
+        lock.readLock().lock();
         List<Class<?>> list = ((TransactionTable) table).getStructedTable().getColumnTypes();
+        lock.readLock().unlock();
         return new MyStorable(list, values);
     }
 
     @Override
     public List<String> getTableNames() {
-        return new ArrayList<String>(tables.keySet());
+        lock.readLock().lock();
+        List<String> res;
+        try {
+            res = new ArrayList<String>(tables.keySet());
+        } finally {
+            lock.readLock().unlock();
+        }
+        return res;
     }
 }
