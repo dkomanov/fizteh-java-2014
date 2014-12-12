e88465a9fa347c75bbd089636b8ec3105d7166f7
Kirill <kirivasile@yandex.ru>
2014-12-12 20:20:50 +0300

    Telnet: я уже начинаю что-то подозревать

--- src/ru/fizteh/fivt/students/VasilevKirill/telnet/Storeable/StoreableParser.java
+++ src/ru/fizteh/fivt/students/VasilevKirill/telnet/Storeable/StoreableParser.java
@@ -1,6 +1,7 @@
 package ru.fizteh.fivt.students.VasilevKirill.telnet.Storeable;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import ru.fizteh.fivt.storage.structured.Storeable;
 import ru.fizteh.fivt.students.VasilevKirill.telnet.structures.MyStorable;
 
@@ -11,22 +12,26 @@ import java.text.ParseException;
  */
 public class StoreableParser {
     public static Storeable stringToStoreable(String value, Class[] typeList) throws ParseException {
-        if (value == null || (value.charAt(0) != '[' && value.charAt(value.length() - 1) != ']')) {
-            throw new IllegalArgumentException("StoreableParser: illegal argument");
-        }
-        Storeable result = new MyStorable(typeList);
-        JSONArray parser = new JSONArray(value);
-        for (int i = 0; i < parser.length(); ++i) {
-            try {
-                if (parser.isNull(i)) {
-                    result.setColumnAt(i, null);
-                } else {
-                    result.setColumnAt(i, typeList[i].cast(parser.get(i)));
-                }
-            } catch (ClassCastException e) {
-                throw new ParseException(parser.get(i).toString(), 0);
+        try {
+            if (value == null || (value.charAt(0) != '[' && value.charAt(value.length() - 1) != ']')) {
+                throw new IllegalArgumentException("StoreableParser: illegal argument");
             }
+            Storeable result = new MyStorable(typeList);
+            JSONArray parser = new JSONArray(value);
+            for (int i = 0; i < parser.length(); ++i) {
+                try {
+                    if (parser.isNull(i)) {
+                        result.setColumnAt(i, null);
+                    } else {
+                        result.setColumnAt(i, typeList[i].cast(parser.get(i)));
+                    }
+                } catch (ClassCastException e) {
+                    throw new ParseException(parser.get(i).toString(), 0);
+                }
+            }
+            return result;
+        } catch (JSONException e) {
+            return null;
         }
-        return result;
     }
 }
