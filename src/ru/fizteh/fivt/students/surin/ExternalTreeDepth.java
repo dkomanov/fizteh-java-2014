7735c2ca4d05735f88622da5e3d45f6717cba8a0
mike <surinmike@gmail.com>
2014-12-13 18:49:41 +0300

    v2.0

--- src/ru/fizteh/fivt/students/surin/ExternalTreeDepth.java
+++ src/ru/fizteh/fivt/students/surin/ExternalTreeDepth.java
@@ -1,94 +1,114 @@
 package ru.fizteh.fivt.students.surin;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
  * Created by mike on 11.12.14.
  */
 public class ExternalTreeDepth {
+    static Integer root = null;
 
-    static Integer findRoot(File edges, int n) throws IOException, ClassNotFoundException {
+    static void findRoot(File edges, int n) throws IOException, ClassNotFoundException {
         File buf1 = File.createTempFile("iterate", ".tmp");
         File buf2 = File.createTempFile("iterate", ".tmp");
-        try (ObjectOutputStream os = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(buf1)));
-             BufferedReader inp = new BufferedReader(new FileReader(edges))) {
-            os.writeObject(n + 1);
-            String ss;
-            while ((ss = inp.readLine()) != null) {
+        ObjectOutputStream os = new ObjectOutputStream(buf1, ExternalTreeDepth::writeInt);
+        BufferedReader inp = new BufferedReader(new FileReader(edges));
+        os.write(n + 1);
+        String ss;
+        while ((ss = inp.readLine()) != null) {
                 StringTokenizer st = new StringTokenizer(ss);
                 int parent = Integer.valueOf(st.nextToken());
                 int vertex = Integer.valueOf(st.nextToken());
                 if (vertex != 0) {
-                    os.writeObject(vertex);
+                    os.write(vertex);
                 }
-            }
-        } catch (EOFException e) {
-            //
         }
-        new ExternalSorter<Integer>(buf1, buf2).run();
-        try (ObjectInputStream ip = new ObjectInputStream(new BufferedInputStream(new FileInputStream(buf2)))) {
-            Integer prev = 0;
-            while (true) {
-                Integer cur = (Integer) ip.readObject();
-                if (cur - prev > 1) {
-                    buf1.delete();
-                    buf2.delete();
-                    return prev + 1;
-                }
-                prev = cur;
-            }
-        } catch (EOFException e) {
-            //
-        }
+        os.close();
+        new ExternalSorter<Integer>(buf1, buf2, ExternalTreeDepth::readInt, ExternalTreeDepth::writeInt).run();
+
+        FileIterator.Consumer consumer  =
+                new FileIterator.Consumer() {
+                    int prev = 0;
+                    @Override
+                    public void apply(Object o, ArrayList out) throws IOException {
+                        Integer cur = (Integer) o;
+                        if (cur - prev > 1) {
+                            ExternalTreeDepth.root = prev + 1;
+                        }
+                        prev = cur;
+                    }
+                };
+
+        FileIterator.forEach(new ObjectInputStream(buf2, ExternalTreeDepth::readInt), consumer);
         buf1.delete();
         buf2.delete();
-        return -1;
     }
 
     static void iterate(File parents) throws IOException, ClassNotFoundException {
         File buf1 = File.createTempFile("iterate", ".tmp");
         File buf2 = File.createTempFile("iterate", ".tmp");
-        try (ObjectOutputStream os = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(buf1)));
-             ObjectInputStream ip = new ObjectInputStream(new BufferedInputStream(new FileInputStream(parents)))) {
-            while (true) {
-                Pair<Integer, Pair<Integer, Integer>> p = (Pair<Integer, Pair<Integer, Integer>>) ip.readObject();
-                Integer v = p.first;
-                Integer par = p.second.first;
-                Integer h = p.second.second;
-                os.writeObject(new Pair<>(par, new Pair<>(v, h)));
-            }
-        } catch (EOFException ignored) {
-            //
-        }
-        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(buf1, buf2).run();
-        try (ObjectOutputStream os = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(buf1)));
-             ObjectInputStream ir = new ObjectInputStream(new BufferedInputStream(new FileInputStream(buf2)));
-             ObjectInputStream ip = new ObjectInputStream(new BufferedInputStream(new FileInputStream(parents)))) {
-            Pair<Integer, Pair<Integer, Integer>> p;
-            Integer v = -1;
-            Integer par = -1;
-            Integer h = -1;
-            while (true) {
-                Pair<Integer, Pair<Integer, Integer>> request = (Pair<Integer, Pair<Integer, Integer>>) ir.readObject();
-                while (request.first >= v) {
-                    if (request.first.equals(v)) {
-                        os.writeObject(new Pair<>(request.second.first, new Pair<>(par, h + request.second.second)));
-                        break;
-                    }
-                    p = (Pair<Integer, Pair<Integer, Integer>>) ip.readObject();
-                    v = p.first;
-                    par = p.second.first;
-                    h = p.second.second;
+        FileIterator.map(new ObjectInputStream(parents, ExternalTreeDepth::readTriple),
+                         new ObjectOutputStream(buf1, ExternalTreeDepth::writeTriple),
+                (Object o, ArrayList out) -> {
+                    Pair<Integer, Pair<Integer, Integer>> p = (Pair<Integer, Pair<Integer, Integer>>) o;
+                    Integer v = p.first;
+                    Integer par = p.second.first;
+                    Integer h = p.second.second;
+                    out.add(new Pair<>(par, new Pair<>(v, h)));
+                });
+        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(buf1, buf2,
+                ExternalTreeDepth::readTriple, ExternalTreeDepth::writeTriple).run();
+        FileIterator.joinWith(new ObjectInputStream(buf2, ExternalTreeDepth::readTriple),
+                                new ObjectInputStream(parents, ExternalTreeDepth::readTriple),
+                                new ObjectOutputStream(buf1, ExternalTreeDepth::writeTriple),
+                (Object a, Object b) -> ((Pair) a).first.compareTo(((Pair) b).first),
+                (Object req, Object orig) -> {
+                    Pair<Integer, Pair<Integer, Integer>> org = (Pair<Integer, Pair<Integer, Integer>>) orig;
+                    Integer v = org.first;
+                    Integer par = org.second.first;
+                    Integer h = org.second.second;
+                    Pair<Integer, Pair<Integer, Integer>> request = (Pair<Integer, Pair<Integer, Integer>>) req;
+                    return new Pair<>(request.second.first, new Pair<>(par, h + request.second.second));
                 }
-            }
-        } catch (EOFException ignored) {
-            //
-        }
-        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(buf1, parents).run();
+        );
+        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(buf1, parents,
+                ExternalTreeDepth::readTriple, ExternalTreeDepth::writeTriple).run();
         buf1.delete();
         buf2.delete();
     }
+
+    static void writeInt(Object value, OutputStream os) throws IOException {
+        int n = (Integer) value;
+        byte[] buf = new byte[] {
+                (byte) (n >> 24),
+                (byte) (n >> 16),
+                (byte) (n >> 8),
+                (byte) n};
+        os.write(buf);
+    }
+
+    static int readInt(InputStream is) throws IOException {
+        byte[] buf = new byte[4];
+        if (is.read(buf) != 4) {
+            throw new EOFException("eof");
+        }
+        return buf[3] + (((int) buf[2]) << 8) + (((int) buf[1]) << 16) + (((int) buf[0]) << 24);
+    }
+
+    static void writeTriple(Object o, OutputStream os) throws IOException {
+        Pair<Integer, Pair<Integer, Integer>> p = (Pair<Integer, Pair<Integer, Integer>>) o;
+        writeInt(p.first, os);
+        writeInt(p.second.first, os);
+        writeInt(p.second.second, os);
+    }
+
+    static Pair<Integer, Pair<Integer, Integer>> readTriple(InputStream is) throws IOException {
+        return new Pair<>(readInt(is), new Pair<>(readInt(is), readInt(is)));
+    }
+
+    static Integer result;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         if (args.length != 2) {
@@ -100,24 +120,26 @@ public class ExternalTreeDepth {
         File parents = File.createTempFile("tree", ".tmp");
         File tmp = File.createTempFile("tree", ".tmp");
         int n = 1;
-        ObjectOutputStream os = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(tmp)));
+        ObjectOutputStream os = new ObjectOutputStream(tmp, ExternalTreeDepth::writeTriple);
         String ss;
         while ((ss = inp.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(ss);
             int parent = Integer.valueOf(st.nextToken());
             int vertex = Integer.valueOf(st.nextToken());
             if (vertex != 0) {
-                os.writeObject(new Pair<>(vertex, new Pair(parent, 1)));
+                os.write(new Pair<>(vertex, new Pair(parent, 1)));
                 n++;
             }
         }
         inp.close();
-        Integer root = findRoot(infile, n);
-        assert root != -1;
-        os.writeObject(new Pair<>(root, new Pair<Integer, Integer>(root, 0)));
+        findRoot(infile, n);
+        assert root != null;
+        System.err.println(root);
+        os.write(new Pair<>(root, new Pair<>(root, 0)));
         os.close();
 
-        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(tmp, parents).run();
+        new ExternalSorter<Pair<Integer, Pair<Integer, Integer>>>(tmp, parents,
+                    ExternalTreeDepth::readTriple, ExternalTreeDepth::writeTriple).run();
         tmp.delete();
 
         int d = 1;
@@ -125,18 +147,17 @@ public class ExternalTreeDepth {
             iterate(parents);
             d *= 2;
         }
-        int res = 0;
-        try (ObjectInputStream is = new ObjectInputStream(new BufferedInputStream(new FileInputStream(parents)))) {
-            while (true) {
-                Pair<Integer, Pair<Integer, Integer>> t = (Pair<Integer, Pair<Integer, Integer>>) is.readObject();
-                res = Math.max(res, t.second.second);
-            }
-        } catch (EOFException ignored) {
-            //
-        }
+        result = Integer.MIN_VALUE;
+        FileIterator.forEach(
+                new ObjectInputStream(parents, ExternalTreeDepth::readTriple),
+                (Object o, ArrayList out) -> {
+                    Pair<Integer, Pair<Integer, Integer>> trp = (Pair<Integer, Pair<Integer, Integer>>) o;
+                    result = Math.max(result, trp.second.second);
+                });
+
         parents.delete();
         PrintWriter outp = new PrintWriter(outfile);
-        outp.print(res + 1);
+        outp.print(result + 1);
         outp.close();
     }
 }
