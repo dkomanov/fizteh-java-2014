7a207bbfc80696ddbc10490fa33512ad9c5ea06f
Mark Ryabov <mir@epage.ru>
2014-12-14 01:11:19 +0300

    Bugs fixed

--- src/ru/fizteh/fivt/students/ryad0m/parallel/StructedTable.java
+++ src/ru/fizteh/fivt/students/ryad0m/parallel/StructedTable.java
@@ -24,8 +24,7 @@ public class StructedTable {
         location = path;
         dirTable = new DirTable(path);
         File columnFile = location.resolve("signature.tsv").toFile();
-        try {
-            FileInputStream fis = new FileInputStream(columnFile);
+        try (FileInputStream fis = new FileInputStream(columnFile)) {
             Scanner scanner = new Scanner(fis);
             columnTypes = Typer.typeListFromString(scanner.nextLine());
             scanner.close();
@@ -81,8 +80,7 @@ public class StructedTable {
             location.toFile().mkdirs();
             dirTable.save();
             File columnFile = location.resolve("signature.tsv").toFile();
-            try {
-                FileOutputStream fos = new FileOutputStream(columnFile);
+            try (FileOutputStream fos = new FileOutputStream(columnFile)) {
                 PrintStream printStream = new PrintStream(fos);
                 printStream.print(Typer.stringFromTypeList(columnTypes));
                 printStream.close();
