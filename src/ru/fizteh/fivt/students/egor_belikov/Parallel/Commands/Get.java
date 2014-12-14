dec8bc864f9a12825b2446859042f46e4dd23e5d
ol5478 <egor-belikov@yandex.ru>
2014-12-14 21:08:12 +0300

    greshnovato fixed

--- src/ru/fizteh/fivt/students/egor_belikov/Parallel/Commands/Get.java
+++ src/ru/fizteh/fivt/students/egor_belikov/Parallel/Commands/Get.java
@@ -1,6 +1,7 @@
 package ru.fizteh.fivt.students.egor_belikov.Parallel.Commands;
 
 import ru.fizteh.fivt.students.egor_belikov.Parallel.MyTableProvider;
+import ru.fizteh.fivt.students.egor_belikov.Parallel.ParallelMain;
 
 /**
  * Created by egor on 13.12.14.
@@ -8,7 +9,7 @@ import ru.fizteh.fivt.students.egor_belikov.Parallel.MyTableProvider;
 public class Get implements Command {
     @Override
     public void execute(String[] args, MyTableProvider myTableProvider) throws Exception {
-        if (myTableProvider.currentTable.get(args[1]) == null) {
+        if (myTableProvider.getTable(ParallelMain.currentTable).get(args[1]) == null) {
             System.out.println("not found");
         } else {
             System.out.println("found");
