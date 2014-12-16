026b796e2cbba80b3164e585dfda88a8ad8e8667
mike <surinmike@gmail.com>
2014-12-16 18:35:47 +0300

    streams & external sort

--- src/ru/fizteh/fivt/students/surin/ObjectOutputStream.java
+++ src/ru/fizteh/fivt/students/surin/ObjectOutputStream.java
@@ -12,12 +12,29 @@ public class ObjectOutputStream<T> implements AutoCloseable {
     private OutputStream os;
     private Writer writer;
 
-    ObjectOutputStream(File of, Writer writer) throws FileNotFoundException {
+    public static void writeInt(Object value, OutputStream os) throws IOException {
+        int n = (Integer) value;
+        byte[] buf = new byte[] {
+                (byte) (n >> 24),
+                (byte) (n >> 16),
+                (byte) (n >> 8),
+                (byte) n};
+        os.write(buf);
+    }
+
+    public static void writeTriple(Object o, OutputStream os) throws IOException {
+        Pair<Integer, Pair<Integer, Integer>> p = (Pair<Integer, Pair<Integer, Integer>>) o;
+        writeInt(p.first, os);
+        writeInt(p.second.first, os);
+        writeInt(p.second.second, os);
+    }
+
+    public ObjectOutputStream(File of, Writer writer) throws FileNotFoundException {
         this.os = new BufferedOutputStream(new FileOutputStream(of));
         this.writer = writer;
     }
 
-    ObjectOutputStream(OutputStream os, Writer writer) {
+    public ObjectOutputStream(OutputStream os, Writer writer) {
         this.os = os;
         this.writer = writer;
     }
