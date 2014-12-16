39996c52250bc46f8e2914621ebf80fdf2757d26
mike <surinmike@gmail.com>
2014-12-16 18:35:47 +0300

    streams & external sort

--- src/ru/fizteh/fivt/students/surin/ObjectInputStream.java
+++ src/ru/fizteh/fivt/students/surin/ObjectInputStream.java
@@ -18,6 +18,19 @@ public class ObjectInputStream<T> implements AutoCloseable {
         this.reader = reader;
     }
 
+    public static int readInt(InputStream is) throws IOException {
+        byte[] buf = new byte[4];
+        if (is.read(buf) != 4) {
+            throw new EOFException("eof");
+        }
+        return buf[3] + (((int) buf[2]) << 8) + (((int) buf[1]) << 16) + (((int) buf[0]) << 24);
+    }
+
+    public static Pair<Integer, Pair<Integer, Integer>> readTriple(InputStream is) throws IOException {
+        return new Pair<>(readInt(is), new Pair<>(readInt(is), readInt(is)));
+    }
+
+
     ObjectInputStream(File inf, Reader reader) throws FileNotFoundException {
         this.is = new BufferedInputStream(new FileInputStream(inf));
         this.reader = reader;
