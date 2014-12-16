53bac2c4ab4d37396f3d643a7d6e591e1be795fa
Alexander Khalyapov <khalyapov@phystech.edu>
2014-12-16 14:18:35 +0300

    Shell, Filemap, etc deleted from branch

--- src/ru/fizteh/fivt/students/AlexanderKhalyapov/FileMap/Command.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.AlexanderKhalyapov.FileMap;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface Command {
-    String getName();
-    void executeCmd(Map<String, String> dataBase, String[] args) throws IOException;
-}
--- src/ru/fizteh/fivt/students/AlexanderKhalyapov/Shell/Command.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.AlexanderKhalyapov.Shell;
-
-import java.io.IOException;
-
-public interface Command {
-    String getName();
-    void executeCmd(Shell shell, String[] args) throws IOException;
-}
