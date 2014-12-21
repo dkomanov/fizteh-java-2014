20eb8b55fd66b8528a14d6b451dcc0f38e4480ad
ZatsepinMikhail <salamence1996@gmail.com>
2014-12-21 19:27:07 +0300

    исправление ошибок из  travis

--- src/ru/fizteh/fivt/students/ZatsepinMikhail/Proxy/JUnit/TestLoggingfactory.java
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/Proxy/JUnit/TestLoggingfactory.java
@@ -74,23 +74,20 @@ public class TestLoggingfactory {
             //suppress
         }
 
-        try {
-            JSONObject result = new JSONObject(writer.toString());
-            assertTrue(result.has("timestamp"));
-            assertTrue(result.has("class"));
-            assertTrue(result.has("method"));
-            assertTrue(result.has("arguments"));
-            assertTrue(!result.has("returnValue"));
-            assertTrue(result.has("thrown"));
 
-            assertTrue(result.get("method").equals("get"));
-            assertTrue(result.get("thrown").equals("java.lang.IllegalArgumentException"));
-            JSONArray args = result.getJSONArray("arguments");
-            assertTrue(args.length() == 1);
-            assertTrue(args.get(0).toString().equals("null"));
-        } catch (JSONException e) {
-            //
-        }
+        JSONObject result = new JSONObject(writer.toString());
+        assertTrue(result.has("timestamp"));
+        assertTrue(result.has("class"));
+        assertTrue(result.has("method"));
+        assertTrue(result.has("arguments"));
+        assertTrue(!result.has("returnValue"));
+        assertTrue(result.has("thrown"));
+
+        assertTrue(result.get("method").equals("get"));
+        assertTrue(result.get("thrown").equals("java.lang.IllegalArgumentException"));
+        JSONArray args = result.getJSONArray("arguments");
+        assertTrue(args.length() == 1);
+        assertTrue(args.get(0).toString().equals("null"));
     }
 
     @Test
