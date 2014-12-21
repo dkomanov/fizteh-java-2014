52ac76f7b876f3468a422d36132b2dfa8c3aeac6
ZatsepinMikhail <salamence1996@gmail.com>
2014-12-21 18:27:55 +0300

    попытка исправить конфликт

---
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/MultiFileHashMap/CommandCreate.java
@@ -0,0 +1,38 @@
+package ru.fizteh.fivt.students.ZatsepinMikhail.MultiFileHashMap;
+
+import ru.fizteh.fivt.students.ZatsepinMikhail.MultiFileHashMap.FileMapPackage.FileMap;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CommandCreate extends CommandMultiFileHashMap {
+    public CommandCreate() {
+        name = "create";
+        numberOfArguments = 2;
+    }
+
+    @Override
+    public boolean run(MFileHashMap myMultiDataBase, String[] args) {
+        if (numberOfArguments != args.length) {
+            System.out.println(name + ": wrong number of arguments");
+            return false;
+        }
+        Path pathOfNewTable = Paths.get(myMultiDataBase.getDataBaseDirectory()
+            + System.getProperty("file.separator") + args[1]);
+        if (Files.exists(pathOfNewTable) & Files.isDirectory(pathOfNewTable)) {
+            System.out.println(args[1] + " exists");
+            return true;
+        }
+        try {
+            Files.createDirectory(pathOfNewTable);
+            System.out.println("created");
+            myMultiDataBase.addTable(args[1], new FileMap(pathOfNewTable.toString()));
+        } catch (IOException e) {
+            System.out.println(name + ": error while creating directory");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/ru/fizteh/fivt/students/ZatsepinMikhail/MultiFileHashMap/CommandUse.java
@@ -0,0 +1,26 @@
+package ru.fizteh.fivt.students.ZatsepinMikhail.MultiFileHashMap;
+
+import ru.fizteh.fivt.students.ZatsepinMikhail.MultiFileHashMap.FileMapPackage.FileMap;
+
+public class CommandUse extends CommandMultiFileHashMap {
+    public CommandUse() {
+        name = "use";
+        numberOfArguments = 2;
+    }
+
+    @Override
+    public boolean run(MFileHashMap myMap, String[] args) {
+        if (numberOfArguments != args.length) {
+            System.out.println(name + ": wrong number of arguments");
+            return false;
+        }
+        FileMap newCurrentTable = myMap.findTableByName(args[1]);
+        if (newCurrentTable != null) {
+            myMap.setCurrentTable(newCurrentTable);
+            System.out.println("using " + args[1]);
+        } else {
+            System.out.println(args[1] + " not exists");
+        }
+        return true;
+    }
+}
