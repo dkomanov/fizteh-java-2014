31483fb00f390f2a46c87c1e6231cfae0bb9ebfc
DeSerg <sergpopov95@gmail.com>
2014-12-21 17:17:07 +0300

    Some fixes

--- src/ru/fizteh/fivt/students/deserg/telnet/server/ConnectionManager.java
+++ src/ru/fizteh/fivt/students/deserg/telnet/server/ConnectionManager.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
