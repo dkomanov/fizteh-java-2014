156409fa66e7270f68ab29ea337d2f8cfff55391
IgorTitov396 <1hp_phoenix@mail.ru>
2014-12-22 11:02:02 +0300

    Update FileMapCommand.java

--- src/ru/fizteh/fivt/students/titov/JUnit/file_map/FileMapCommand.java
+++ src/ru/fizteh/fivt/students/titov/JUnit/file_map/FileMapCommand.java
@@ -1,8 +1,8 @@
 package ru.fizteh.fivt.students.titov.JUnit.file_map;
 
 public abstract class FileMapCommand extends Command<FileMap> {
-    public void initialize(String command_name, int n) {
-        name = command_name;
+    public void initialize(String commandName, int n) {
+        name = commandName;
         numberOfArguments = n;
     }
 }
